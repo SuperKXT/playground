2fb61f2d926786fa3e7847750e9224f029c29975
Saad Khan <saadkhankxt@hotmail.com>
2022-07-29 10:40:55 +0500

    Extend wordle.ts

--- wordle/wordle.ts
+++ wordle/wordle.ts
@@ -1,33 +1,64 @@
 import wordList from './word-list';
 
-const findWord = (letters: string) => {
-
-	for (const word of wordList) {
-
-		const matched = word.split('').filter(char => letters.includes(char)).length;
-		if (matched === 5) {
-			console.log(word);
-		}
-
-	}
-
+const matchStrings = (
+	string1: string,
+	string2: string,
+	matchAtLeast: number = 5,
+	noRepeat?: boolean,
+): boolean => {
+	let array1 = string1.split('');
+	if (noRepeat) array1 = Array.from(new Set(array1));
+	const matched = array1.filter(char =>
+		string2.includes(char)
+	).length;
+	return matched === matchAtLeast;
 };
 
-const matchPattern = (pattern: RegExp) => {
+const findWordsByLetters = (
+	letters: string,
+	matchAtLeast: number = 5,
+	noRepeat?: boolean,
+): string[] => {
 
-	for (const word of wordList) {
+	const matches = wordList.filter(word =>
+		matchStrings(
+			word,
+			letters,
+			matchAtLeast,
+			noRepeat
+		)
+	);
+	console.log(`Found (\x1b[32m${matches.length}\x1b[0m) Matches`);
+	console.log(`\x1b[32m${matches.join(', ')}\x1b[0m`);
+	return matches;
+};
 
-		const matched = word.match(pattern);
-		if (matched) {
-			console.log(matched.map(value => value));
-		}
+const matchPattern = (
+	pattern: RegExp,
+	knownLetters?: string
+): string[] => {
 
-	}
+	const matches = wordList.filter(word =>
+		word.match(pattern)
+		&& (
+			!knownLetters
+			|| matchStrings(
+				word,
+				knownLetters,
+				knownLetters.length,
+				true
+			)
+		)
+	);
+
+	console.log(`Found (\x1b[32m${matches.length}\x1b[0m) Matches`);
+	console.log(`\x1b[32m${matches.join(', ')}\x1b[0m`);
+	return matches;
 
 };
 
 /* cspell: disable-next-line */
-// // findWord('wtyuopfghjkvmqzxvm');
+//findWordByLetters('stue');
 
 /* cspell: disable-next-line */
-matchPattern(/sto[stowyuipadfgjvbnm][stowyuipadfgjvbnm]/);
+matchPattern(/re[rlwpfgjkbqzx][rewpfgjkbqzx][lwpfgjkbqzx]/, 'rel');
