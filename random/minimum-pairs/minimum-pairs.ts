8890c7e4858284739bf827d7dafd00c783ffe0c0
Saad Khan <saadkhankxt@hotmail.com>
2022-08-16 21:08:02 +0500

    Tweak minimum-pairs

--- random/minimum-pairs/minimum-pairs.ts
+++ random/minimum-pairs/minimum-pairs.ts
@@ -6,18 +6,19 @@ export const minimumPairs = (numbers: number[]) => {
 	let minimumDistance = Infinity;
 	const pairs: Pair[] = [];
 
-	for (let i = 1; i <= sorted.length; i++) {
+	for (let i = 1; i < sorted.length; i++) {
 
 		const current = sorted[i] as number;
 		const previous = sorted[i - 1] as number;
 		const distance = current - previous;
 
+		if (distance > minimumDistance) continue;
+
+		pairs.push(`${previous} ${current}`);
+
 		if (distance < minimumDistance) {
 			minimumDistance = distance;
 			pairs.splice(pairs.length);
-		}
-		if (distance === minimumDistance) {
-			pairs.push(`${previous} ${current}`);
 		}
 
 	}
