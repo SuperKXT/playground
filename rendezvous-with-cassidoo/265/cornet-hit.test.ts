d43239a5884c00b7a6c255172de2dc7bc96ad746
SuperKXT <saadkhankxt@hotmail.com>
2022-09-12 14:31:09 +0500

    Add entry for rendezvous-with-cassidoo#265

---
+++ rendezvous-with-cassidoo/265/cornet-hit.test.ts
@@ -0,0 +1,32 @@
+import { cornerHit, XY } from './cornet-hit';
+
+interface Test {
+	boxSize: XY,
+	initialPosition: XY,
+	screenSize: XY,
+	result: boolean,
+}
+
+const tests: Test[] = [
+	{
+		boxSize: [5, 5],
+		initialPosition: [0, 0],
+		screenSize: [100, 100],
+		result: true,
+	},
+	{
+		boxSize: [5, 5],
+		initialPosition: [45, 70],
+		screenSize: [400, 200],
+		result: false,
+	},
+];
+
+describe('testing cornerHit', () => {
+	for (const test of tests) {
+		it(`should return ${test.result} for box: ${test.boxSize}, position: ${test.initialPosition}, size: ${test.screenSize}`, () => {
+			const result = cornerHit(test.boxSize, test.initialPosition, test.screenSize);
+			expect(result).toStrictEqual(test.result);
+		});
+	}
+});
---
+++ rendezvous-with-cassidoo/265/cornet-hit.ts
@@ -0,0 +1,8 @@
+export type XY = [number, number];
+export const cornerHit = (
+	boxSize: XY,
+	initialPosition: XY,
+	screenSize: XY
+): boolean => {
+	return false;
+};
