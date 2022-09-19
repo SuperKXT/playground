cd5b3b6658e299d02e8f9dc06ba4b751034f708b
SuperKXT <saadkhankxt@hotmail.com>
2022-09-19 14:38:36 +0500

    Add solution for rendezvous-with-cassidoo#265

--- rendezvous-with-cassidoo/265/cornet-hit.ts
+++ rendezvous-with-cassidoo/265/cornet-hit.ts
@@ -4,5 +4,9 @@ export const cornerHit = (
 	initialPosition: XY,
 	screenSize: XY
 ): boolean => {
-	return false;
+	const [width, height] = boxSize;
+	const [x, y] = initialPosition;
+	const [screenCornerX, screenCornerY] = screenSize;
+	const [logoCornerX, logoCornerY] = [x + width, y + height];
+	return screenCornerX - logoCornerX === screenCornerY - logoCornerY;
 };
