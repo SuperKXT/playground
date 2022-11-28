353b1232bd79728f6daf8ce4d8e90b02cd8e93e4
SuperKXT <superkxt@outlook.com>
2022-11-28 21:23:24 +0500

    Add recursive-rename types to separate file

---
+++ recursive-rename/recursive-rename.types.ts
@@ -0,0 +1,48 @@
+export enum RenameErrors {
+	EXISTS = 'path already exists',
+}
+
+interface AgnosticResult {
+	type: 'success' | 'error' | 'unchanged',
+	oldName: string,
+	newName?: string,
+	error?: string,
+	children?: RenameResult[],
+}
+
+interface ValidResult extends AgnosticResult {
+	type: 'success',
+	newName: string,
+	error?: undefined,
+}
+
+interface ErrorResult extends AgnosticResult {
+	type: 'error',
+	newName: string,
+	error: string,
+}
+
+interface UnchangedResult extends AgnosticResult {
+	type: 'unchanged',
+	newName?: undefined,
+	error?: undefined,
+}
+
+export type RenameResult = (
+	| ValidResult
+	| ErrorResult
+	| UnchangedResult
+);
+
+export interface RenameOptions {
+	verbose?: boolean,
+	yes?: boolean,
+	onlyChanges?: boolean,
+}
+
+export type RenameLogFunction = (
+	results: RenameResult[],
+	verbose?: boolean,
+	onlyChanges?: boolean,
+	isConfirmation?: boolean
+) => string;
