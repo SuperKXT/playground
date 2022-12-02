4a9d0ef45f89e9458b2b1edd4d3f30c7d61bda79
SuperKXT <superkxt@outlook.com>
2022-12-02 11:02:51 +0500

    Add advent-of-code-2022-day-1 part 2 solution

--- advent-of-code/2022/day-1/count-calories.test.ts
+++ advent-of-code/2022/day-1/count-calories.test.ts
@@ -1,8 +1,14 @@
-import { countCalories } from './count-calories';
+import { countCalories, CountCaloriesSolution } from './count-calories';
 
 describe('testing countCalories', () => {
 	it('should print the index and amount of the elf with most calories', async () => {
 		const response = await countCalories();
-		expect(response).toStrictEqual([229, 69626]);
+		const solution: CountCaloriesSolution = {
+			maxIndex: 229,
+			maxCalories: 69626,
+			topThreeIndexes: [229, 178, 213],
+			topThreeCalories: 206780,
+		};
+		expect(response).toStrictEqual(solution);
 	});
 });
--- advent-of-code/2022/day-1/count-calories.ts
+++ advent-of-code/2022/day-1/count-calories.ts
@@ -1,7 +1,14 @@
 import { readFile } from 'fs/promises';
 import path from 'path';
 
-export const countCalories = async (): Promise<[number, number]> => {
+export interface CountCaloriesSolution {
+	maxIndex: number,
+	maxCalories: number,
+	topThreeIndexes: [number, number, number],
+	topThreeCalories: number,
+}
+
+export const countCalories = async (): Promise<CountCaloriesSolution> => {
 
 	const input = await readFile(
 		path.join(__dirname, 'input.txt'),
@@ -17,6 +24,20 @@ export const countCalories = async (): Promise<[number, number]> => {
 	const maxCalories = Math.max(...calories);
 	const maxIndex = calories.indexOf(maxCalories);
 
-	return [maxIndex, maxCalories];
+	const topThree = [...calories].sort((a, b) => b - a).slice(0, 3);
+	const topThreeIndexes = topThree.map(row =>
+		calories.indexOf(row)
+	) as [number, number, number];
+	const topThreeCalories = topThree.reduce(
+		(sum, calories) => sum += calories
+		, 0
+	);
+
+	return {
+		maxIndex,
+		maxCalories,
+		topThreeIndexes,
+		topThreeCalories,
+	};
 
 };
