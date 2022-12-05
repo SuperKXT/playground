21ba8bb931a6694f3c9a0b4276a098a9d8deddd6
SuperKXT <superkxt@outlook.com>
2022-12-05 14:42:20 +0500

    Replace jest.config.js with ts config file

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import {
+	JestConfigWithTsJest,
+	pathsToModuleNameMapper,
+} from 'ts-jest';
+
+// eslint-disable-next-line import/extensions
+import { compilerOptions } from './tsconfig.json';
+
+const config: JestConfigWithTsJest = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	clearMocks: true,
+	collectCoverage: true,
+	moduleNameMapper: pathsToModuleNameMapper(
+		compilerOptions.paths,
+		{ prefix: '<rootDir>' }
+	),
+};
+
+export default config;
