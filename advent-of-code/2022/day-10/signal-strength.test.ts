cf4033b46e0ab935a6dc5d1ea659b3d4559a600f
SuperKXT <superkxt@outlook.com>
2022-12-10 18:53:28 +0500

    Add solution for advent-of-code/2022/day-10

---
+++ advent-of-code/2022/day-10/signal-strength.test.ts
@@ -0,0 +1,190 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+import { signalStrength } from './signal-strength';
+
+const example = `
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`;
+
+type Solution = ReturnType<typeof signalStrength>;
+
+describe('testing signalStrength', () => {
+	it('should return the correct solution for example test', () => {
+		const response = signalStrength(example);
+		const solution: Solution = {
+			strength: 13140,
+			crtOutput: [
+				'##..##..##..##..##..##..##..##..##..##..',
+				'###...###...###...###...###...###...###.',
+				'####....####....####....####....####....',
+				'#####.....#####.....#####.....#####.....',
+				'######......######......######......####',
+				'#######.......#######.......#######.....',
+			].join('\n'),
+		};
+		expect(response).toStrictEqual(solution);
+	});
+	it('should return the correct solution for the input file', async () => {
+		const input = await readFile(
+			path.join(__dirname, 'input.txt'),
+			'utf-8'
+		);
+		const solution: Solution = {
+			strength: 16060,
+			crtOutput: [
+				'###...##...##..####.#..#.#....#..#.####.',
+				'#..#.#..#.#..#.#....#.#..#....#..#.#....',
+				'###..#..#.#....###..##...#....####.###..',
+				'#..#.####.#....#....#.#..#....#..#.#....',
+				'#..#.#..#.#..#.#....#.#..#....#..#.#....',
+				'###..#..#..##..####.#..#.####.#..#.#....',
+			].join('\n'),
+		};
+		expect(signalStrength(input)).toStrictEqual(solution);
+	});
+});
---
+++ advent-of-code/2022/day-10/signal-strength.ts
@@ -0,0 +1,89 @@
+const commands = [
+	'addx',
+	'noop',
+] as const;
+
+type Command = typeof commands[number];
+
+const isCommand = (
+	value: any
+): value is Command => (
+	commands.includes(value)
+);
+
+interface Solution {
+	strength: number,
+	crtOutput: string,
+}
+
+interface Clock {
+	cycle: number,
+	register: number,
+	target: number,
+}
+
+const getCrtOutput = (
+	{ register, cycle }: Clock
+): '#' | '.' | '\n#' | '\n.' => {
+	const sprite = [register - 1, register, register + 1];
+	const position = ((cycle - 1) % 40);
+	const isEol = (
+		cycle > 1
+		&& position === 0
+	);
+	return (
+		sprite.includes(position)
+			? `${isEol ? '\n' : ''}#`
+			: `${isEol ? '\n' : ''}.`
+	);
+};
+
+export const signalStrength = (
+	input: string
+): Solution => {
+
+	const clock: Clock = {
+		cycle: 0,
+		register: 1,
+		target: 20,
+	};
+
+	const solution: Solution = {
+		strength: 0,
+		crtOutput: '',
+	};
+
+	for (const row of input.split('\n')) {
+
+		const [command, x] = row.split(' ');
+		if (
+			!isCommand(command)
+			|| (command === 'addx' && isNaN(Number(x)))
+		) continue;
+
+		const prevRegister = clock.register;
+
+		switch (command) {
+			case 'noop':
+				clock.cycle++;
+				solution.crtOutput += getCrtOutput(clock);
+				break;
+			case 'addx':
+				clock.cycle++;
+				solution.crtOutput += getCrtOutput(clock);
+				clock.cycle++;
+				solution.crtOutput += getCrtOutput(clock);
+				clock.register += Number(x);
+				break;
+		}
+
+		if (clock.cycle >= clock.target) {
+			solution.strength += clock.target * prevRegister;
+			clock.target += 40;
+		}
+
+	}
+
+	return solution;
+
+};
