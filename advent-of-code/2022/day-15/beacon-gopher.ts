bcd6cd3942f663acb4e628ad159d926fcd780346
SuperKXT <superkxt@outlook.com>
2022-12-15 19:29:30 +0500

    Add incomplete solution for aoc/22/15

--- advent-of-code/2022/day-15/beacon-gopher.ts
+++ advent-of-code/2022/day-15/beacon-gopher.ts
@@ -14,7 +14,8 @@ interface Beacon {
 interface Sensor {
 	row: number,
 	col: number,
-	beacon: Beacon,
+	beacon: number,
+	distance: number,
 }
 
 export const beaconGopher = (
@@ -29,10 +30,74 @@ export const beaconGopher = (
 
 	const beacons: Beacon[] = [];
 	const sensors: Sensor[] = [];
+	let startRow = 0;
+	let endRow = 0;
+	let startCol = 0;
+	let endCol = 0;
+
+	const map: Map = [];
 
 	for (const line of input.split('\n')) {
+		const [
+			sensorCol,
+			sensorRow,
+			beaconCol,
+			beaconRow,
+		] = line
+			.replace(/(Sensor at x=)|( y=)|( closest beacon is at x=)/g, '')
+			.split(/,|:/)
+			.map(Number) as [number, number, number, number];
+
+		const distance = (
+			Math.abs(sensorRow - beaconRow)
+			+ Math.abs(sensorCol + beaconCol)
+		);
+
+		const currStartRow = sensorRow - distance;
+		const currEndRow = Math.max(sensorRow, beaconRow, sensorRow + distance);
+		const currStartCol = sensorCol - distance;
+		const currEndCol = Math.max(sensorCol, beaconCol, sensorCol + distance);
+
+		if (startRow > currStartRow) {
+			map.unshift(
+				...new Array(startRow - currStartRow).fill(
+					new Array(endCol - startCol).fill('.')
+				)
+			);
+			startRow = currStartRow;
+		}
+		if (endRow < currEndRow) {
+			map.push(
+				...new Array(currEndRow - endRow).fill(
+					new Array(endCol - startCol).fill('.')
+				)
+			);
+			endRow = currEndRow;
+		}
+		if (startCol > currStartCol) startCol = currStartCol;
+		if (endCol > currEndCol) endCol = currEndCol;
+
+		const beacon: Beacon = {
+			row: beaconRow,
+			col: beaconCol,
+		};
+
+		const index = beacons.findIndex(({ row, col }) =>
+			row === beaconRow && col === beaconCol
+		);
+		const sensor: Sensor = {
+			row: sensorRow,
+			col: sensorCol,
+			beacon: index === -1 ? beacons.length : index,
+			distance,
+		};
+		if (index === -1) beacons.push(beacon);
+		sensors.push(sensor);
 
 	}
+
+	console.log(sensors, beacon);
+	console.log(map.map(row => row.join('')).join('\n'));
 
 	return solution;
 
