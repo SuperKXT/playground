a0b44b2bff557ebab60a0a168b99356a0b80a25f
SuperKXT <superkxt@outlook.com>
2022-12-29 23:01:42 +0500

    Add canBePalindrome function

---
+++ random/palindrome/palindrome.test.ts
@@ -0,0 +1,21 @@
+import { canBePalindrome } from './palindrome';
+
+interface Test {
+	string: string,
+	result: boolean,
+}
+
+const tests: Test[] = [
+	{ string: 'aaccd', result: true }, // cSpell: disable-line
+	{ string: 'nurses run', result: true },
+	{ string: 'madam', result: true },
+	{ string: 'madam', result: true },
+	{ string: 'palindrome', result: false },
+	{ string: 'tight fit', result: false },
+];
+
+describe('testing canBePalindrome', () => {
+	it.each(tests)('return if the characters can be a palindrome', ({ string, result }) => {
+		expect(canBePalindrome(string)).toBe(result);
+	});
+});
---
+++ random/palindrome/palindrome.ts
@@ -0,0 +1,18 @@
+export const canBePalindrome = (
+	string: string
+): boolean => {
+	let hasOdd = false;
+	const arr = Array.from(string.replace(/\s/g, '')).sort();
+	for (let i = 0; i < arr.length; i++) {
+		const curr = arr[i] as string;
+		const end = arr.lastIndexOf(curr);
+		const sub = arr.slice(i, end + 1);
+		if (sub.length % 2 === 0) {
+			i = end;
+			continue;
+		}
+		else if (hasOdd) return false;
+		else hasOdd = true;
+	}
+	return true;
+};
