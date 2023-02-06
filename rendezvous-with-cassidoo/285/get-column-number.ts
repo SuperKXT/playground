6ca4e89d3aa8b253d0e6cdec48c4b0eb647bdc9d
SuperKXT <superkxt@outlook.com>
2023-02-06 19:32:26 +0500

    Tweak rendezvous-with-cassidoo#285

--- rendezvous-with-cassidoo/285/get-column-number.ts
+++ rendezvous-with-cassidoo/285/get-column-number.ts
@@ -1,7 +1,7 @@
 export const getColumnNumber = (
 	string: string
 ): number => {
-	return Array.from(string).reduceRight(
+	return Array.from(string).reduce(
 		(sum, char, index) => {
 			const code = char.charCodeAt(0) - 64;
 			const weight = 26 ** (string.length - 1 - index);
