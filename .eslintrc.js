400c7ec4210e0aaca92865c322dd8c3b42735062
SuperKXT <superkxt@outlook.com>
2023-02-15 14:47:29 +0500

    Update eslint config for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,31 +7,15 @@ const config = {
 		node: true,
 		jest: true,
 	},
-	extends: [
-		'eslint:recommended',
-	],
+	extends: ['eslint:recommended', 'prettier'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-	plugins: [
-		'import',
-		'unused-imports',
-	],
+	plugins: ['import', 'unused-imports'],
 	rules: {
-		indent: ['off', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
 		radix: ['warn', 'as-needed'],
-		'comma-dangle': ['error', {
-			arrays: 'always-multiline',
-			objects: 'always-multiline',
-			imports: 'always-multiline',
-			exports: 'always-multiline',
-			functions: 'never',
-		}],
 		'import/extensions': 'warn',
 		'import/order': [
 			'error',
@@ -59,49 +43,42 @@ const config = {
 		],
 		'import/no-duplicates': 'warn',
 		'import/no-default-export': 'error',
-		'no-console': ['error', {
-			allow: ['warn', 'error', 'info'],
-		}],
-		'no-multiple-empty-lines': ['error', {
-			max: 1,
-			maxEOF: 0,
-			maxBOF: 0,
-		}],
-		'eol-last': ['error', 'always'],
-		'quote-props': ['error', 'as-needed'],
+		'no-console': [
+			'error',
+			{
+				allow: ['warn', 'error', 'info'],
+			},
+		],
 		'no-unused-vars': 'off',
 		'unused-imports/no-unused-imports': 'warn',
 		'unused-imports/no-unused-vars': [
 			'warn',
-			{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+			{
+				vars: 'all',
+				varsIgnorePattern: '^_',
+				args: 'after-used',
+				argsIgnorePattern: '^_',
+			},
 		],
 		'object-shorthand': ['error', 'always'],
-		'no-constant-condition': ['error', {
-			checkLoops: false,
-		}],
+		'no-constant-condition': [
+			'error',
+			{
+				checkLoops: false,
+			},
+		],
 	},
 	overrides: [
 		{
 			files: ['**/*.ts', '**/*.tsx'],
-			extends: [
-				'plugin:@typescript-eslint/recommended',
-			],
+			extends: ['plugin:@typescript-eslint/recommended'],
 			plugins: ['@typescript-eslint'],
 			rules: {
+				'@typescript-eslint/no-extra-semi': 'off',
 				'@typescript-eslint/no-explicit-any': 'off',
 				'@typescript-eslint/no-inferrable-types': 'off',
 				'@typescript-eslint/no-unnecessary-type-constraint': 'off',
 				'@typescript-eslint/prefer-nullish-coalescing': 'off',
-				'@typescript-eslint/member-delimiter-style': ['error', {
-					multiline: {
-						delimiter: 'comma',
-						requireLast: true,
-					},
-					singleline: {
-						delimiter: 'comma',
-						requireLast: true,
-					},
-				}],
 				'@typescript-eslint/no-unused-vars': 'off',
 				'@typescript-eslint/no-unnecessary-condition': 'error',
 			},
