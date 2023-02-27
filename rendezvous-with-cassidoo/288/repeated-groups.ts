37669d4e476e3e3161b5c981af8034d96e33c66d
SuperKXT <superkxt@outlook.com>
2023-02-27 11:34:56 +0500

    Improve #288

--- rendezvous-with-cassidoo/288/repeated-groups.ts
+++ rendezvous-with-cassidoo/288/repeated-groups.ts
@@ -2,19 +2,13 @@ export const repeatedGroups = (input: number[]): number[][] => {
 	const groups: number[][] = [];
 	let current: null | number = null;
 	for (let i = 0; i < input.length; i++) {
-		const last = input[i - 1];
 		const number = input[i] as number;
-		const next = input[i + 1];
-		if (next === number) {
-			if (number === current) {
-				groups.at(-1)?.push(number);
-			} else {
-				groups.push([number]);
-				current = number;
-			}
-		} else if (last === number) {
+		if (number === current) {
 			groups.at(-1)?.push(number);
+		} else {
+			groups.push([number]);
+			current = number;
 		}
 	}
-	return groups;
+	return groups.filter((group) => group.length > 1);
 };
