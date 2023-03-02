3f0f0bec6a2ec7bd2b548dbeae007cae45b5a8d0
SuperKXT <superkxt@outlook.com>
2023-03-02 18:32:48 +0500

    Add new rules to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,13 @@ const config = {
 				allow: ['warn', 'error', 'info'],
 			},
 		],
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: 'TSEnumDeclaration',
+				message: "Don't declare enums. Use POJO with as const instead",
+			},
+		],
 		'no-unused-vars': 'off',
 		'unused-imports/no-unused-imports': 'warn',
 		'import/extensions': [
@@ -35,6 +42,7 @@ const config = {
 		],
 		'import/no-duplicates': 'warn',
 		'import/no-default-export': 'error',
+		'import/consistent-type-specifier-style': 'error',
 		'object-shorthand': ['error', 'always'],
 		'no-constant-condition': [
 			'error',
@@ -88,6 +96,46 @@ const config = {
 						allowConstantLoopConditions: true,
 					},
 				],
+				'@typescript-eslint/no-confusing-void-expression': [
+					'error',
+					{
+						ignoreArrowShorthand: true,
+					},
+				],
+				'@typescript-eslint/no-require-imports': 'error',
+				'@typescript-eslint/no-redundant-type-constituents': 'error',
+				'@typescript-eslint/no-useless-empty-export': 'error',
+				'@typescript-eslint/prefer-readonly': 'error',
+				'@typescript-eslint/switch-exhaustiveness-check': 'error',
+				'@typescript-eslint/sort-type-constituents': 'warn',
+				'@typescript-eslint/require-array-sort-compare': [
+					'error',
+					{
+						ignoreStringArrays: true,
+					},
+				],
+				'@typescript-eslint/promise-function-async': 'warn',
+				'@typescript-eslint/default-param-last': 'warn',
+				'no-dupe-class-members': 'off',
+				'@typescript-eslint/no-dupe-class-members': 'warn',
+				'@typescript-eslint/no-loop-func': 'warn',
+				'@typescript-eslint/no-shadow': [
+					'error',
+					{
+						builtinGlobals: true,
+					},
+				],
+				'@typescript-eslint/no-unused-expressions': [
+					'error',
+					{
+						allowShortCircuit: true,
+						allowTernary: true,
+						enforceForJSX: true,
+					},
+				],
+				'@typescript-eslint/return-await': 'error',
+				'no-use-before-define': 'off',
+				'@typescript-eslint/no-use-before-define': 'warn',
 			},
 		},
 		{
