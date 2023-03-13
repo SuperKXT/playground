1a5bc2c62eabcf2792cc5e04350e299606b7f7e5
SuperKXT <superkxt@outlook.com>
2023-03-13 11:19:27 +0500

    Add solution for rendezvous-with-cassidoo#291

--- rendezvous-with-cassidoo/291/fraction-math.test.ts
+++ rendezvous-with-cassidoo/291/fraction-math.test.ts
@@ -11,7 +11,31 @@ const TESTS: Test[] = [
 		output: '3/2',
 	},
 	{
+		args: ['3/4', 'subtract', '3/4'],
+		output: '0/16',
+	},
+	{
+		args: ['3/4', 'multiply', '3/4'],
+		output: '9/16',
+	},
+	{
+		args: ['3/4', 'divide', '3/4'],
+		output: '1/1',
+	},
+	{
+		args: ['1/8', 'add', '2/2'],
+		output: '9/8',
+	},
+	{
+		args: ['1/8', 'subtract', '2/2'],
+		output: '-7/8',
+	},
+	{
 		args: ['1/8', 'multiply', '2/2'],
+		output: '1/8',
+	},
+	{
+		args: ['1/8', 'divide', '2/2'],
 		output: '1/8',
 	},
 ];
--- rendezvous-with-cassidoo/291/fraction-math.ts
+++ rendezvous-with-cassidoo/291/fraction-math.ts
@@ -1,9 +1,45 @@
 type Fraction = `${number}/${number}`;
 
 export const fractionMath = (
-	first: Fraction,
+	fractionA: Fraction,
 	operation: 'add' | 'divide' | 'multiply' | 'subtract',
-	second: Fraction
+	fractionB: Fraction
 ): Fraction => {
-	return first;
+	const parsedA = fractionA.split('/').map(Number) as [number, number];
+	const parsedB = fractionB.split('/').map(Number) as [number, number];
+	const solution: [number, number] = [0, 0];
+	switch (operation) {
+		case 'add': {
+			solution[0] = parsedA[0] * parsedB[1] + parsedB[0] * parsedA[1];
+			solution[1] = parsedA[1] * parsedB[1];
+			break;
+		}
+		case 'subtract': {
+			solution[0] = parsedA[0] * parsedB[1] - parsedB[0] * parsedA[1];
+			solution[1] = parsedA[1] * parsedB[1];
+			break;
+		}
+		case 'multiply': {
+			solution[0] = parsedA[0] * parsedB[0];
+			solution[1] = parsedA[1] * parsedB[1];
+			break;
+		}
+		case 'divide': {
+			solution[0] = parsedA[0] * parsedB[1];
+			solution[1] = parsedA[1] * parsedB[0];
+			break;
+		}
+	}
+	const findFactors = (number: number) => {
+		return [...new Array<undefined>(Math.abs(number))]
+			.reduce<number[]>((array, _, index) => {
+				return number % (index + 1) === 0 ? [...array, index + 1] : array;
+			}, [])
+			.sort((first, second) => second - first);
+	};
+	const numeratorFactors = findFactors(solution[0]);
+	const denominatorFactors = findFactors(solution[1]);
+	const hcf =
+		numeratorFactors.find((factor) => denominatorFactors.includes(factor)) ?? 1;
+	return `${solution[0] / hcf}/${solution[1] / hcf}`;
 };
