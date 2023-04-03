095fad7978d61a7ec1087d6d1f122a0af8f3a945
SuperKXT <superkxt@outlook.com>
2023-04-03 15:05:28 +0500

    Prettify object helpers response

--- helpers/object/object.helpers.ts
+++ helpers/object/object.helpers.ts
@@ -12,24 +12,22 @@ export const objectValues = <Keys extends PropertyKey, Type>(
 
 export const omit = <Key extends string, Type extends Record<Key, unknown>>(
 	object: Type,
-	toOmit: Key | Key[]
-): Omit<Type, Key> => {
-	const toOmitArray = Array.isArray(toOmit) ? toOmit : [toOmit];
-	const clone = objectEntries(object).reduce((obj, [key, value]) => {
-		if (toOmitArray.includes(key)) return obj;
-
+	keys: Key | Key[]
+): Prettify<Omit<Type, Key>> => {
+	const keyArray = Array.isArray(keys) ? keys : [keys];
+	return objectEntries(object).reduce<Omit<Type, Key>>((obj, [key, value]) => {
+		if (keyArray.includes(key)) return obj;
 		return { ...obj, [key]: value };
-	}, {});
-	return clone as Omit<Type, Key>;
+	}, {} as Omit<Type, Key>);
 };
 
 export const pick = <Key extends string, Type extends Record<Key, unknown>>(
 	object: Type,
-	toOmit: Key | Key[]
-) => {
-	const toPickArray = Array.isArray(toOmit) ? toOmit : [toOmit];
+	keys: Key | Key[]
+): Prettify<Pick<Type, Key>> => {
+	const keyArray = Array.isArray(keys) ? keys : [keys];
 	return objectEntries(object).reduce<Pick<Type, Key>>((obj, [key, value]) => {
-		if (!toPickArray.includes(key)) return obj;
+		if (!keyArray.includes(key)) return obj;
 		return { ...obj, [key]: value };
 	}, {} as Pick<Type, Key>);
 };
