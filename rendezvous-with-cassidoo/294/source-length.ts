84097316387d1b32a879aa25e56d204b2598ec01
SuperKXT <superkxt@outlook.com>
2023-04-03 13:57:45 +0500

    Change getSourceLength response to string

--- rendezvous-with-cassidoo/294/source-length.ts
+++ rendezvous-with-cassidoo/294/source-length.ts
@@ -1,6 +1,8 @@
 import { readFile } from 'fs/promises';
 
-export const getSourceLength = async (): Promise<number> => {
+import { numberToWords } from '~/helpers/format';
+
+export const getSourceLength = async (): Promise<string> => {
 	const file = await readFile(__filename, 'utf-8');
 	const lines = file.split('\n').map((row) => row.trim());
 	let isFunctionBody = false;
@@ -9,11 +11,11 @@ export const getSourceLength = async (): Promise<number> => {
 		const trimmed = line.trim();
 		if (
 			trimmed ===
-			'export const getSourceLength = async (): Promise<number> => {'
+			'export const getSourceLength = async (): Promise<string> => {'
 		)
 			isFunctionBody = true;
 		else if (trimmed === '};') break;
 		else if (isFunctionBody) length += trimmed.length;
 	}
-	return length;
+	return numberToWords(length);
 };
