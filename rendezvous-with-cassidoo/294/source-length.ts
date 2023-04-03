e1c67ab9bb15b2648f6a95cc8618695033d10c9c
SuperKXT <superkxt@outlook.com>
2023-04-03 09:38:25 +0500

    Tweak rwc#294

--- rendezvous-with-cassidoo/294/source-length.ts
+++ rendezvous-with-cassidoo/294/source-length.ts
@@ -3,7 +3,7 @@ import { readFile } from 'fs/promises';
 export const getSourceLength = async (): Promise<number> => {
 	const file = await readFile(__filename, 'utf-8');
 	const lines = file.split('\n').map((row) => row.trim());
-	let isProgram = false;
+	let isFunctionBody = false;
 	let length = 0;
 	for (const line of lines) {
 		const trimmed = line.trim();
@@ -11,9 +11,9 @@ export const getSourceLength = async (): Promise<number> => {
 			trimmed ===
 			'export const getSourceLength = async (): Promise<number> => {'
 		)
-			isProgram = true;
+			isFunctionBody = true;
 		else if (trimmed === '};') break;
-		else if (isProgram) length += trimmed.length;
+		else if (isFunctionBody) length += trimmed.length;
 	}
 	return length;
 };
