6a7eef8ce800e96564d73deb0767029b39a72e7d
SuperKXT <superkxt@outlook.com>
2023-04-20 00:58:11 +0500

    Add solution for rwc#296

--- rendezvous-with-cassidoo/296/max-line-points.test.ts
+++ rendezvous-with-cassidoo/296/max-line-points.test.ts
@@ -1,6 +1,43 @@
 import { maxPointsOnLine } from './max-line-points';
 
-test('should return the maximum points in a straight line', async () => {
-	const response = await maxPointsOnLine([[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]);
-	expect(response).toBe(4);
+interface Test {
+	input: [number, number][],
+	output: number,
+}
+
+const tests: Test[] = [
+	{
+    input: [
+      [1, 1],
+      [3, 2],
+      [5, 3],
+      [4, 1],
+      [2, 3],
+      [1, 4],
+    ],
+    output: 4,
+  },
+  {
+    input: [
+      [1, 1],
+      [2, 2],
+      [3, 3],
+    ],
+    output: 3,
+  },
+  {
+    input: [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [1, 9],
+    ],
+    output: 5,
+  },
+]
+
+test.each(tests)('should return the maximum points in a straight line', async ({ input, output }) => {
+	const response = await maxPointsOnLine(input);
+	expect(response).toBe(output);
 });
--- rendezvous-with-cassidoo/296/max-line-points.ts
+++ rendezvous-with-cassidoo/296/max-line-points.ts
@@ -1,5 +1,14 @@
-type Point = [number, number];
-
-export const maxPointsOnLine = (points: Point[]): number => {
-	return 0;
+export const maxPointsOnLine = (points: [number, number][]): number => {
+	let max = 0;
+	for (const [x1, y1] of points) {
+		let lines = new Map<number, number>();
+		for (const [x2, y2] of points) {
+			if (x1 === x2 && y1 === y2) continue;
+			const slope = x2 === x1 ? Infinity : (y2 - y1) / (x2 - x1);
+			const length = (lines.get(slope) ?? 1) + 1;
+			lines.set(slope, length);
+			if (length > max) max = length;
+		}
+	}
+	return max;
 };
