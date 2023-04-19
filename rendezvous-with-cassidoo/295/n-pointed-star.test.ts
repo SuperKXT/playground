f6d8711987b5d6e82b8f64a7eae8afaa58fca34a
SuperKXT <superkxt@outlook.com>
2023-04-19 23:06:08 +0500

    Add solution for rwc#295

--- rendezvous-with-cassidoo/295/n-pointed-star.test.ts
+++ rendezvous-with-cassidoo/295/n-pointed-star.test.ts
@@ -1,6 +1,27 @@
 import { getStarAngles } from './n-pointed-star';
 
-test('should return the angles of the star with n sides', async () => {
-	const response = await getStarAngles(1);
-	expect(response).toBe(0);
+interface Test {
+	input: number;
+	output: number;
+}
+
+const tests: Test[] = [
+	{ input: 3, output: 180 },
+	{ input: 4, output: 360 },
+	{ input: 5, output: 180 },
+	{ input: 6, output: 360 },
+	{ input: 7, output: 180 },
+	{ input: 8, output: 360 },
+	{ input: 9, output: 180 }
+]
+
+test.each(tests)('should return the angles of the star with n sides', async ({ input, output }) => {
+	const response = getStarAngles(input);
+	expect(response).toBe(output);
 });
+
+test('should fail for less then 3 sides', ( ) => {
+	expect(() => {
+		getStarAngles(2)
+	}).toThrow('stars must have at least 3 sides!')
+})
--- rendezvous-with-cassidoo/295/n-pointed-star.ts
+++ rendezvous-with-cassidoo/295/n-pointed-star.ts
@@ -1,3 +1,4 @@
-export const getStarAngles = async (points: number): Promise<number> => {
-	return 0;
+export const getStarAngles = (points: number) => {
+	if (points < 3) throw new Error('stars must have at least 3 sides!');
+	return points % 2 === 0 ? 360 : 180;
 };
