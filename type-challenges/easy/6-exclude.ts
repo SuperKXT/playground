f736907e1fe7ddea9f245ddd726ac08589b490c0
SuperKXT <superkxt@outlook.com>
2023-04-30 16:49:30 +0500

    Add solution for type-challenges/easy#6

---
+++ type-challenges/easy/6-exclude.ts
@@ -0,0 +1,43 @@
+/*
+  43 - Exclude
+  -------
+  by Zheeeng (@zheeeng) #easy #built-in #union
+
+  ### Question
+
+  Implement the built-in Exclude<T, U>
+
+  > Exclude from T those types that are assignable to U
+
+  For example:
+
+  ```ts
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+  ```
+
+  > View on GitHub: https://tsch.js.org/43
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+/* _____________ Test Cases _____________ */
+// eslint-disable-next-line import/first
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type _cases = [
+	Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
+	Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
+	Expect<
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		Equal<MyExclude<string | number | (() => void), Function>, string | number>
+	>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/43/answer
+  > View solutions: https://tsch.js.org/43/solutions
+  > More Challenges: https://tsch.js.org
+*/
