a68941f5e15135c37f28c260e90f9f03a22f4d6f
SuperKXT <superkxt@outlook.com>
2023-05-01 15:51:30 +0500

    Add cover-union-keys type challenge

---
+++ type-challenges/custom/1-cover-union-keys.ts
@@ -0,0 +1,28 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+
+/**
+ * Type that takes a union and adds missing properties to all branches
+ */
+
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+
+type Cover<T, U = T> = Prettify<
+	U extends U
+		? {
+				[K in keyof U]: U[K];
+		  } & {
+				[K in KeysOfUnion<T> as K extends keyof U ? never : K]?: never;
+		  }
+		: never
+>;
+
+type _ = Cover<t>;
+//   ^?
+
+/* _____________ Test Cases _____________ */
+type t = { type: 'first'; first: number } | { type: 'second'; second: number };
+type Covered =
+	| { type: 'first'; first: number; second?: undefined }
+	| { type: 'second'; first?: undefined; second: number };
+
+type _cases = [Expect<Equal<Cover<t>, Covered>>];
