06a3c6f050d16ed1865a1f54d8496858ded62b98
SuperKXT <superkxt@outlook.com>
2023-05-01 14:45:47 +0500

    Add solution for type-challenges/medium#25

---
+++ type-challenges/medium/25-diff.ts
@@ -0,0 +1,54 @@
+/*
+  645 - Diff
+  -------
+  by ZYSzys (@ZYSzys) #medium #object
+
+  ### Question
+
+  Get an `Object` that is the difference between `O` & `O1`
+
+  > View on GitHub: https://tsch.js.org/645
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Diff<
+	O extends Record<string, unknown>,
+	O1 extends Record<string, unknown>
+> = {
+	[K in keyof O | keyof O1 as K extends keyof O & keyof O1
+		? never
+		: K]: K extends keyof O ? O[K] : K extends keyof O1 ? O1[K] : never;
+};
+
+/* _____________ Test Cases _____________ */
+// eslint-disable-next-line import/first
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Foo = {
+	name: string;
+	age: string;
+};
+type Bar = {
+	name: string;
+	age: string;
+	gender: number;
+};
+type Coo = {
+	name: string;
+	gender: number;
+};
+
+type _cases = [
+	Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+	Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+	Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+	Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/645/answer
+  > View solutions: https://tsch.js.org/645/solutions
+  > More Challenges: https://tsch.js.org
+*/
