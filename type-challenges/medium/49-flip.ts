9e3d6b50a46e99779db7a0bc847c8ac4076601a1
SuperKXT <superkxt@outlook.com>
2023-05-02 16:29:21 +0500

    Add solution for type-challenges/medium#49

---
+++ type-challenges/medium/49-flip.ts
@@ -0,0 +1,56 @@
+/*
+  4179 - Flip
+  -------
+  by Farhan Kathawala (@kathawala) #medium #object
+
+  ### Question
+
+  Implement the type of `just-flip-object`. Examples:
+
+  ```typescript
+  Flip<{ a: "x", b: "y", c: "z" }>; // {x: 'a', y: 'b', z: 'c'}
+  Flip<{ a: 1, b: 2, c: 3 }>; // {1: 'a', 2: 'b', 3: 'c'}
+  Flip<{ a: false, b: true }>; // {false: 'a', true: 'b'}
+  ```
+
+  No need to support nested objects and values which cannot be object keys such as arrays
+
+  > View on GitHub: https://tsch.js.org/4179
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Flip<
+	T extends Record<
+		PropertyKey,
+		string | number | bigint | boolean | null | undefined
+	>
+> = {
+	[K in keyof T as `${T[K]}`]: K;
+};
+
+/* _____________ Test Cases _____________ */
+// eslint-disable-next-line import/first
+import type { Equal, Expect, NotEqual } from '@type-challenges/utils';
+
+type _cases = [
+	Expect<Equal<{ a: 'pi' }, Flip<{ pi: 'a' }>>>,
+	Expect<NotEqual<{ b: 'pi' }, Flip<{ pi: 'a' }>>>,
+	Expect<Equal<{ 3.14: 'pi'; true: 'bool' }, Flip<{ pi: 3.14; bool: true }>>>,
+	Expect<
+		Equal<{ val2: 'prop2'; val: 'prop' }, Flip<{ prop: 'val'; prop2: 'val2' }>>
+	>,
+	Expect<
+		Equal<
+			{ null: 'val2'; undefined: 'val'; 134: 'val3' },
+			Flip<{ val2: null; val: undefined; val3: 134n }>
+		>
+	>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4179/answer
+  > View solutions: https://tsch.js.org/4179/solutions
+  > More Challenges: https://tsch.js.org
+*/
