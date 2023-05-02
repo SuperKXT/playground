275299a3cab81cd7456c5cfa22c9b0d766d3f2d7
SuperKXT <superkxt@outlook.com>
2023-05-02 13:19:54 +0500

    Add solution for type-challenges/medium#47

--- type-challenges/medium/46-flatten-depth.ts
+++ type-challenges/medium/46-flatten-depth.ts
@@ -10,7 +10,7 @@
   For example:
 
   ```typescript
-  type a = FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2> // [1, 2, 3, 4, [5]]. flattern 2 times
+  type a = FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2> // [1, 2, 3, 4, [5]]. flatten 2 times
   type b = FlattenDepth<[1, 2, [3, 4], [[[5]]]]> // [1, 2, 3, 4, [[5]]]. Depth defaults to be 1
   ```
 
