fa9c1fddb0b8297aea18800ec168cb1bcb0ae832
SuperKXT <superkxt@outlook.com>
2023-05-05 17:38:40 +0500

    Add `vitest.config.ts`

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-default-export */
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+	plugins: [tsconfigPaths()],
+	test: {
+		globals: true,
+		clearMocks: true,
+		coverage: {
+			enabled: true,
+			reporter: ['text', 'json', 'html'],
+		},
+		environment: 'node',
+	},
+});
