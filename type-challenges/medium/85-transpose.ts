917877da0065ab219da618dcd4661f2603f95205
SuperKXT <superkxt@outlook.com>
2023-05-05 14:09:10 +0500

    Add solution for type-challenges/medium#85

---
+++ type-challenges/medium/85-transpose.ts
@@ -0,0 +1,55 @@
+/*
+  25270 - Transpose
+  -------
+  by Apollo Wayne (@Shinerising) #medium #array #math
+
+  ### Question
+
+  The transpose of a matrix is an operator which flips a matrix over its diagonal; that is, it switches the row and column indices of the matrix A by producing another matrix, often denoted by A<sup>T</sup>.
+
+  ```ts
+  type Matrix = Transpose <[[1]]>; // expected to be [[1]]
+  type Matrix1 = Transpose <[[1, 2], [3, 4]]>; // expected to be [[1, 3], [2, 4]]
+  type Matrix2 = Transpose <[[1, 2, 3], [4, 5, 6]]>; // expected to be [[1, 4], [2, 5], [3, 6]]
+  ```
+
+  > View on GitHub: https://tsch.js.org/25270
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// TODO RETRY
+type Transpose<
+	T extends number[][],
+	First extends number[] = T['length'] extends 0 ? [] : T[0]
+> = {
+	[K in keyof First]: {
+		[K2 in keyof T]: K extends keyof T[K2] ? T[K2][K] : never;
+	};
+};
+
+/* _____________ Test Cases _____________ */
+// eslint-disable-next-line import/first
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type _cases = [
+	Expect<Equal<Transpose<[]>, []>>,
+	Expect<Equal<Transpose<[[1]]>, [[1]]>>,
+	Expect<Equal<Transpose<[[1, 2]]>, [[1], [2]]>>,
+	Expect<Equal<Transpose<[[1, 2], [3, 4]]>, [[1, 3], [2, 4]]>>,
+	Expect<Equal<Transpose<[[1, 2, 3], [4, 5, 6]]>, [[1, 4], [2, 5], [3, 6]]>>,
+	Expect<Equal<Transpose<[[1, 4], [2, 5], [3, 6]]>, [[1, 2, 3], [4, 5, 6]]>>,
+	Expect<
+		Equal<
+			Transpose<[[1, 2, 3], [4, 5, 6], [7, 8, 9]]>,
+			[[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+		>
+	>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/25270/answer
+  > View solutions: https://tsch.js.org/25270/solutions
+  > More Challenges: https://tsch.js.org
+*/
