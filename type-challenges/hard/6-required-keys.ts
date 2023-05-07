4efd89600e659abd51f98deca48ce3de1ac409ce
SuperKXT <superkxt@outlook.com>
2023-05-07 18:16:18 +0500

    Add solution for type-challenges/hard#6

---
+++ type-challenges/hard/6-required-keys.ts
@@ -0,0 +1,57 @@
+/*
+  89 - Required Keys
+  -------
+  by yituan (@yi-tuan) #hard #utils
+
+  ### Question
+
+  Implement the advanced util type `RequiredKeys<T>`, which picks all the required keys into a union.
+
+  For example
+
+  ```ts
+  type Result = RequiredKeys<{ foo: number; bar?: string }>;
+  // expected to be “foo”
+  ```
+
+  > View on GitHub: https://tsch.js.org/89
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type RequiredKeys<
+	T extends object,
+	R extends Required<T> = Required<T>
+> = keyof {
+	[K in keyof T as T[K] extends R[K] ? K : never]: T[K];
+};
+
+type _RequiredKeys<
+	T extends object,
+	R extends Required<T> = Required<T>,
+	K extends keyof T = keyof T
+> = K extends K ? (T[K] extends R[K] ? K : never) : never;
+
+/* _____________ Test Cases _____________ */
+// eslint-disable-next-line import/first
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type _cases = [
+	Expect<Equal<RequiredKeys<{ a: number; b?: string }>, 'a'>>,
+	Expect<Equal<RequiredKeys<{ a: undefined; b?: undefined }>, 'a'>>,
+	Expect<
+		Equal<
+			RequiredKeys<{ a: undefined; b?: undefined; c: string; d: null }>,
+			'a' | 'c' | 'd'
+		>
+	>,
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	Expect<Equal<RequiredKeys<{}>, never>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/89/answer
+  > View solutions: https://tsch.js.org/89/solutions
+  > More Challenges: https://tsch.js.org
+*/
