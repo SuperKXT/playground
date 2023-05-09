17eeadddf30380bc7cd3af690e4e16f603b05cdf
SuperKXT <superkxt@outlook.com>
2023-05-09 16:04:42 +0500

    Update solution for type-challenges/hard#39

---
+++ type-challenges/hard/39-replace-union.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/*
+  13580 - Replace Union
+  -------
+  by Konstantin Barabanov (@crutch12) #hard
+
+  ### Question
+
+  Given an `union of types` and `array of type pairs` to replace (`[[string, number], [Date, null]]`), return a new union replaced with the `type pairs`.
+
+  > View on GitHub: https://tsch.js.org/13580
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type UnionReplace<T, U extends readonly [any, any][]> = U extends [
+	infer F extends [any, any],
+	...infer Rest extends [any, any][]
+]
+	? UnionReplace<F[0] extends T ? Exclude<T, F[0]> | F[1] : T, Rest>
+	: T;
+
+/* _____________ Test Cases _____________ */
+// eslint-disable-next-line import/first
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type _cases = [
+	// string -> null
+	Expect<Equal<UnionReplace<number | string, [[string, null]]>, number | null>>,
+
+	// Date -> string; Function -> undefined
+	Expect<
+		Equal<
+			UnionReplace<
+				Function | Date | object,
+				[[Date, string], [Function, undefined]]
+			>,
+			undefined | string | object
+		>
+	>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/13580/answer
+  > View solutions: https://tsch.js.org/13580/solutions
+  > More Challenges: https://tsch.js.org
+*/
