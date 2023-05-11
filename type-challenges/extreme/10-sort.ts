2c2cdf217b29329e80d46cfdd67051a19e3a0674
SuperKXT <superkxt@outlook.com>
2023-05-11 17:32:36 +0500

    Solve type-challenge/extreme#10

--- type-challenges/extreme/10-sort.ts
+++ type-challenges/extreme/10-sort.ts
@@ -30,7 +30,84 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Sort = any;
+type Fill<T extends number, A extends number[] = []> = A['length'] extends T
+	? A
+	: Fill<T, [...A, 1]>;
+
+type Shift<T extends number[]> = T extends [any, ...infer R extends number[]]
+	? R
+	: never;
+
+type NumberToArray<
+	T extends number,
+	R extends string = `${T}`,
+	A extends number[] = []
+> = R extends `${infer F extends number}${infer L}`
+	? NumberToArray<T, L, [...A, F]>
+	: A;
+
+type CompareDigitDigits<
+	T extends number[],
+	U extends number[],
+	TF extends number[] = Fill<T[0]>,
+	UF extends number[] = Fill<U[0]>
+> = T['length'] extends 0
+	? 'same'
+	: T[0] extends U[0]
+	? CompareDigitDigits<Shift<T>, Shift<U>>
+	: UF[TF['length']] extends undefined
+	? 'greater'
+	: 'lesser';
+
+export type CompareNum<
+	T extends number,
+	U extends number,
+	TA extends number[] = NumberToArray<T>,
+	UA extends number[] = NumberToArray<U>
+> = T extends U
+	? 'equal'
+	: TA['length'] extends UA['length']
+	? CompareDigitDigits<TA, UA>
+	: UA[TA['length']] extends undefined
+	? 'greater'
+	: 'lesser';
+
+type SmallestOrGreatest<
+	Union extends number,
+	IsGreatest extends boolean,
+	V extends number = Union
+> = Union extends Union
+	? [
+			V extends V
+				? CompareNum<V, Union> extends (
+						IsGreatest extends true ? 'greater' : 'lesser'
+				  )
+					? V
+					: never
+				: never
+	  ] extends [never]
+		? Union
+		: never
+	: never;
+
+type Without<T extends number[], U extends number> = T extends [
+	infer F extends number,
+	...infer R extends number[]
+]
+	? F extends U
+		? R
+		: Without<[...R, F], U>
+	: never;
+
+type Sort<
+	T extends number[],
+	Desc extends boolean = false,
+	Sorted extends number[] = [],
+	Union extends number = T[number],
+	Current extends number = SmallestOrGreatest<Union, Desc>
+> = [Union] extends [never]
+	? Sorted
+	: Sort<Without<T, Current>, Desc, [...Sorted, Current]>;
 
 /* _____________ Test Cases _____________ */
 // eslint-disable-next-line import/first
--- type-challenges/extreme/7-multiply.ts
+++ type-challenges/extreme/7-multiply.ts
@@ -36,8 +36,6 @@ type Multiply<
 	? `${Result}`
 	: Multiply<A, B, Times, [...Idx, 1], Sum<Result, A>>;
 
-// TODO Improve this to pass the longer tests
-
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils';
 import type { Sum, UnsignedInt } from './6-sum';
@@ -48,11 +46,11 @@ type _cases = [
 	Expect<Equal<Multiply<'4', 10>, '40'>>,
 	Expect<Equal<Multiply<0, 16>, '0'>>,
 	Expect<Equal<Multiply<'13', '21'>, '273'>>,
-	// // Expect<Equal<Multiply<'43423', 321543n>, '13962361689'>>,
+	Expect<Equal<Multiply<'43423', 321543n>, '13962361689'>>,
 	Expect<Equal<Multiply<9999, 1>, '9999'>>,
-	// // Expect<Equal<Multiply<4325234, '39532'>, '170985150488'>>,
+	Expect<Equal<Multiply<4325234, '39532'>, '170985150488'>>,
 	Expect<Equal<Multiply<100_000n, '1'>, '100000'>>,
-	// // Expect<Equal<Multiply<259, 9125385>, '2363474715'>>,
+	Expect<Equal<Multiply<259, 9125385>, '2363474715'>>,
 	Expect<Equal<Multiply<9, 99>, '891'>>,
 	Expect<Equal<Multiply<315, '100'>, '31500'>>,
 	Expect<Equal<Multiply<11n, 13n>, '143'>>,
