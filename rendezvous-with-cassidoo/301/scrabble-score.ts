2524b68eb3406718a3cb964c44ce59678bb4b567
SuperKXT <superkxt@outlook.com>
2023-05-28 19:59:07 +0500

    Implement rwc#301 on the type level

--- rendezvous-with-cassidoo/301/scrabble-score.ts
+++ rendezvous-with-cassidoo/301/scrabble-score.ts
@@ -1,3 +1,39 @@
+/** cSpell: disable */
+type Tuple<
+	T extends number,
+	Result extends 1[] = []
+> = Result['length'] extends T ? Result : Tuple<T, [...Result, 1]>;
+
+type ScrabbleKey = {
+	EAIONRTLSU: 1;
+	DG: 2;
+	BCMP: 3;
+	FHVWY: 4;
+	K: 5;
+	JX: 8;
+	QZ: 10;
+};
+
+type ScrabbleKeyScore<
+	T extends string,
+	Keys extends keyof ScrabbleKey = keyof ScrabbleKey
+> = Keys extends Keys
+	? Keys extends `${any}${T}${any}`
+		? ScrabbleKey[Keys]
+		: never
+	: never;
+
+type Scrabble<
+	T extends string,
+	Score extends any[] = []
+> = T extends `${infer First}${infer Rest}`
+	? ScrabbleKeyScore<First> extends infer N extends number
+		? [N] extends [never]
+			? `Invalid Character ${First}`
+			: Scrabble<Rest, [...Score, ...Tuple<N>]>
+		: `Invalid Character ${First}`
+	: Score['length'];
+
 const onePoints = ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U'];
 const twoPoints = ['D', 'G'];
 const threePoints = ['B', 'C', 'M', 'P'];
@@ -6,10 +42,9 @@ const fivePoints = ['K'];
 const eightPoints = ['J', 'X'];
 const tenPoints = ['Q', 'Z'];
 
-export const scrabbleScore = (input: string): number => {
+export const scrabbleScore = <T extends string>(input: T): Scrabble<T> => {
 	let score = 0;
-	for (const curr of input.split('')) {
-		const letter = curr.toUpperCase();
+	for (const letter of input.toUpperCase().split('')) {
 		if (onePoints.includes(letter)) score += 1;
 		else if (twoPoints.includes(letter)) score += 2;
 		else if (threePoints.includes(letter)) score += 3;
@@ -19,5 +54,5 @@ export const scrabbleScore = (input: string): number => {
 		else if (tenPoints.includes(letter)) score += 10;
 		else throw new Error('invalid letter');
 	}
-	return score;
+	return score as never;
 };
