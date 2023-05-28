adec937cf99d7879b563df92b178e973f2cf2159
SuperKXT <superkxt@outlook.com>
2023-05-28 19:24:07 +0500

    Add solution for rwc#301

--- rendezvous-with-cassidoo/301/scrabble-score.test.ts
+++ rendezvous-with-cassidoo/301/scrabble-score.test.ts
@@ -1,6 +1,9 @@
 import { scrabbleScore } from './scrabble-score';
 
 test('should test scrabbleScore', () => {
-	const response = scrabbleScore('FIZZBUZZ');
-	expect(response).toBe(49);
+	const response1 = scrabbleScore('FIZZBUZZ');
+	expect(response1).toBe(49);
+
+	const response2 = scrabbleScore('EDBFKJQ');
+	expect(response2).toBe(33);
 });
--- rendezvous-with-cassidoo/301/scrabble-score.ts
+++ rendezvous-with-cassidoo/301/scrabble-score.ts
@@ -1,3 +1,23 @@
+const onePoints = ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U'];
+const twoPoints = ['D', 'G'];
+const threePoints = ['B', 'C', 'M', 'P'];
+const fourPoints = ['F', 'H', 'V', 'W', 'Y'];
+const fivePoints = ['K'];
+const eightPoints = ['J', 'X'];
+const tenPoints = ['Q', 'Z'];
+
 export const scrabbleScore = (input: string): number => {
-	return 0;
+	let score = 0;
+	for (const curr of input.split('')) {
+		const letter = curr.toUpperCase();
+		if (onePoints.includes(letter)) score += 1;
+		else if (twoPoints.includes(letter)) score += 2;
+		else if (threePoints.includes(letter)) score += 3;
+		else if (fourPoints.includes(letter)) score += 4;
+		else if (fivePoints.includes(letter)) score += 5;
+		else if (eightPoints.includes(letter)) score += 8;
+		else if (tenPoints.includes(letter)) score += 10;
+		else throw new Error('invalid letter');
+	}
+	return score;
 };
