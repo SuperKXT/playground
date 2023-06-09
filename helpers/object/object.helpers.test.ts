6aa2ebfa9ece4a7cc503cea30156de19730d3b7c
SuperKXT <superkxt@outlook.com>
2023-06-09 19:01:33 +0500

    Add `deepMerge` object helper

--- helpers/object/object.helpers.test.ts
+++ helpers/object/object.helpers.test.ts
@@ -1,4 +1,5 @@
 import {
+	deepMerge,
 	objectEntries,
 	objectKeys,
 	objectValues,
@@ -6,66 +7,72 @@ import {
 	pick,
 } from './object.helpers';
 
-describe('testing objectEntries', () => {
-	it('should return the correct value and types for object entries', () => {
-		const object = { first: 1, second: 2 } as const;
-		const entries = objectEntries(object);
-		expect(entries).toStrictEqual([
-			['first', 1],
-			['second', 2],
-		]);
-		assertType<['first' | 'second', 1 | 2][]>(entries);
-	});
+test('testing objectEntries', () => {
+	const object = { first: 1, second: 2 } as const;
+	const entries = objectEntries(object);
+	expect(entries).toStrictEqual([
+		['first', 1],
+		['second', 2],
+	]);
+	assertType<['first' | 'second', 1 | 2][]>(entries);
 });
 
-describe('testing objectKeys', () => {
-	it('should return the correct value and types for object keys', () => {
-		const object = { first: 1, second: 2 } as const;
-		const keys = objectKeys(object);
-		expect(keys).toStrictEqual(['first', 'second']);
-		assertType<('first' | 'second')[]>(keys);
-	});
+test('testing objectKeys', () => {
+	const object = { first: 1, second: 2 } as const;
+	const keys = objectKeys(object);
+	expect(keys).toStrictEqual(['first', 'second']);
+	assertType<('first' | 'second')[]>(keys);
 });
 
-describe('testing objectValues', () => {
-	it('should return the correct value and types for object values', () => {
-		const object = { first: 1, second: 2 } as const;
-		const values = objectValues(object);
-		expect(values).toStrictEqual([1, 2]);
-		assertType<(1 | 2)[]>(values);
-	});
+test('testing objectValues', () => {
+	const object = { first: 1, second: 2 } as const;
+	const values = objectValues(object);
+	expect(values).toStrictEqual([1, 2]);
+	assertType<(1 | 2)[]>(values);
 });
 
-describe('testing omit', () => {
-	it('should remove a single key from object', () => {
-		const omitted = { first: 1 };
-		const object = { ...omitted, second: 2 };
-		const result = omit(object, 'second');
-		expect(result).toStrictEqual(omitted);
-		assertType<typeof omitted>(result);
-	});
-	it('should remove multiple keys from the object', () => {
-		const omitted = { first: 1 };
-		const object = { ...omitted, second: 2, third: 3 };
-		const result = omit(object, ['second', 'third']);
-		expect(result).toStrictEqual(omitted);
-		assertType<typeof omitted>(result);
-	});
+test('testing omit with a single key', () => {
+	const omitted = { first: 1 };
+	const object = { ...omitted, second: 2 };
+	const result = omit(object, 'second');
+	expect(result).toStrictEqual(omitted);
+	assertType<typeof omitted>(result);
 });
 
-describe('testing pick', () => {
-	it('should pick a single key from object', () => {
-		const picked = { a: 1 };
-		const object = { ...picked, b: 2 };
-		const result = pick(object, 'a');
-		expect(result).toStrictEqual(picked);
-		assertType<typeof picked>(result);
-	});
-	it('should pick multiple keys from the object', () => {
-		const picked = { a: 1, b: 2 };
-		const object = { ...picked, c: 3 };
-		const result = pick(object, ['a', 'b']);
-		expect(result).toStrictEqual(picked);
-		assertType<typeof picked>(result);
-	});
+test('testing omit with multiple keys', () => {
+	const omitted = { first: 1 };
+	const object = { ...omitted, second: 2, third: 3 };
+	const result = omit(object, ['second', 'third']);
+	expect(result).toStrictEqual(omitted);
+	assertType<typeof omitted>(result);
+});
+
+test('testing pick with a single key', () => {
+	const picked = { a: 1 };
+	const object = { ...picked, b: 2 };
+	const result = pick(object, 'a');
+	expect(result).toStrictEqual(picked);
+	assertType<typeof picked>(result);
+});
+
+test('testing pick with multiple keys', () => {
+	const picked = { a: 1, b: 2 };
+	const object = { ...picked, c: 3 };
+	const result = pick(object, ['a', 'b']);
+	expect(result).toStrictEqual(picked);
+	assertType<typeof picked>(result);
+});
+
+test('testing deepMerge', () => {
+	const first = { a: 1, b: 'old', nested: { d: 1, e: 'old' } } as const;
+	const second = { b: 'new', c: 3, nested: { e: 'new', f: 3 } } as const;
+	const merged = {
+		a: 1 as const,
+		b: 'new' as const,
+		c: 3 as const,
+		nested: { d: 1 as const, e: 'new' as const, f: 3 as const },
+	};
+	const result = deepMerge(first, second);
+	expect(result).toStrictEqual(merged);
+	assertType<typeof merged>(result);
 });
--- helpers/object/object.helpers.ts
+++ helpers/object/object.helpers.ts
@@ -1,39 +1,56 @@
-import type { Utils } from '~/types/utils';
+import { isObject } from 'helpers/type';
 
-export const objectEntries = <Keys extends PropertyKey, Type>(
-	object: Record<Keys, Type>
-) => Object.entries(object) as [Keys, Type][];
+import type { Utils } from 'types/utils';
 
-export const objectKeys = <Keys extends PropertyKey, Type>(
-	object: Record<Keys, Type>
-) => Object.keys(object) as Keys[];
-
-export const objectValues = <Keys extends PropertyKey, Type>(
-	object: Record<Keys, Type>
-) => Object.values<Type>(object);
-
-export const omit = <
-	Type extends Record<string, unknown>,
-	Key extends keyof Type
->(
-	object: Type,
-	keys: Key | Key[]
-): Utils.prettify<Omit<Type, Key>> => {
-	const keySet = new Set<PropertyKey>(Array.isArray(keys) ? keys : [keys]);
-	return Object.fromEntries(
-		Object.entries(object).filter(([key]) => !keySet.has(key))
-	) as Utils.prettify<Omit<Type, Key>>;
+export const objectEntries = <T extends Obj>(
+	object: T
+): [keyof T, T[keyof T]][] => {
+	return Object.entries(object) as never;
 };
 
-export const pick = <
-	Type extends Record<string, unknown>,
-	Key extends keyof Type
->(
+export const objectKeys = <T extends Obj>(object: T): (keyof T)[] => {
+	return Object.keys(object);
+};
+
+export const objectValues = <T extends Obj>(object: T): T[keyof T][] => {
+	return Object.values(object) as never;
+};
+
+export const omit = <Type extends Obj, ToOmit extends keyof Type>(
 	object: Type,
-	keys: Key | Key[]
-): Utils.prettify<Pick<Type, Key>> => {
-	const keySet = new Set<PropertyKey>(Array.isArray(keys) ? keys : [keys]);
-	return Object.fromEntries(
-		Object.entries(object).filter(([key]) => keySet.has(key))
-	) as Utils.prettify<Pick<Type, Key>>;
+	keys: ToOmit | ToOmit[]
+): Utils.prettify<Omit<Type, ToOmit>> => {
+	const keyArray = Array.isArray(keys) ? keys : [keys];
+	return objectEntries(object).reduce((obj, [key, value]) => {
+		if (keyArray.includes(key as never)) return obj;
+		return { ...obj, [key]: value };
+	}, {}) as never;
+};
+
+export const pick = <Type extends Obj, ToPick extends keyof Type>(
+	object: Type,
+	keys: ToPick | ToPick[]
+): Utils.prettify<Pick<Type, ToPick>> => {
+	const keyArray = Array.isArray(keys) ? keys : [keys];
+	return objectEntries(object).reduce((obj, [key, value]) => {
+		if (!keyArray.includes(key as never)) return obj;
+		return { ...obj, [key]: value };
+	}, {}) as never;
+};
+
+export const deepMerge = <T extends Obj, U extends Obj>(
+	first: T,
+	second: U
+): Utils.deepMerge<T, U> => {
+	const commonKeys = { ...first, ...second };
+	const merged: Record<string, unknown> = {};
+	for (const key of Object.keys(commonKeys)) {
+		const firstCurr = first[key];
+		const secondCurr = second[key];
+		merged[key] =
+			isObject(firstCurr) && isObject(secondCurr)
+				? deepMerge(firstCurr, secondCurr)
+				: secondCurr ?? firstCurr;
+	}
+	return merged as never;
 };
