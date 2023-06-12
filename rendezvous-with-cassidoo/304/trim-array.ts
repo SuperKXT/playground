2a111e01fdf467e508bc1806da01948a8bae7ceb
SuperKXT <superkxt@outlook.com>
2023-06-12 11:05:31 +0500

    Tweak `trimArray`

--- rendezvous-with-cassidoo/304/trim-array.ts
+++ rendezvous-with-cassidoo/304/trim-array.ts
@@ -3,7 +3,7 @@ type tuple<T extends number, R extends any[] = []> = R['length'] extends T
 	: tuple<T, [...R, any]>;
 
 type TrimArray<
-	Arr extends readonly Readonly<number>[],
+	Arr extends readonly number[],
 	Start extends number,
 	End extends number
 > = Arr extends readonly [...tuple<Start>, ...infer Rest, ...tuple<End>]
