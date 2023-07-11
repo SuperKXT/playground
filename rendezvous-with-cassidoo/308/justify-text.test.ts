593dacabdcae6fb0b31fd89c7475838a21f5588f
SuperKXT <superkxt@outlook.com>
2023-07-11 15:20:39 +0500

    Add typescript implementation of `rwc#308`

--- rendezvous-with-cassidoo/308/justify-text.test.ts
+++ rendezvous-with-cassidoo/308/justify-text.test.ts
@@ -5,21 +5,25 @@ test('testing reversedSquares against test 1', () => {
 		['This', 'is', 'an', 'example', 'of', 'text', 'justification.'],
 		16
 	);
-	const expected = ['This    is    an', 'example  of text', 'justification.  '];
+	const expected = [
+		'This    is    an',
+		'example  of text',
+		'justification.  ',
+	] as const;
 	expect(result).toStrictEqual(expected);
 	assertType<typeof result>(expected);
 });
 
 test('testing reversedSquares against test 2', () => {
 	const result = justifyText(['something', 'is', 'off', 'with', 'this'], 4);
-	const expected = ['something', 'is  ', 'off ', 'with', 'this'];
+	const expected = ['something', 'is  ', 'off ', 'with', 'this'] as const;
 	expect(result).toStrictEqual(expected);
 	assertType<typeof result>(expected);
 });
 
 test('testing reversedSquares against test 3', () => {
 	const result = justifyText(['something', 'is', 'off', 'with', 'this'], 10);
-	const expected = ['something ', 'is     off', 'with  this'];
+	const expected = ['something ', 'is     off', 'with  this'] as const;
 	expect(result).toStrictEqual(expected);
 	assertType<typeof result>(expected);
 });
