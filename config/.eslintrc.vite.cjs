809e694d0b0dc04c29936aafc96cb4cfbea8f324
SuperKXT <superkxt@outlook.com>
2023-07-16 18:34:14 +0500

    Update `config/.eslintrc.vite.cjs`

--- config/.eslintrc.vite.cjs
+++ config/.eslintrc.vite.cjs
@@ -2,19 +2,26 @@
 const config = {
 	env: {
 		es2021: true,
-		node: true,
 		browser: true,
 	},
-	extends: ['eslint:recommended', 'prettier', 'plugin:import/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
+		'prettier',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+	],
+	plugins: ['import', 'unused-imports', '@typescript-eslint'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
+		project: true,
+		tsconfigRootDir: __dirname,
 	},
-	plugins: ['import', 'unused-imports'],
 	settings: {
 		'import/resolver': {
-			node: true,
 			typescript: true,
 		},
 	},
@@ -35,7 +42,6 @@ const config = {
 		'no-alert': 'warn',
 		'no-bitwise': 'warn',
 		'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
-		'no-constant-condition': ['warn', { checkLoops: false }],
 		'no-constructor-return': 'warn',
 		'no-else-return': ['warn', { allowElseIf: false }],
 		'no-extend-native': 'warn',
@@ -141,113 +147,92 @@ const config = {
 				destructuredArrayIgnorePattern: '^_',
 			},
 		],
+
+		'@typescript-eslint/consistent-type-exports': 'warn',
+		'@typescript-eslint/consistent-type-imports': 'warn',
+		'@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+		'@typescript-eslint/default-param-last': 'warn',
+		'no-dupe-class-members': 'off',
+		'@typescript-eslint/no-dupe-class-members': 'warn',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-floating-promises': 'off',
+		'@typescript-eslint/no-inferrable-types': 'off',
+		'@typescript-eslint/no-loop-func': 'warn',
+		'@typescript-eslint/no-misused-promises': [
+			'warn',
+			{
+				checksVoidReturn: false,
+			},
+		],
+		'@typescript-eslint/no-redundant-type-constituents': 'warn',
+		'@typescript-eslint/no-shadow': 'warn',
+		'no-constant-condition': 'off',
+		'@typescript-eslint/no-unnecessary-condition': [
+			'warn',
+			{ allowConstantLoopConditions: true },
+		],
+		'@typescript-eslint/no-unused-expressions': [
+			'error',
+			{
+				allowShortCircuit: true,
+				allowTernary: true,
+				enforceForJSX: true,
+			},
+		],
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-use-before-define': 'warn',
+		'@typescript-eslint/no-useless-empty-export': 'warn',
+		'@typescript-eslint/non-nullable-type-assertion-style': 'off',
+		'@typescript-eslint/prefer-nullish-coalescing': 'off',
+		'@typescript-eslint/prefer-reduce-type-parameter': 'off',
+		'@typescript-eslint/no-invalid-void-type': 'off',
+		'@typescript-eslint/promise-function-async': 'warn',
+		'@typescript-eslint/require-array-sort-compare': [
+			'warn',
+			{ ignoreStringArrays: true },
+		],
+		'@typescript-eslint/return-await': 'warn',
+		'@typescript-eslint/switch-exhaustiveness-check': 'warn',
+		'@typescript-eslint/restrict-template-expressions': [
+			'warn',
+			{ allowAny: true },
+		],
+		'@typescript-eslint/ban-types': [
+			'warn',
+			{
+				types: {
+					'{}': false,
+					extendDefaults: true,
+				},
+			},
+		],
+		'@typescript-eslint/no-namespace': ['warn', { allowDeclarations: true }],
 	},
 	overrides: [
 		{
-			extends: [
-				'plugin:@typescript-eslint/recommended',
-				'plugin:@typescript-eslint/recommended-requiring-type-checking',
-				'plugin:@typescript-eslint/strict',
-				'plugin:import/typescript',
-			],
-			files: ['**/*.ts', '**/*.tsx'],
-			parserOptions: {
-				project: './tsconfig.json',
-			},
-			plugins: ['@typescript-eslint'],
-			rules: {
-				'@typescript-eslint/consistent-type-exports': 'warn',
-				'@typescript-eslint/consistent-type-imports': 'warn',
-				'@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
-				'@typescript-eslint/default-param-last': 'warn',
-				'no-dupe-class-members': 'off',
-				'@typescript-eslint/no-dupe-class-members': 'warn',
-				'@typescript-eslint/no-explicit-any': 'off',
-				'@typescript-eslint/no-floating-promises': 'off',
-				'@typescript-eslint/no-inferrable-types': 'off',
-				'@typescript-eslint/no-loop-func': 'warn',
-				'@typescript-eslint/no-misused-promises': [
-					'warn',
-					{
-						checksVoidReturn: false,
-					},
-				],
-				'@typescript-eslint/no-redundant-type-constituents': 'warn',
-				'@typescript-eslint/no-shadow': 'warn',
-				'no-constant-condition': 'off',
-				'@typescript-eslint/no-unnecessary-condition': [
-					'warn',
-					{ allowConstantLoopConditions: true },
-				],
-				'@typescript-eslint/no-unused-expressions': [
-					'error',
-					{
-						allowShortCircuit: true,
-						allowTernary: true,
-						enforceForJSX: true,
-					},
-				],
-				'@typescript-eslint/no-unused-vars': 'off',
-				'@typescript-eslint/no-use-before-define': 'warn',
-				'@typescript-eslint/no-useless-empty-export': 'warn',
-				'@typescript-eslint/non-nullable-type-assertion-style': 'off',
-				'@typescript-eslint/prefer-nullish-coalescing': 'off',
-				'@typescript-eslint/prefer-reduce-type-parameter': 'off',
-				'@typescript-eslint/no-invalid-void-type': 'off',
-				'@typescript-eslint/promise-function-async': 'warn',
-				'@typescript-eslint/require-array-sort-compare': [
-					'warn',
-					{
-						ignoreStringArrays: true,
-					},
-				],
-				'@typescript-eslint/return-await': 'warn',
-				'@typescript-eslint/switch-exhaustiveness-check': 'warn',
-				'@typescript-eslint/restrict-template-expressions': [
-					'warn',
-					{ allowAny: true },
-				],
-				'@typescript-eslint/ban-types': [
-					'warn',
-					{
-						types: {
-							'{}': false,
-							extendDefaults: true,
-						},
-					},
-				],
-				'@typescript-eslint/no-namespace': [
-					'warn',
-					{ allowDeclarations: true },
-				],
-
-				// TODO these are turned off because eslint is slow and incorrectly keeps inferring types as any
-				'@typescript-eslint/no-unsafe-argument': 'off',
-				'@typescript-eslint/no-unsafe-assignment': 'off',
-				'@typescript-eslint/no-unsafe-call': 'off',
-				'@typescript-eslint/no-unsafe-member-access': 'off',
-				'@typescript-eslint/no-unsafe-return': 'off',
-			},
-		},
-		{
 			files: ['**/*.tsx', '**/*.jsx'],
 			extends: [
-				'react-app',
 				'plugin:react/recommended',
 				'plugin:react/jsx-runtime',
+				'plugin:react-hooks/recommended',
+				'plugin:jsx-a11y/recommended',
 			],
 			plugins: ['jsx-expressions'],
+			settings: {
+				react: {
+					version: 'detect',
+				},
+			},
 			rules: {
 				'@typescript-eslint/no-unnecessary-type-constraint': 'off',
+				'@typescript-eslint/no-unused-vars': 'off',
 				'react/jsx-filename-extension': [
 					'warn',
 					{ extensions: ['jsx', 'tsx'] },
 				],
-				'@typescript-eslint/no-unused-vars': 'off',
 				'react/jsx-fragments': 'warn',
 				'react/jsx-max-depth': ['warn', { max: 6 }],
 				'react/jsx-no-constructed-context-values': 'warn',
-				'jsx-expressions/strict-logical-expressions': 'warn',
 				'react/jsx-no-script-url': 'warn',
 				'react/jsx-no-useless-fragment': ['warn', { allowExpressions: true }],
 				'react/jsx-sort-props': [
@@ -268,13 +253,11 @@ const config = {
 				'react/self-closing-comp': 'warn',
 				'react/void-dom-elements-no-children': 'warn',
 				'react/prop-types': 'off',
+				'jsx-expressions/strict-logical-expressions': 'warn',
 			},
 		},
 		{
 			files: ['*.test.ts'],
-			parserOptions: {
-				project: ['./tsconfig.json'],
-			},
 			extends: ['plugin:vitest/all', 'plugin:testing-library/react'],
 			plugins: ['vitest', 'testing-library'],
 			rules: {
@@ -299,6 +282,17 @@ const config = {
 		},
 		{
 			files: ['scripts/**/*'],
+			rules: {
+				'import/no-nodejs-modules': 'off',
+			},
+		},
+		{
+			files: ['**/*'],
+			excludedFiles: ['src/**/*'],
+			env: {
+				es2021: true,
+				node: true,
+			},
 			rules: {
 				'import/no-nodejs-modules': 'off',
 			},
