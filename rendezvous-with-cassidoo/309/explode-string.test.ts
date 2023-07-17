0a9a46ff34b0ffaa141709eb515e47ec50dcb5eb
SuperKXT <superkxt@outlook.com>
2023-07-17 10:21:56 +0500

    Add `rwc#309` solution

---
+++ rendezvous-with-cassidoo/309/explode-string.test.ts
@@ -0,0 +1,15 @@
+import { explodeString } from './explode-string';
+
+test('testing explodeString against test 1', () => {
+	const result = explodeString('Ahh, abracadabra!');
+	const expected = ['!', ',', 'A', 'aaaaa', 'bb', 'c', 'd', 'hh', 'rr'];
+	expect(result).toStrictEqual(expected);
+	assertType<typeof result>(expected);
+});
+
+test('testing explodeString against test 2', () => {
+	const result = explodeString('o/o/');
+	const expected = ['//', '\\', 'oo'];
+	expect(result).toStrictEqual(expected);
+	assertType<typeof result>(expected);
+});
