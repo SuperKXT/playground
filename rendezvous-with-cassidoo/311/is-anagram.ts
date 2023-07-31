26d41c3e31b8998d64c9e3d32987358d0ff98629
SuperKXT <superkxt@outlook.com>
2023-07-31 10:47:42 +0500

    Add typescript solution for `rwc#311`

--- rendezvous-with-cassidoo/311/is-anagram.ts
+++ rendezvous-with-cassidoo/311/is-anagram.ts
@@ -1,9 +1,20 @@
+type IsAnagram<
+	T extends string,
+	U extends string,
+> = T extends `${infer first}${infer rest}`
+	? U extends `${infer before}${first}${infer after}`
+		? IsAnagram<rest, `${before}${after}`>
+		: false
+	: U extends ''
+	? true
+	: false;
+
 export const isAnagram = <T extends string, U extends string>(
 	first: T,
 	second: U,
-): boolean => {
-	if (first.length !== second.length) return false;
-	const firstArray = Array.from(first.toLowerCase()).sort();
-	const secondArray = Array.from(second.toLowerCase()).sort();
-	return firstArray.join('') === secondArray.join('');
+): IsAnagram<T, U> => {
+	if (first.length !== second.length) return false as never;
+	const firstArray = first.toLowerCase().split('').sort();
+	const secondArray = second.toLowerCase().split('').sort();
+	return (firstArray.join('') === secondArray.join('')) as never;
 };
