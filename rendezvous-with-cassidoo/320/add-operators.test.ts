d42897e4bb3937a3cf6f69dafcc491dbe1e88e84
SuperKXT <superkxt@outlook.com>
2023-10-08 22:44:15 +0500

    Add solution for `rwc#320`

--- rendezvous-with-cassidoo/320/add-operators.test.ts
+++ rendezvous-with-cassidoo/320/add-operators.test.ts
@@ -2,7 +2,7 @@ import { addOperators } from './add-operators.js';
 
 test('testing addOperators against test 1', () => {
 	const result = addOperators(123, 6);
-	const expected = ['1*2*3', '1+2+3'];
+	const expected = ['1+2+3', '1*2*3'];
 	expect(result).toStrictEqual(expected);
 	assertType<typeof result>(expected);
 });
--- rendezvous-with-cassidoo/320/add-operators.ts
+++ rendezvous-with-cassidoo/320/add-operators.ts
@@ -1,14 +1,51 @@
+const operators = ['+', '-', '*', '/'] as const;
+
+type Operator = (typeof operators)[number];
+
 export type AddOperators<
 	Source extends number,
 	Target extends number,
 > = string[];
 
-type Result = { result: string; sum: number };
-type ResultCombo = { '+': Result; '-': Result; '*': Result; '/': Result };
+type Result = { string: string; amount: number };
+
+const digitOperate = (digits: string) => {
+	const results: Result[] = [];
+
+	for (const operator of operators) {
+		const result: Result = {
+			string: '',
+			amount: operator === '+' || operator === '-' ? 0 : 1,
+		};
+		for (const digit of digits) {
+			result.string += (result.string ? operator : '') + digit;
+			switch (operator) {
+				case '+':
+					result.amount += Number(digit);
+					break;
+				case '-':
+					result.amount -= Number(digit);
+					break;
+				case '*':
+					result.amount *= Number(digit);
+					break;
+				case '/':
+					result.amount /= Number(digit);
+					break;
+			}
+		}
+		results.push(result);
+	}
+
+	return results;
+};
 
 export const addOperators = <Source extends number, Target extends number>(
 	source: Source,
 	target: Target,
 ): AddOperators<Source, Target> => {
-	return [];
+	const results = digitOperate(source.toString());
+	return results
+		.filter((row) => row.amount === target)
+		.map((row) => row.string);
 };
