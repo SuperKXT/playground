759c82f28725a8167cc555057c479d8562ca5230
SuperKXT <superkxt@outlook.com>
2023-10-16 10:58:03 +0500

    Add ts solution for `rwc#322`

--- rendezvous-with-cassidoo/322/is-isomorphic.ts
+++ rendezvous-with-cassidoo/322/is-isomorphic.ts
@@ -1,14 +1,30 @@
+export type IsIsomorphic<
+	T extends string,
+	U extends string,
+	map extends Record<string, string> = {},
+> = T extends `${infer first}${infer tRest}`
+	? U extends `${infer mapped}${infer uRest}`
+		? map[first] extends string
+			? map[first] extends mapped
+				? IsIsomorphic<tRest, uRest, map & { [x in first]: mapped }>
+				: false
+			: IsIsomorphic<tRest, uRest, map & { [x in first]: mapped }>
+		: false
+	: U extends T
+	? true
+	: false;
+
 export const isIsomorphic = <T extends string, U extends string>(
 	first: T,
 	second: U,
-): boolean => {
-	if (first.length !== second.length) return false;
+): IsIsomorphic<T, U> => {
+	if (first.length !== second.length) return false as never;
 	const map = new Map<string, string>();
 	for (let i = 0; i < first.length; ++i) {
 		const f = first[i] as string;
 		const s = second[i] as string;
-		if (map.has(f) && map.get(f) !== s) return false;
+		if (map.has(f) && map.get(f) !== s) return false as never;
 		map.set(f, s);
 	}
-	return true;
+	return true as never;
 };
