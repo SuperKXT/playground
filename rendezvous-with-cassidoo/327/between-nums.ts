997603633b49626264d5c69c08d26b1c16922f48
SuperKXT <superkxt@outlook.com>
2023-11-20 16:45:58 +0500

    Add ts solution for `rwc#327`

--- rendezvous-with-cassidoo/327/between-nums.ts
+++ rendezvous-with-cassidoo/327/between-nums.ts
@@ -1,5 +1,89 @@
+type tuple<num extends number, tup extends 1[] = []> = tup['length'] extends num
+	? tup
+	: tuple<num, [...tup, 1]>;
+
+type removeN<
+	tup extends 1[],
+	n extends number,
+	idx extends 1[] = [],
+> = idx['length'] extends n
+	? tup
+	: tup extends [1, ...infer rest extends 1[]]
+	  ? removeN<rest, n, [...idx, 1]>
+	  : [];
+
+type divisible<
+	num extends number,
+	by extends number,
+	tup extends 1[] = tuple<num>,
+	idx extends 1[] = [],
+> = tup extends []
+	? true
+	: [...tup, 1][by] extends undefined
+	  ? false
+	  : divisible<never, by, removeN<tup, by>, [...idx, 1]>;
+
+type IsPrime<num extends number, idx extends 1[] = [1, 1]> = num extends 0
+	? false
+	: num extends 1
+	  ? true
+	  : idx['length'] extends num
+	    ? true
+	    : divisible<num, idx['length']> extends true
+	      ? false
+	      : IsPrime<num, [...idx, 1]>;
+
+type IsOdd<
+	num extends number,
+	tup extends unknown[] = tuple<num>,
+> = tup extends [unknown, unknown, ...infer rest]
+	? IsOdd<never, rest>
+	: tup['length'] extends 1
+	  ? true
+	  : false;
+
+type IsEven<num extends number> = IsOdd<num> extends true ? false : true;
+
+type checkNum<
+	num extends number,
+	check extends 'even' | 'odd' | 'prime',
+> = check extends 'even'
+	? IsEven<num>
+	: check extends 'odd'
+	  ? IsOdd<num>
+	  : IsPrime<num>;
+
+type BetweenNums<
+	first extends number,
+	second extends number,
+	check extends 'even' | 'odd' | 'prime',
+	firstTup extends 1[] = tuple<first>,
+	secondTup extends 1[] = tuple<second>,
+	start extends 1[] = firstTup[secondTup['length']] extends 1
+		? secondTup
+		: firstTup,
+	end extends 1[] = firstTup[secondTup['length']] extends 1
+		? firstTup
+		: secondTup,
+	idx extends 1[] = [1],
+	result extends number[] = [],
+	curr extends number = [...start, ...idx]['length'],
+> = curr extends end['length']
+	? result
+	: BetweenNums<
+			never,
+			never,
+			check,
+			never,
+			never,
+			start,
+			end,
+			[...idx, 1],
+			[...result, ...(checkNum<curr, check> extends true ? [curr] : [])]
+	  >;
+
 const isPrime = (num: number) => {
-	for (let i = 2; i < num; i++) if (num % i === 0) return false;
+	for (let i = 2; i < Math.sqrt(num); i += 2) if (num % i === 0) return false;
 	return true;
 };
 
@@ -11,7 +95,7 @@ export const betweenNums = <
 	first: First,
 	second: Second,
 	check: Check,
-): number[] => {
+): BetweenNums<First, Second, Check> => {
 	const result: number[] = [];
 	for (let i = Math.min(first, second) + 1; i < Math.max(first, second); i++) {
 		switch (check) {
@@ -26,5 +110,5 @@ export const betweenNums = <
 				break;
 		}
 	}
-	return result;
+	return result as never;
 };
