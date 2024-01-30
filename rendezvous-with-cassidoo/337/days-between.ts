a26a47eef0d518d1e599a212a6afc2824193115e
SuperKXT <superkxt@outlook.com>
2024-01-30 11:48:03 +0500

    Tweak `rwc#337`

--- rendezvous-with-cassidoo/337/days-between.ts
+++ rendezvous-with-cassidoo/337/days-between.ts
@@ -1,6 +1,4 @@
-type digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-
-const dateRegex = /^\d\d\d\d-\d\d-\d\d$/u;
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/u;
 
 export const daysBetween = <From extends string, To extends string>(
 	from: From,
