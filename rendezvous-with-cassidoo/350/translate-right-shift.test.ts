bf9976b8d7be96fe7ec7e2a628bf8f8c8809e885
SuperKXT <superkxt@outlook.com>
2024-05-15 16:41:22 +0500

    Add `fast-equals`

--- rendezvous-with-cassidoo/350/translate-right-shift.test.ts
+++ rendezvous-with-cassidoo/350/translate-right-shift.test.ts
@@ -1,13 +1,13 @@
 import { translateShiftRight } from './translate-right-shift.js';
 
-test('testing printArrow against test 1', () => {
+test('testing translateShiftRight against test 1', () => {
 	const result = translateShiftRight(';p; epeor'); // cSpell: disable-line
 	const expected = 'lol wowie'; // cSpell: disable-line
 	expect(result).toStrictEqual(expected);
 	assertType<typeof result>(expected);
 });
 
-test('testing printArrow against test 2', () => {
+test('testing translateShiftRight against test 2', () => {
 	const result = translateShiftRight('ejp s, o');
 	const expected = 'who am i';
 	expect(result).toStrictEqual(expected);
