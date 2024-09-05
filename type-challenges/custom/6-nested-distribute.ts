69c848c8e38c1533f76b4c921cc30cca40d59f2c
SuperKXT <superkxt@outlook.com>
2024-09-05 11:41:38 +0500

    Add `nested-distribute` custom type challenge

---
+++ type-challenges/custom/6-nested-distribute.ts
@@ -0,0 +1,77 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type In = {
+	prop1: string | number;
+	prop2: {
+		prop3: boolean | { prop4: 1 | 2 };
+	};
+};
+
+type Out =
+	| {
+			prop1: string;
+			prop2: {
+				prop3: true;
+			};
+	  }
+	| {
+			prop1: string;
+			prop2: {
+				prop3: false;
+			};
+	  }
+	| {
+			prop1: string;
+			prop2: {
+				prop3: { prop4: 1 };
+			};
+	  }
+	| {
+			prop1: string;
+			prop2: {
+				prop3: { prop4: 2 };
+			};
+	  }
+	| {
+			prop1: number;
+			prop2: {
+				prop3: true;
+			};
+	  }
+	| {
+			prop1: number;
+			prop2: {
+				prop3: false;
+			};
+	  }
+	| {
+			prop1: number;
+			prop2: {
+				prop3: { prop4: 1 };
+			};
+	  }
+	| {
+			prop1: number;
+			prop2: {
+				prop3: { prop4: 2 };
+			};
+	  };
+
+type prettify<T> = { [k in keyof T]: T[k] } & {};
+
+type _distributeVal<T, Key extends keyof T, Val = T[Key]> = Val extends Val
+	? nestedDistribute<Omit<T, Key>> & {
+			[x in Key]: Val extends object ? nestedDistribute<Val> : Val;
+		}
+	: {};
+
+type _distributeOnce<T, Keys extends keyof T = keyof T> = prettify<
+	[Keys] extends [never] ? {} : Keys extends Keys ? _distributeVal<T, Keys> : {}
+>;
+
+type nestedDistribute<T> = _distributeOnce<_distributeOnce<T>>;
+
+type _Out = nestedDistribute<In>;
+//   ^?
+
+type _cases = [Expect<Equal<_Out, Out>>];
