89ea1f48221ceefb9c7bc12b39d4ac1ec6f3054a
SuperKXT <superkxt@outlook.com>
2024-09-30 15:55:25 +0500

    Update `scripts/merge-json`

--- scripts/merge-json/merge-json.ts
+++ scripts/merge-json/merge-json.ts
@@ -2,9 +2,9 @@ import { readFile, writeFile } from 'fs/promises';
 
 import { z } from 'zod';
 
-const PARAMS_SCHEMA = z
-	.tuple([z.unknown(), z.unknown(), z.string(), z.string()])
-	.rest(z.string());
+import { stringifyError } from '../../helpers/error.helpers.js';
+
+const PARAMS_SCHEMA = z.tuple([z.string(), z.string()]).rest(z.string());
 
 const [source, ...targets] = PARAMS_SCHEMA.parse(process.argv.slice(2));
 
@@ -15,10 +15,13 @@ const sourceFile = JSON.parse(await readFile(source, 'utf-8')) as Record<
 
 await Promise.all(
 	targets.map(async (target) => {
-		const targetFile = JSON.parse(await readFile(target, 'utf-8')) as Record<
-			string,
-			unknown
-		>;
+		let targetFile: Record<string, unknown> = {};
+		try {
+			targetFile = JSON.parse(await readFile(target, 'utf-8')) as never;
+			console.info(`Merging ${target}`);
+		} catch (error) {
+			console.error(`Error merging ${target}: ${stringifyError(error)}`);
+		}
 		for (const key in targetFile) {
 			if (!Object.hasOwn(targetFile, key)) continue;
 			if (Object.hasOwn(sourceFile, key)) continue;
@@ -27,9 +30,6 @@ await Promise.all(
 		}
 	}),
 );
-
-console.log(source);
-console.log(Object.keys(sourceFile).length);
 
 await writeFile(source, JSON.stringify(sourceFile, null, '\t'));
 
