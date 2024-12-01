d6217a072242edcd21dbfcca4767dea0836824be
SuperKXT <superkxt@outlook.com>
2024-12-01 16:56:10 +0500

    Add `getRandomInteger` helper

---
+++ helpers/random.helpers.test.ts
@@ -0,0 +1,11 @@
+import { getRandomInteger } from './random.helpers.js';
+
+test('testing getRandomInteger helper', () => {
+	const min = 0;
+	const max = 100_000;
+	for (let i = 0; i < 100_000; i++) {
+		const val = getRandomInteger(min, max);
+		expect(val).toBeGreaterThanOrEqual(min);
+		expect(val).toBeLessThanOrEqual(max);
+	}
+});
