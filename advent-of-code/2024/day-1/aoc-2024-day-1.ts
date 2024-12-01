e7e351ac159d3913951e6782f492fa1cc903de56
SuperKXT <superkxt@outlook.com>
2024-12-01 17:02:57 +0500

    Add `aoc/2024/day-1` part 2 solution

--- advent-of-code/2024/day-1/aoc-2024-day-1.ts
+++ advent-of-code/2024/day-1/aoc-2024-day-1.ts
@@ -1,8 +1,9 @@
 import { inPlaceInsertToSortedArray } from '../../../helpers/array.helpers.js';
 
-export const compareLists = (input: string): number => {
+export const aoc2024Day1 = (input: string) => {
 	const leftArr: number[] = [];
 	const rightArr: number[] = [];
+	const rightCountMap = new Map<number, number>();
 	for (const row of input.split('\n')) {
 		const split = row.trim().split(/\s+/u);
 		const left = Number(split[0]);
@@ -10,10 +11,16 @@ export const compareLists = (input: string): number => {
 		if (!left || !right) continue;
 		inPlaceInsertToSortedArray(leftArr, left);
 		inPlaceInsertToSortedArray(rightArr, right);
+		rightCountMap.set(right, (rightCountMap.get(right) ?? 0) + 1);
 	}
 	let distance = 0;
+	let similarity = 0;
 	for (let i = 0; i < leftArr.length; i++) {
-		distance += Math.abs((leftArr[i] as number) - (rightArr[i] as number));
+		const leftVal = leftArr[i] as number;
+		const rightVal = rightArr[i] as number;
+		const rightCount = rightCountMap.get(leftVal) ?? 0;
+		distance += Math.abs(leftVal - rightVal);
+		similarity += leftVal * rightCount;
 	}
-	return distance;
+	return { distance, similarity };
 };
