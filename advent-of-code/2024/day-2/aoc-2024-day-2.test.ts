42bc02e19e518124642ec88c5260ed7053aee3b1
SuperKXT <superkxt@outlook.com>
2024-12-02 11:03:26 +0500

    Add solution for `aoc/2024/day-2/2`

--- advent-of-code/2024/day-2/aoc-2024-day-2.test.ts
+++ advent-of-code/2024/day-2/aoc-2024-day-2.test.ts
@@ -11,8 +11,10 @@ test('testing aoc-2024-day-2', async () => {
 	const sample = await readFile(path.join(dirname, 'sample.txt'), 'utf-8');
 	const sampleRes = aoc2024Day2(sample);
 	expect(sampleRes.safeCount).toBe(2);
+	expect(sampleRes.adjustedSafeCount).toBe(4);
 
 	const input = await readFile(path.join(dirname, 'input.txt'), 'utf-8');
 	const response = aoc2024Day2(input);
 	expect(response.safeCount).toBe(510);
+	expect(response.adjustedSafeCount).toBe(510);
 });
