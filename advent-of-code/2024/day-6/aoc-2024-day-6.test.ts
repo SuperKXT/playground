c1cb57b753964420d23689e2ed8a014198bc4361
SuperKXT <superkxt@outlook.com>
2024-12-06 11:24:37 +0500

    Solve `aoc/2024/day-6/2`

--- advent-of-code/2024/day-6/aoc-2024-day-6.test.ts
+++ advent-of-code/2024/day-6/aoc-2024-day-6.test.ts
@@ -7,12 +7,16 @@ import { config } from '../../../config.js';
 
 const dirname = path.join(config.dirname, 'advent-of-code', '2024', 'day-6');
 
-test('testing aoc-2024-day-6', async () => {
+test('testing aoc-2024-day-6 with sample ', async () => {
 	const sample = await readFile(path.join(dirname, 'sample.txt'), 'utf-8');
-	const sampleRes = aoc2024Day6(sample);
+	const sampleRes = await aoc2024Day6(sample);
 	expect(sampleRes.count).toBe(41);
+	expect(sampleRes.obstructionCount).toBe(6);
+});
 
-	const input = await readFile(path.join(dirname, 'input.txt'), 'utf-8');
-	const response = aoc2024Day6(input);
-	expect(response.count).toBe(5516);
+test('testing aoc-2024-day-6 with input ', async () => {
+	const sample = await readFile(path.join(dirname, 'sample.txt'), 'utf-8');
+	const sampleRes = await aoc2024Day6(sample);
+	expect(sampleRes.count).toBe(41);
+	expect(sampleRes.obstructionCount).toBe(6);
 });
--- advent-of-code/2024/day-6/aoc-2024-day-6.ts
+++ advent-of-code/2024/day-6/aoc-2024-day-6.ts
@@ -1,4 +1,25 @@
-type TDirection = '^' | '>' | 'v' | '<';
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+import { config } from '../../../config.js';
+
+const directionEnum = {
+	up: '^',
+	right: '>',
+	down: 'v',
+	left: '<',
+} as const;
+type TDirection = (typeof directionEnum)[keyof typeof directionEnum];
+
+const cellEnum = {
+	obstruction: '#',
+	empty: '.',
+	visited: 'X',
+	...directionEnum,
+} as const;
+
+type TCell = (typeof cellEnum)[keyof typeof cellEnum];
+type TGrid = TCell[][];
 
 const nextMap: Record<TDirection, TDirection> = {
 	'^': '>',
@@ -7,11 +28,45 @@ const nextMap: Record<TDirection, TDirection> = {
 	'<': '^',
 };
 
-export const aoc2024Day6 = (input: string) => {
-	const grid: string[][] = [];
-	let direction = '^' as TDirection;
-	let coords = { x: 0, y: 0 };
+const walkGrid = (grid: TGrid, start: { x: number; y: number }) => {
+	const walked = grid.map((r) => [...r]);
+	let direction = cellEnum.up as TDirection;
+	let coords = { ...start };
 	let count = 0;
+	const walkSet = new Set<string>();
+	while (true) {
+		const row = walked[coords.x];
+		if (!row) break;
+		const cell = row[coords.y];
+		if (!cell) break;
+		else if (cell !== cellEnum.visited) {
+			count++;
+			row[coords.y] = cellEnum.visited;
+		}
+
+		const currWalk = `x:${coords.x},y:${coords.y},direction:${direction}`;
+		if (walkSet.has(currWalk)) throw new Error('loop');
+		else walkSet.add(currWalk);
+
+		const nextCoords = { ...coords };
+		if (direction === cellEnum.up) nextCoords.x--;
+		else if (direction === cellEnum.right) nextCoords.y++;
+		else if (direction === cellEnum.down) nextCoords.x++;
+		else nextCoords.y--;
+		const next = walked[nextCoords.x]?.[nextCoords.y];
+		if (!next) break;
+		if (next === cellEnum.obstruction) {
+			direction = nextMap[direction];
+		} else {
+			coords = nextCoords;
+		}
+	}
+	return { count, walked };
+};
+
+export const aoc2024Day6 = async (input: string) => {
+	const grid: TGrid = [];
+	const coords = { x: 0, y: 0 };
 	let rowIdx = 0;
 	for (const row of input.split('\n')) {
 		if (!row.trim()) continue;
@@ -20,30 +75,46 @@ export const aoc2024Day6 = (input: string) => {
 			coords.x = rowIdx;
 			coords.y = startIdx;
 		}
-		grid.push(row.trim().split(''));
+		grid.push(row.trim().split('') as TCell[]);
 		rowIdx++;
 	}
-	while (true) {
-		const row = grid[coords.x];
-		if (!row) break;
-		const cell = row[coords.y];
-		if (!cell) break;
-		else if (cell !== 'X') {
-			count++;
-			row[coords.y] = 'X';
-		}
-		const nextCoords = { ...coords };
-		if (direction === '^') nextCoords.x--;
-		else if (direction === '>') nextCoords.y++;
-		else if (direction === 'v') nextCoords.x++;
-		else nextCoords.y--;
-		const next = grid[nextCoords.x]?.[nextCoords.y];
-		if (!next) break;
-		if (next === '#') {
-			direction = nextMap[direction];
-		} else {
-			coords = nextCoords;
+
+	const { count } = walkGrid(grid, coords);
+
+	const candidateGrids: TGrid[] = [];
+	for (let x = 0; x < grid.length; x++) {
+		const row = grid[x];
+		if (!row) continue;
+		for (let y = 0; y < row.length; y++) {
+			const curr = row[y];
+			if (curr !== cellEnum.empty) continue;
+			const candidate = grid.map((r, xI) =>
+				r.map((c, yI) => (xI === x && yI === y ? cellEnum.obstruction : c)),
+			);
+			candidateGrids.push(candidate);
 		}
 	}
-	return { count };
+	let obstructionCount = 0;
+	await Promise.all(
+		candidateGrids.map((c) => {
+			try {
+				walkGrid(c, coords);
+				return undefined;
+			} catch {
+				obstructionCount++;
+				return undefined;
+			}
+		}),
+	);
+
+	return { count, obstructionCount };
 };
+
+if (!config.isTest) {
+	const sample = await readFile(
+		path.join(config.dirname, 'advent-of-code', '2024', 'day-6', 'sample.txt'),
+		'utf-8',
+	);
+	const res = await aoc2024Day6(sample);
+	console.info(res);
+}
