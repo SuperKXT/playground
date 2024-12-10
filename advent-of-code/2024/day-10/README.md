815eaa31ad630449f12e53be6cc106995822ee53
SuperKXT <superkxt@outlook.com>
2024-12-10 10:46:45 +0500

    Solve `aoc/2024/day-10`

---
+++ advent-of-code/2024/day-10/README.md
@@ -0,0 +1,3 @@
+# Day 10
+
+https://adventofcode.com/2024/day/10
---
+++ advent-of-code/2024/day-10/aoc-2024-day-10.test.ts
@@ -0,0 +1,18 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+import { aoc2024Day10, day10Path } from './aoc-2024-day-10.js';
+
+test('testing aoc-2024-day-10 with sample ', async () => {
+	const input = await readFile(path.join(day10Path, 'sample.txt'), 'utf-8');
+	const res = aoc2024Day10(input);
+	expect(res.summits).toBe(36);
+	expect(res.treks).toBe(81);
+});
+
+test('testing aoc-2024-day-10 with input ', async () => {
+	const input = await readFile(path.join(day10Path, 'input.txt'), 'utf-8');
+	const res = aoc2024Day10(input);
+	expect(res.summits).toBe(512);
+	expect(res.treks).toBe(1045);
+});
