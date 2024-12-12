94c0d2261046dde03c2846ba8fcb31c913754119
SuperKXT <superkxt@outlook.com>
2024-12-12 14:21:36 +0500

    Fix `aoc/2024/day-12` tests

--- advent-of-code/2024/day-12/aoc-2024-day-12.test.ts
+++ advent-of-code/2024/day-12/aoc-2024-day-12.test.ts
@@ -14,5 +14,5 @@ test('testing aoc-2024-day-12 with input ', async () => {
 	const input = await readFile(path.join(day12Path, 'input.txt'), 'utf-8');
 	const res = aoc2024Day12(input);
 	expect(res.cost).toBe(1573474);
-	expect(res.bulkCost).toBe(1573474);
+	expect(res.bulkCost).toBe(966476);
 });
