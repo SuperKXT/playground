62bd491401d8d1e23d13ebdde71d0e06c960fffa
SuperKXT <superkxt@outlook.com>
2024-12-16 15:24:06 +0500

    Solve `aoc/2024/day-15/2`

--- advent-of-code/2024/day-15/aoc-2024-day-15.test.ts
+++ advent-of-code/2024/day-15/aoc-2024-day-15.test.ts
@@ -1,22 +1,29 @@
 import { readFile } from 'fs/promises';
 import path from 'path';
 
-import { aoc2024Day15, day15Path } from './aoc-2024-day-15.js';
+import {
+	aoc2024Day15Part1,
+	aoc2024Day15Part2,
+	day15Path,
+} from './aoc-2024-day-15.js';
 
 test('testing aoc-2024-day-15 with sample ', async () => {
 	const input = await readFile(path.join(day15Path, 'sample.txt'), 'utf-8');
-	const res = aoc2024Day15(input);
+	const res = aoc2024Day15Part1(input);
 	expect(res.sum).toBe(10092);
+	expect(aoc2024Day15Part2(input).sum).toBe(9021);
 });
 
 test('testing aoc-2024-day-15 with sample-2 ', async () => {
 	const input = await readFile(path.join(day15Path, 'sample-2.txt'), 'utf-8');
-	const res = aoc2024Day15(input);
+	const res = aoc2024Day15Part1(input);
 	expect(res.sum).toBe(2028);
+	expect(aoc2024Day15Part2(input).sum).toBe(1751);
 });
 
 test('testing aoc-2024-day-15 with input ', async () => {
 	const input = await readFile(path.join(day15Path, 'input.txt'), 'utf-8');
-	const res = aoc2024Day15(input);
+	const res = aoc2024Day15Part1(input);
 	expect(res.sum).toBe(1430439);
+	expect(aoc2024Day15Part2(input).sum).toBe(1751);
 });
--- advent-of-code/2024/day-15/aoc-2024-day-15.ts
+++ advent-of-code/2024/day-15/aoc-2024-day-15.ts
@@ -16,6 +16,8 @@ const gridCellEnum = {
 	box: 'O',
 	wall: '#',
 	robot: '@',
+	boxLeft: '[',
+	boxRight: ']',
 } as const;
 type TGridCell = (typeof gridCellEnum)[keyof typeof gridCellEnum];
 
@@ -25,7 +27,7 @@ type TGrid = TGridRow[];
 const movements = ['^', 'v', '>', '<'] as const;
 type TMovement = (typeof movements)[number];
 
-export const aoc2024Day15 = (input: string) => {
+export const aoc2024Day15Part1 = (input: string) => {
 	const split = input.trim().split('\n\n');
 	const grid: TGrid = [];
 	const swappedGrid: TGrid = [];
@@ -165,10 +167,133 @@ export const aoc2024Day15 = (input: string) => {
 	return { sum };
 };
 
+const makeMove = (
+	grid: TGrid,
+	pos: [number, number],
+	move: TMovement,
+	curr: TGridCell = gridCellEnum.robot,
+): boolean => {
+	const nextPos: [number, number] = [
+		move === '^' ? pos[0] - 1 : move === 'v' ? pos[0] + 1 : pos[0],
+		move === '<' ? pos[1] - 1 : move === '>' ? pos[1] + 1 : pos[1],
+	];
+	const movedSet = new Set<string>();
+	const nextCell = grid[nextPos[0]]?.[nextPos[1]];
+	if (!nextCell) throw new Error('invalid grid!');
+
+	if (nextCell === gridCellEnum.wall) return false;
+
+	if (movedSet.has(`${nextPos[0]}-${nextPos[1]}`)) return true;
+	movedSet.add(`${nextPos[0]}-${nextPos[1]}`);
+
+	let success = true;
+	if (nextCell !== gridCellEnum.empty) {
+		if (move === '^' || move === 'v') {
+			if (nextCell === gridCellEnum.boxLeft) {
+				const right: [number, number] = [nextPos[0], nextPos[1] + 1];
+				success =
+					makeMove(grid, nextPos, move, nextCell) &&
+					makeMove(grid, right, move, gridCellEnum.boxRight);
+			} else if (nextCell === gridCellEnum.boxRight) {
+				const left: [number, number] = [nextPos[0], nextPos[1] - 1];
+				success =
+					makeMove(grid, nextPos, move, nextCell) &&
+					makeMove(grid, left, move, gridCellEnum.boxLeft);
+			} else {
+				success = makeMove(grid, nextPos, move, nextCell);
+			}
+		} else {
+			success = makeMove(grid, nextPos, move, nextCell);
+		}
+	}
+
+	if (!success) return false;
+
+	grid[nextPos[0]]![nextPos[1]] = curr;
+	grid[pos[0]]![pos[1]] = gridCellEnum.empty;
+	if (curr === gridCellEnum.robot) {
+		pos[0] = nextPos[0];
+		pos[1] = nextPos[1];
+	}
+	return true;
+};
+
+export const aoc2024Day15Part2 = (input: string) => {
+	const split = input.trim().split('\n\n');
+	let grid: TGrid = [];
+	let rows = 1;
+	let cols = 1;
+	const pos = [0, 0] as [number, number];
+	let cY = 0;
+	let cX = 0;
+	for (const curr of split[0]?.trim() ?? '') {
+		if (curr === '\n') {
+			rows++;
+			cols = cX + 1;
+			cY++;
+			cX = 0;
+			continue;
+		}
+		const row = grid[cY] ?? [];
+		if (!grid[cY]) grid[cY] = row;
+
+		if (curr === gridCellEnum.robot) {
+			if (pos[0] !== 0 && pos[1] !== 1) throw new Error('Invalid input');
+			pos[0] = cY;
+			pos[1] = cX;
+			row[cX] = gridCellEnum.robot;
+			row[cX + 1] = gridCellEnum.empty;
+		} else if (curr === gridCellEnum.box) {
+			row[cX] = gridCellEnum.boxLeft;
+			row[cX + 1] = gridCellEnum.boxRight;
+		} else if (curr === gridCellEnum.wall) {
+			row[cX] = gridCellEnum.wall;
+			row[cX + 1] = gridCellEnum.wall;
+		} else if (curr === gridCellEnum.empty) {
+			row[cX] = gridCellEnum.empty;
+			row[cX + 1] = gridCellEnum.empty;
+		} else throw new Error('Invalid input');
+
+		cX += 2;
+	}
+
+	const moves =
+		split[1]
+			?.trim()
+			.split('')
+			.filter((s): s is TMovement => movements.includes(s)) ?? [];
+
+	if (!grid.length || !grid[0] || !moves.length)
+		throw new Error('Invalid input');
+
+	for (const move of moves) {
+		const updated = structuredClone(grid);
+		const success = makeMove(updated, pos, move);
+		if (success) grid = updated;
+	}
+
+	let sum = 0;
+	for (let y = 0; y < rows; y++) {
+		for (let x = 0; x < cols; x++) {
+			if (
+				grid[y]![x] !== gridCellEnum.box &&
+				grid[y]![x] !== gridCellEnum.boxLeft
+			)
+				continue;
+			sum += y * 100 + x;
+		}
+	}
+
+	// console.log(grid.map((r) => r.join('')).join('\n'));
+
+	return { sum };
+};
+
 if (!config.isTest) {
 	console.time('aoc-2024-day-15');
 	const input = await readFile(path.join(day15Path, 'input.txt'), 'utf-8');
-	const res = aoc2024Day15(input);
-	console.info(res);
+	const part1 = aoc2024Day15Part1(input);
+	const part2 = aoc2024Day15Part2(input);
+	console.info({ part1, part2 });
 	console.timeEnd('aoc-2024-day-15');
 }
