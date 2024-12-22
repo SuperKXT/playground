f0c9971c1587bf4c6867025a79a8b88406cdbbf5
SuperKXT <superkxt@outlook.com>
2024-12-22 14:39:35 +0500

    Solve `aoc/2024/day-22/2`

--- advent-of-code/2024/day-22/aoc-2024-day-22.test.ts
+++ advent-of-code/2024/day-22/aoc-2024-day-22.test.ts
@@ -7,10 +7,12 @@ test('testing aoc-2024-day-22 with sample ', async () => {
 	const input = await readFile(path.join(day22Path, 'sample.txt'), 'utf-8');
 	const res = aoc2024Day22(input);
 	expect(res.sum).toBe(37327623n);
+	expect(res.highest).toBe(24);
 });
 
 test('testing aoc-2024-day-22 with input ', async () => {
 	const input = await readFile(path.join(day22Path, 'input.txt'), 'utf-8');
 	const res = aoc2024Day22(input);
-	expect(res.sum).toBe(209);
+	expect(res.sum).toBe(15608699004n);
+	expect(res.highest).toBe(1791);
 });
--- advent-of-code/2024/day-22/aoc-2024-day-22.ts
+++ advent-of-code/2024/day-22/aoc-2024-day-22.ts
@@ -26,21 +26,44 @@ const getNewSecret = (a: bigint) => {
 	return num3;
 };
 
+const getPrice = (a: bigint) => {
+	return Number(a.toString().at(-1) ?? '');
+};
+
 export const aoc2024Day22 = (input: string) => {
 	const codes = input.trim().split('\n').map(BigInt).filter(Boolean);
 	if (!codes.length) throw new Error('Invalid input');
 
-	for (let i = 0; i < 2000; i++) {
-		for (let j = 0; j < codes.length; j++) {
-			const code = codes[j] as bigint;
-			const newCode = getNewSecret(code);
-			codes[j] = newCode;
+	const sequenceMap = new Map<string, number>();
+	for (let j = 0; j < codes.length; j++) {
+		const visitedSequences = new Set<string>();
+		let code = codes[j] as bigint;
+		const prev4: { price: number; diff: number }[] = [];
+		for (let i = 0; i < 2000; i++) {
+			code = getNewSecret(code);
+
+			const price = getPrice(code);
+			const prev = prev4.at(-1);
+			prev4.push({ price, diff: prev ? price - prev.price : 0 });
+			if (prev4.length > 4) prev4.shift();
+			const sequence = prev4.map((r) => r.diff).join(',');
+			if (prev4.length === 4 && !visitedSequences.has(sequence)) {
+				visitedSequences.add(sequence);
+				const existing = sequenceMap.get(sequence) ?? 0;
+				sequenceMap.set(sequence, existing + price);
+			}
 		}
+		codes[j] = code;
+	}
+
+	let highest = -Infinity;
+	for (const price of sequenceMap.values()) {
+		highest = Math.max(highest, price);
 	}
 
 	const sum = codes.reduce((acc, code) => acc + code);
 
-	return { sum };
+	return { sum, highest };
 };
 
 if (!config.isTest) {
