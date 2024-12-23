725dbc1401945f30f43e03685ab1ef8cd23c560f
SuperKXT <superkxt@outlook.com>
2024-12-23 15:23:52 +0500

    Solve `aoc/2024/day-23/1`

---
+++ advent-of-code/2024/day-23/README.md
@@ -0,0 +1,3 @@
+# Day 23
+
+https://adventofcode.com/2024/day/23
---
+++ advent-of-code/2024/day-23/aoc-2024-day-23.test.ts
@@ -0,0 +1,16 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+import { aoc2024Day23, day23Path } from './aoc-2024-day-23.js';
+
+test('testing aoc-2024-day-23 with sample ', async () => {
+	const input = await readFile(path.join(day23Path, 'sample.txt'), 'utf-8');
+	const res = aoc2024Day23(input);
+	expect(res.connections).toBe(7);
+});
+
+test('testing aoc-2024-day-23 with input ', async () => {
+	const input = await readFile(path.join(day23Path, 'input.txt'), 'utf-8');
+	const res = aoc2024Day23(input);
+	expect(res.connections).toBe(1358);
+});
