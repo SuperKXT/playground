8fe95945b5f8005586b49a685c85c289aec24230
SuperKXT <superkxt@outlook.com>
2024-12-25 11:05:28 +0500

    Solve `aoc/2024/day-25/1`

---
+++ advent-of-code/2024/day-25/README.md
@@ -0,0 +1,3 @@
+# Day 25
+
+https://adventofcode.com/2024/day/25
---
+++ advent-of-code/2024/day-25/aoc-2024-day-25.test.ts
@@ -0,0 +1,16 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+import { aoc2024Day24, day25Path } from './aoc-2024-day-25.js';
+
+test('testing aoc-2024-day-25 with sample ', async () => {
+	const input = await readFile(path.join(day25Path, 'sample.txt'), 'utf-8');
+	const res = aoc2024Day24(input);
+	expect(res.matches).toBe(3);
+});
+
+test('testing aoc-2024-day-25 with input ', async () => {
+	const input = await readFile(path.join(day25Path, 'input.txt'), 'utf-8');
+	const res = aoc2024Day24(input);
+	expect(res.matches).toBe(3249);
+});
