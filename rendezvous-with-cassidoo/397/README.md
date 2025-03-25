9b6d3bc6c94301412a07a47b8ddfd830909a4e19
SuperKXT <superkxt@outlook.com>
2025-03-25 11:01:10 +0500

    Add js solution for `rwc#397`

---
+++ rendezvous-with-cassidoo/397/README.md
@@ -0,0 +1,25 @@
+# findLongestStreak
+
+[issue #397 of rendezvous with cassidoo.](https://buttondown.com/cassidoo/archive/this-present-moment-was-once-the-unimaginable/)
+
+Write a function that finds the longest streak of consecutive true values
+in a boolean array that meets or exceeds a given streak goal.
+
+Return 0 if no such streak exists.
+
+Example:
+
+```ts
+findLongestStreak([true, true, false, true, true, true], 3);
+3;
+
+findLongestStreak([true, true, true, false, true], 4);
+0;
+
+findLongestStreak([true, true, true, true], 2);
+4;
+```
+
+---
+
+[Solution Playground](https://tsplay.dev/NVDY7W)
---
+++ rendezvous-with-cassidoo/397/find-longest-streak.test.ts
@@ -0,0 +1,22 @@
+import { findLongestStreak } from "./find-longest-streak.js";
+
+test("testing shieldBreak against test 1", () => {
+	const result = findLongestStreak([true, true, false, true, true, true], 3);
+	const expected = 3;
+	expect(result).toStrictEqual(expected);
+	assertType<Readonly<typeof result>>(expected);
+});
+
+test("testing shieldBreak against test 2", () => {
+	const result = findLongestStreak([true, true, true, false, true], 4);
+	const expected = 0;
+	expect(result).toStrictEqual(expected);
+	assertType<Readonly<typeof result>>(expected);
+});
+
+test("testing shieldBreak against test 3", () => {
+	const result = findLongestStreak([true, true, true, true], 2);
+	const expected = 4;
+	expect(result).toStrictEqual(expected);
+	assertType<Readonly<typeof result>>(expected);
+});
