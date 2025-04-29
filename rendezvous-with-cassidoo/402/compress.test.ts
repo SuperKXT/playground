060bd88e5eb2a4e5871b9e656fabb67c1b240640
SuperKXT <superkxt@outlook.com>
2025-04-29 10:57:15 +0500

    Add ts solution for `rwc#403`

--- rendezvous-with-cassidoo/402/compress.test.ts
+++ rendezvous-with-cassidoo/402/compress.test.ts
@@ -4,14 +4,14 @@ import { compress } from "./compress.js";
 
 test("testing compress against test 1", () => {
 	const result = compress(["a", "b", "b", "b", "c"]);
-	const expected = ["a", "b", "3", "c"];
+	const expected = ["a", "b", "3", "c"] as const;
 	expect(result).toStrictEqual(expected);
 	assertType<Readonly<typeof result>>(expected);
 });
 
 test("testing compress against test 2", () => {
 	const result = compress(["a", "a", "b", "b", "c", "c", "c"]);
-	const expected = ["a", "2", "b", "2", "c", "3"];
+	const expected = ["a", "2", "b", "2", "c", "3"] as const;
 	expect(result).toStrictEqual(expected);
 	assertType<Readonly<typeof result>>(expected);
 });
