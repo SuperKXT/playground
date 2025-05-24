0e4c9608753c5c83546dcf67f72d1a0202125cea
SuperKXT <superkxt@outlook.com>
2025-05-24 13:20:24 +0500

    Refactor `tower-of-hanoi`

--- random/tower-of-hanoi/tower-of-hanoi.ts
+++ random/tower-of-hanoi/tower-of-hanoi.ts
@@ -1,9 +1,12 @@
 type TTower = "A" | "C" | "B";
-const towerMap: Record<TTower, { left: TTower; right: TTower }> = {
+const towerMap = {
 	A: { left: "B", right: "C" },
 	C: { left: "A", right: "B" },
 	B: { left: "C", right: "A" },
-};
+} satisfies Record<TTower, { left: TTower; right: TTower }>;
+
+type TTowerMap = typeof towerMap;
+
 export type TStep = [TTower, TTower];
 
 const getPadded = (val: string, rings: number) => {
@@ -40,8 +43,7 @@ const _getTowerString = (rings: number, towers: Record<TTower, string[]>) => {
 };
 
 export const towerOfHanoi = (rings: number): TStep[] => {
-	const smallestDirection = rings % 2 === 0 ? "right" : "left";
-
+	const direction = rings % 2 === 0 ? "right" : "left";
 	let smallest = "A" as TTower;
 	let move: "smallest" | "other" = "smallest";
 	const towers = {
@@ -53,7 +55,7 @@ export const towerOfHanoi = (rings: number): TStep[] => {
 
 	while (towers.B.length !== rings) {
 		if (move === "smallest") {
-			const next = towerMap[smallest][smallestDirection];
+			const next = towerMap[smallest][direction];
 			towers[next].unshift(towers[smallest].shift() as string);
 			steps.push([smallest, next]);
 			smallest = next;
@@ -61,14 +63,11 @@ export const towerOfHanoi = (rings: number): TStep[] => {
 		} else {
 			const tower1 = smallest === "A" ? "B" : smallest === "B" ? "A" : "A";
 			const tower2 = smallest === "A" ? "C" : smallest === "B" ? "C" : "B";
-			const ring1 = towers[tower1].shift() as string;
-			const ring2 = towers[tower2].shift() as string;
+			const ring1 = towers[tower1][0];
+			const ring2 = towers[tower2][0] as string;
 			const to = !ring1 || ring1 > ring2 ? tower1 : tower2;
 			const from = to === tower1 ? tower2 : tower1;
-			const toAdd = [] as string[];
-			if (ring1) toAdd.push(ring1);
-			if (ring2) toAdd.push(ring2);
-			towers[to].unshift(...toAdd.sort());
+			towers[to].unshift(towers[from].shift() as string);
 			steps.push([from, to]);
 			move = "smallest";
 		}
