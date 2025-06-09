5f8ac2960e01de7209ef5d48057705ef6b85939a
SuperKXT <superkxt@outlook.com>
2025-06-09 14:35:51 +0500

    Add ts solution for `rwc#408`

--- rendezvous-with-cassidoo/408/traffic-sequence.ts
+++ rendezvous-with-cassidoo/408/traffic-sequence.ts
@@ -6,13 +6,24 @@ const validMap = {
 	yellow: "red",
 } as const satisfies Record<TTrafficLight, TTrafficLight>;
 
-export const isValidTrafficSequence = (sequence: TTrafficLight[]): boolean => {
+type TIsValidTrafficSequence<T extends TTrafficLight[]> = T extends [
+	infer first extends TTrafficLight,
+	infer second extends TTrafficLight,
+	...infer rest extends TTrafficLight[],
+]
+	? (typeof validMap)[first] extends second
+		? TIsValidTrafficSequence<[second, ...rest]>
+		: false
+	: true;
+
+export const isValidTrafficSequence = <const Sequence extends TTrafficLight[]>(
+	sequence: Sequence,
+): TIsValidTrafficSequence<Sequence> => {
 	const [first, ...rest] = sequence;
-	if (!first) return true;
-	let last = first;
+	let last = first as TTrafficLight;
 	for (const curr of rest) {
-		if (validMap[last] !== curr) return false;
+		if (validMap[last] !== curr) return false as never;
 		last = curr;
 	}
-	return true;
+	return true as never;
 };
