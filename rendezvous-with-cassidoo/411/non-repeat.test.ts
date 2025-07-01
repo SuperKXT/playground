e21f973ad641717f79ebcce28f0338009ea43f3b
SuperKXT <superkxt@outlook.com>
2025-07-01 13:28:31 +0500

    Add ts solution for `rwc#411`

--- rendezvous-with-cassidoo/411/non-repeat.test.ts
+++ rendezvous-with-cassidoo/411/non-repeat.test.ts
@@ -4,14 +4,14 @@ import { nonRepeat } from "./non-repeat.js";
 
 test("testing nonRepeat against test 1", () => {
 	const result = nonRepeat("candy canes do taste yummy");
-	const expected = "u" as string;
+	const expected = "u";
 	expect(result).toStrictEqual(expected);
 	assertType<typeof expected>(result);
 });
 
 test("testing nonRepeat against test 2", () => {
 	const result = nonRepeat("can can ");
-	const expected = "" as string;
+	const expected = "";
 	expect(result).toStrictEqual(expected);
 	assertType<typeof expected>(result);
 });
