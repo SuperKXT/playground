1ca7a25ad7275b35e1015a146d84c13510ec5ecd
SuperKXT <superkxt@outlook.com>
2025-07-02 14:00:36 +0000

    Add ts solution for `leet-code/easy/121`

--- leet-code/easy/121-rotate-string/rotate-string.ts
+++ leet-code/easy/121-rotate-string/rotate-string.ts
@@ -1,11 +1,24 @@
 // https://leetcode.com/problems/rotate-string
 
-export const rotateString = (s: string, goal: string): boolean => {
-	if (s.length !== goal.length) return false;
-	if (s === goal) return true;
+type TRotateString<
+	S extends string,
+	Goal extends string,
+	goalAfter extends string = "",
+> = S extends `${Goal}${goalAfter}`
+	? true
+	: Goal extends `${infer first}${infer rest}`
+		? TRotateString<S, rest, `${goalAfter}${first}`>
+		: false;
+
+export const rotateString = <S extends string, Goal extends string>(
+	s: S,
+	goal: Goal,
+): TRotateString<S, Goal> => {
+	if (s.length !== goal.length) return false as never;
+	if (s === goal) return true as never;
 	for (let i = s.length - 1; i > 0; i--) {
 		const curr = s.slice(i) + s.slice(0, i);
-		if (curr === goal) return true;
+		if (curr === goal) return true as never;
 	}
-	return false;
+	return false as never;
 };
