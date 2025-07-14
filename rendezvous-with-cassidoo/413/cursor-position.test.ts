cb0959efaa1ef61228fc2fc6660c155998356fae
SuperKXT <superkxt@outlook.com>
2025-07-14 15:19:25 +0500

    Add ts solution for `rwc#413`

--- rendezvous-with-cassidoo/413/cursor-position.test.ts
+++ rendezvous-with-cassidoo/413/cursor-position.test.ts
@@ -2,9 +2,21 @@ import { assertType, expect, test } from "vitest";
 
 import { cursorPosition } from "./cursor-position.js";
 
-test("testing nonRepeat against test 1", () => {
+import type { Utils } from "../../types/utils.types.js";
+
+test("testing cursorPosition against test 1", () => {
 	const result = cursorPosition(`Hello, world!\nhow are ya?`, "jlhll"); // @cSpell: disable-line;
-	const expected = "w" as string;
+	const expected = "w";
 	expect(result).toStrictEqual(expected);
-	assertType<typeof expected>(result);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing cursorPosition against test 2", () => {
+	// eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+	const result = cursorPosition(`Hello, world!\n`, "jlhll"); // @cSpell: disable-line;
+	const expected = undefined;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
 });
