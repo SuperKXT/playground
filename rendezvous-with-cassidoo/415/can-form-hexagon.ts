73d11fe65d362508ad597173875b3d72c649c25c
SuperKXT <superkxt@outlook.com>
2025-08-10 15:12:54 +0500

    Add ts solution for `rwc#415`

--- rendezvous-with-cassidoo/415/can-form-hexagon.ts
+++ rendezvous-with-cassidoo/415/can-form-hexagon.ts
@@ -1,7 +1,31 @@
-export const canFormHexagon = (parts: number[]): boolean => {
+type TMapSideLengths<
+	Parts extends readonly number[],
+	map extends Record<number, 1[]> = {},
+> = Parts extends readonly [
+	infer first extends number,
+	...infer rest extends number[],
+]
+	? TMapSideLengths<
+			rest,
+			Omit<map, first> &
+				Record<
+					first,
+					map[first] extends infer curr extends 1[] ? [...curr, 1] : [1]
+				>
+		>
+	: map;
+
+type TCanFormHexagon<
+	Parts extends readonly number[],
+	map = TMapSideLengths<Parts>,
+> = [1, 1] extends map[keyof map] ? true : false;
+
+export const canFormHexagon = <const Parts extends readonly number[]>(
+	parts: Parts,
+): TCanFormHexagon<Parts> => {
 	const map = new Map<number, number>();
 	for (const r of parts) {
 		map.set(r, (map.get(r) ?? 0) + 1);
 	}
-	return Array.from(map.values()).every((r) => r === 2);
+	return Array.from(map.values()).every((r) => r === 2) as never;
 };
