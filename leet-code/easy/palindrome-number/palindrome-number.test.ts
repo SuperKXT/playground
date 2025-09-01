a9dd8619e6176da6b42696244cf329b18f7ab6c2
SuperKXT <superkxt@outlook.com>
2025-09-01 12:18:25 +0500

    Add js solution for `leet-code/easy/palindrome-number`

---
+++ leet-code/easy/palindrome-number/palindrome-number.test.ts
@@ -0,0 +1,29 @@
+import { assertType, expect, test } from "vitest";
+
+import { palindromeNumber } from "./palindrome-number.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing palindromeNumber for test 1", () => {
+	const result = palindromeNumber(121);
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing palindromeNumber for test 2", () => {
+	const result = palindromeNumber(-121);
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing palindromeNumber for test 3", () => {
+	const result = palindromeNumber(10);
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
