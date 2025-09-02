c8ce1fb132b157b436bdfacc493a0e21fc6212b6
SuperKXT <superkxt@outlook.com>
2025-09-02 12:49:01 +0500

    Add ts solution for `leet-code/easy/3`

--- leet-code/easy/3-valid-parenthesis/valid-parenthesis.test.ts
+++ leet-code/easy/3-valid-parenthesis/valid-parenthesis.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing palindromeNumber for test 1", () => {
 	const result = validParenthesis("()");
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing palindromeNumber for test 1", () => {
 
 test("testing palindromeNumber for test 2", () => {
 	const result = validParenthesis("()[]{}");
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -22,7 +22,7 @@ test("testing palindromeNumber for test 2", () => {
 
 test("testing palindromeNumber for test 3", () => {
 	const result = validParenthesis("(]");
-	const expected = false as boolean;
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -30,7 +30,7 @@ test("testing palindromeNumber for test 3", () => {
 
 test("testing palindromeNumber for test 4", () => {
 	const result = validParenthesis("(25 + [3 / 2 * 1])");
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -38,7 +38,15 @@ test("testing palindromeNumber for test 4", () => {
 
 test("testing palindromeNumber for test 5", () => {
 	const result = validParenthesis("([)]");
-	const expected = false as boolean;
+	const expected = false;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing palindromeNumber for test 6", () => {
+	const result = validParenthesis("(");
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
--- leet-code/easy/3-valid-parenthesis/valid-parenthesis.ts
+++ leet-code/easy/3-valid-parenthesis/valid-parenthesis.ts
@@ -1,14 +1,40 @@
-export const validParenthesis = (str: string) => {
+const stackMap = {
+	"(": ")",
+	"[": "]",
+	"{": "}",
+} as const;
+
+type TStackMap = typeof stackMap;
+
+type TValidParenthesis<
+	Str extends string,
+	stack extends unknown[] = [],
+> = Str extends `${infer first}${infer rest}`
+	? first extends "(" | "[" | "{"
+		? TValidParenthesis<rest, [first, ...stack]>
+		: first extends ")" | "]" | "}"
+			? stack extends [infer top extends keyof TStackMap, ...infer newStack]
+				? (typeof stackMap)[top] extends first
+					? TValidParenthesis<rest, newStack>
+					: false
+				: false
+			: TValidParenthesis<rest, stack>
+	: stack["length"] extends 0
+		? true
+		: false;
+
+export const validParenthesis = <const Str extends string>(
+	str: Str,
+): TValidParenthesis<Str> => {
 	const stack: string[] = [];
 	for (const char of str) {
 		if (char === "(" || char === "[" || char === "{") {
 			stack.push(char);
 		} else if (char === ")" || char === "]" || char === "}") {
 			const top = stack.pop();
-			if (char === ")" && top !== "(") return false;
-			if (char === "]" && top !== "[") return false;
-			if (char === "}" && top !== "{") return false;
+			if (stackMap[top as keyof TStackMap] !== char) return false as never;
 		}
 	}
-	return true;
+	if (stack.length > 0) return false as never;
+	return true as never;
 };
