dfa4e9e04968b403a7b229fbe97197d17817bca7
SuperKXT <superkxt@outlook.com>
2025-09-03 16:46:01 +0500

    Add solution for `leet-code/easy/5`

--- leet-code/easy/4-reverse-integer/reverse-integer.test.ts
+++ leet-code/easy/4-reverse-integer/reverse-integer.test.ts
@@ -4,7 +4,7 @@ import { reverseInteger } from "./reverse-integer.js";
 
 import type { Utils } from "../../../types/utils.types.js";
 
-test("testing palindromeNumber for test 1", () => {
+test("testing reverseInteger for test 1", () => {
 	const result = reverseInteger(123);
 	const expected = 321;
 	expect(result).toStrictEqual(expected);
@@ -12,7 +12,7 @@ test("testing palindromeNumber for test 1", () => {
 	assertType<TTrue>(true);
 });
 
-test("testing palindromeNumber for test 2", () => {
+test("testing reverseInteger for test 2", () => {
 	const result = reverseInteger(-123);
 	const expected = -321;
 	expect(result).toStrictEqual(expected);
@@ -20,7 +20,7 @@ test("testing palindromeNumber for test 2", () => {
 	assertType<TTrue>(true);
 });
 
-test("testing palindromeNumber for test 3", () => {
+test("testing reverseInteger for test 3", () => {
 	const result = reverseInteger(120);
 	const expected = 21;
 	expect(result).toStrictEqual(expected);
@@ -28,7 +28,7 @@ test("testing palindromeNumber for test 3", () => {
 	assertType<TTrue>(true);
 });
 
-test("testing palindromeNumber for test 4", () => {
+test("testing reverseInteger for test 4", () => {
 	const result = reverseInteger(-2142312343);
 	const expected = 0;
 	expect(result).toStrictEqual(expected);
@@ -36,7 +36,7 @@ test("testing palindromeNumber for test 4", () => {
 	assertType<TTrue>(true);
 });
 
-test("testing palindromeNumber for test 5", () => {
+test("testing reverseInteger for test 5", () => {
 	const result = reverseInteger(21423255);
 	const expected = 55232412;
 	expect(result).toStrictEqual(expected);
@@ -44,7 +44,7 @@ test("testing palindromeNumber for test 5", () => {
 	assertType<TTrue>(true);
 });
 
-test("testing palindromeNumber for test 6", () => {
+test("testing reverseInteger for test 6", () => {
 	const result = reverseInteger(100000000002);
 	const expected = 0;
 	expect(result).toStrictEqual(expected);
