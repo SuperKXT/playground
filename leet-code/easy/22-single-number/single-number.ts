a3d9236319e639753ae54432e6bca398a9f90a66
SuperKXT <superkxt@outlook.com>
2025-09-09 21:27:15 +0500

    Add ts solution for `leet-code/easy/22`

--- leet-code/easy/22-single-number/single-number.ts
+++ leet-code/easy/22-single-number/single-number.ts
@@ -1,8 +1,28 @@
-export const singleNumber = (nums: number[]): number | null => {
+type TSingleNumber<
+	Nums extends number[],
+	counts extends { val: number; count: 0 | 1 } = never,
+> = Nums extends [infer first extends number, ...infer rest extends number[]]
+	? TSingleNumber<
+			rest,
+			| Exclude<counts, { val: first }>
+			| {
+					val: first;
+					count: Extract<counts, { val: first }> extends never ? 0 : 1;
+			  }
+		>
+	: Extract<counts, { count: 0 }>["val"] extends infer res
+		? [res] extends [never]
+			? null
+			: res
+		: null;
+
+export const singleNumber = <const Nums extends number[]>(
+	nums: Nums,
+): TSingleNumber<Nums> => {
 	const set = new Set<number>();
 	for (const num of nums) {
 		if (set.has(num)) set.delete(num);
 		else set.add(num);
 	}
-	return set.values().next().value ?? null;
+	return (set.values().next().value ?? null) as never;
 };
