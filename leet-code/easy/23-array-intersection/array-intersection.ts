9164e0f9505c2f21fb99498d7fbb419561b9b788
SuperKXT <superkxt@outlook.com>
2025-09-09 21:48:53 +0500

    Add ts solution for `leet-code/easy/23`

--- leet-code/easy/23-array-intersection/array-intersection.ts
+++ leet-code/easy/23-array-intersection/array-intersection.ts
@@ -1,7 +1,28 @@
-export const arrayIntersection = (
-	nums1: number[],
-	nums2: number[],
-): number[] => {
+type TRemoveFirst<Arr extends unknown[], Val> = Arr extends [
+	infer first,
+	...infer rest,
+]
+	? first extends Val
+		? rest
+		: TRemoveFirst<rest, Val>
+	: [];
+
+type TArrayIntersection<
+	Nums1 extends unknown[],
+	Nums2 extends unknown[],
+> = Nums1 extends [infer first, ...infer rest]
+	? first extends Nums2[number]
+		? [first, ...TArrayIntersection<rest, TRemoveFirst<Nums2, first>>]
+		: TArrayIntersection<rest, Nums2>
+	: [];
+
+export const arrayIntersection = <
+	const Nums1 extends number[],
+	const Nums2 extends number[],
+>(
+	nums1: Nums1,
+	nums2: Nums2,
+): TArrayIntersection<Nums1, Nums2> => {
 	const set2 = new Map<number, number>();
 	for (const num of nums2) set2.set(num, (set2.get(num) ?? 0) + 1);
 	const res: number[] = [];
