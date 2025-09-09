725c6e365a3b1e19ae747202fd38b3863da3cddc
SuperKXT <superkxt@outlook.com>
2025-09-09 16:11:36 +0500

    Add solution for `leet-code/easy/14`

---
+++ leet-code/easy/14-first-unique-char/first-unique-char.test.ts
@@ -0,0 +1,29 @@
+import { assertType, expect, test } from "vitest";
+
+import { firstUniqueChar } from "./first-unique-char.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing firstUniqueChar for test 1", () => {
+	const result = firstUniqueChar("leetcode"); // cSpell: disable-line
+	const expected = 0;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing firstUniqueChar for test 2", () => {
+	const result = firstUniqueChar("loveleetcode"); // cSpell: disable-line
+	const expected = 2;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing firstUniqueChar for test 3", () => {
+	const result = firstUniqueChar("aabb"); // cSpell: disable-line
+	const expected = -1;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
+	assertType<TTrue>(true);
+});
