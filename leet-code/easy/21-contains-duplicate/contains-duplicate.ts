9514297dfe0b05267e2962bd088c0201af675680
SuperKXT <superkxt@outlook.com>
2025-09-09 21:08:24 +0500

    Add ts solution for `leet-code/easy/21`

--- leet-code/easy/21-contains-duplicate/contains-duplicate.ts
+++ leet-code/easy/21-contains-duplicate/contains-duplicate.ts
@@ -1,12 +1,25 @@
-// export const containsDuplicate = (nums: number[]): boolean => {
-// 	return new Set(nums).size !== nums.length;
+type TContainsDuplicate<Nums extends unknown[]> = Nums extends [
+	infer first,
+	...infer rest,
+]
+	? first extends rest[number]
+		? true
+		: TContainsDuplicate<rest>
+	: false;
+
+// export const containsDuplicate = <Nums extends unknown[]>(
+// 	nums: number[],
+// ): TContainsDuplicate<Nums> => {
+// 	return (new Set(nums).size !== nums.length) as never;
 // };
 
-export const containsDuplicate = (nums: number[]): boolean => {
-	const set = new Set<number>();
+export const containsDuplicate = <const Nums extends unknown[]>(
+	nums: Nums,
+): TContainsDuplicate<Nums> => {
+	const set = new Set<Nums[number]>();
 	for (const num of nums) {
-		if (set.has(num)) return true;
+		if (set.has(num)) return true as never;
 		set.add(num);
 	}
-	return false;
+	return false as never;
 };
