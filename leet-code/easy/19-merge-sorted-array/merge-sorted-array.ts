62c5963b19bf84ccbe8680c2baf9dd65ac9c813a
SuperKXT <superkxt@outlook.com>
2025-09-09 20:38:35 +0500

    Add js solution for `leet-code/easy/19`

---
+++ leet-code/easy/19-merge-sorted-array/merge-sorted-array.ts
@@ -0,0 +1,15 @@
+export const mergeSortedArrays = (nums1: number[], nums2: number[]): void => {
+	if (!nums2.length) return;
+	let idx = 0;
+	let m = nums1.length - nums2.length;
+	for (const num of nums2) {
+		while (idx < m) {
+			const curr = nums1[idx] as number;
+			if (curr >= num) break;
+			idx++;
+		}
+		nums1.splice(idx, 0, num);
+		m++;
+	}
+	nums1.splice(-nums2.length);
+};
