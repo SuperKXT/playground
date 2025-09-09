da2c4c2cf8f2910108a4c70a7de6f214d3760c85
SuperKXT <superkxt@outlook.com>
2025-09-09 15:53:30 +0500

    Add solution for `leet-code/easy/13`

--- leet-code/easy/13-is-anagram/is-anagram.test.ts
+++ leet-code/easy/13-is-anagram/is-anagram.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing isAnagram for test 1", () => {
 	const result = isAnagram("anagram", "nagaram"); // cSpell: disable-line
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing isAnagram for test 1", () => {
 
 test("testing isAnagram for test 2", () => {
 	const result = isAnagram("rat", "car");
-	const expected = false as boolean;
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
--- leet-code/easy/13-is-anagram/is-anagram.ts
+++ leet-code/easy/13-is-anagram/is-anagram.ts
@@ -1,5 +1,19 @@
-export const isAnagram = (a: string, b: string): boolean => {
+type TIsAnagram<
+	A extends string,
+	B extends string,
+> = A extends `${infer first}${infer rest}`
+	? B extends `${infer before}${first}${infer after}`
+		? TIsAnagram<rest, `${before}${after}`>
+		: false
+	: B extends ""
+		? true
+		: false;
+
+export const isAnagram = <const A extends string, const B extends string>(
+	a: A,
+	b: B,
+): TIsAnagram<A, B> => {
 	const aSorted = a.split("").sort().join("");
 	const bSorted = b.split("").sort().join("");
-	return aSorted === bSorted;
+	return (aSorted === bSorted) as never;
 };
