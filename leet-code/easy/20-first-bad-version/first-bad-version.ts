4a7f93612c7eb8d8106ac60b98e3ec4495930d92
SuperKXT <superkxt@outlook.com>
2025-09-09 20:55:46 +0500

    Add js solution for `leet-code/easy/20`

---
+++ leet-code/easy/20-first-bad-version/first-bad-version.ts
@@ -0,0 +1,12 @@
+export const firstBadVersion = (isBadVersion: (n: number) => boolean) => {
+	return (n: number): number => {
+		let start = 0;
+		let end = n - 1;
+		while (start <= end) {
+			const mid = Math.floor((start + end) / 2);
+			if (isBadVersion(mid)) end = mid - 1;
+			else start = mid + 1;
+		}
+		return start;
+	};
+};
