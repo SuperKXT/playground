7bbb4c0a02a31586c0bd149d878deb3c20781243
SuperKXT <superkxt@outlook.com>
2025-09-09 21:14:15 +0500

    Add js solution for `leet-code/easy/22`

---
+++ leet-code/easy/22-single-number/single-number.test.ts
@@ -0,0 +1,37 @@
+import { assertType, expect, test } from "vitest";
+
+import { singleNumber } from "./single-number.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing singleNumber for test 1", () => {
+	const result = singleNumber([2, 2, 1]);
+	const expected = 1 as number | null;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing singleNumber for test 2", () => {
+	const result = singleNumber([4, 1, 2, 1, 2]);
+	const expected = 4 as number | null;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing singleNumber for test 3", () => {
+	const result = singleNumber([1]);
+	const expected = 1 as number | null;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing singleNumber for test 3", () => {
+	const result = singleNumber([1, 1]);
+	const expected = null as number | null;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
+	assertType<TTrue>(true);
+});
