7a5fae79b2821b0801b8e792e9cbbe9d6fae8814
SuperKXT <superkxt@outlook.com>
2025-09-12 18:27:04 +0500

    Add js solution for `leet-code/easy/28`

---
+++ leet-code/easy/28-add-two-numbers/add-two-numbers.ts
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/rotate-image
+
+import type { TLinkedListNode } from "../../../helpers/linked-list.helpers.js";
+
+export const addTwoNumbers = (
+	num1: TLinkedListNode<number>,
+	num2: TLinkedListNode<number>,
+	carry: number = 0,
+): TLinkedListNode<number> => {
+	if (!num1 && !num2 && !carry) return null;
+	const currVal = num1?.value ?? 0;
+	const nextVal = num2?.value ?? 0;
+	const sum = carry + currVal + nextVal;
+	return {
+		value: sum % 10,
+		next: addTwoNumbers(
+			num1?.next ?? null,
+			num2?.next ?? null,
+			Math.trunc(sum / 10),
+		),
+	};
+};
