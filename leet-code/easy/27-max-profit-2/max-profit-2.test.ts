fc9f88499f368de18f9b80187e1c7f40d622e201
SuperKXT <superkxt@outlook.com>
2025-09-13 17:17:37 +0500

    Add js solution for `leet-code/easy/14`

--- leet-code/easy/27-max-profit-2/max-profit-2.test.ts
+++ leet-code/easy/27-max-profit-2/max-profit-2.test.ts
@@ -1,12 +1,12 @@
 import { assertType, expect, test } from "vitest";
 
-import { maxProfit } from "./max-profit.js";
+import { maxProfit } from "./max-profit-2.js";
 
 import type { Utils } from "../../../types/utils.types.js";
 
 test("testing maxProfit for test 1", () => {
 	const result = maxProfit([7, 1, 5, 3, 6, 4]);
-	const expected = 5 as number;
+	const expected = 7 as number;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
@@ -30,7 +30,7 @@ test("testing maxProfit for test 3", () => {
 
 test("testing maxProfit for test 4", () => {
 	const result = maxProfit([2, 1, 2, 0, 1]);
-	const expected = 1 as number;
+	const expected = 2 as number;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
@@ -38,7 +38,15 @@ test("testing maxProfit for test 4", () => {
 
 test("testing maxProfit for test 5", () => {
 	const result = maxProfit([2, 1, 2, 1, 0, 1, 2]);
-	const expected = 2 as number;
+	const expected = 3 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing maxProfit for test 6", () => {
+	const result = maxProfit([7, 1, 5, 3, 6, 4]);
+	const expected = 5 as number;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
