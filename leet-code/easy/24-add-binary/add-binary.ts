d2d512b2e51e41b571bc7bd8f3c8174d30b44e8d
SuperKXT <superkxt@outlook.com>
2025-09-13 12:48:15 +0500

    Add js solution for `leet-code/easy/24`

---
+++ leet-code/easy/24-add-binary/add-binary.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/add-binary
+
+// export const addBinary = (a: string, b: string): string => {
+// 	return (parseInt(a, 2) + parseInt(b, 2)).toString(2);
+// };
+
+export const addBinary = (a: string, b: string): string => {
+	let result = "";
+	const length = Math.max(a.length, b.length);
+	let carry = 0;
+	for (let idx = 0; idx < length; idx++) {
+		const currA = Number(a.at(-1 * idx - 1) ?? "0");
+		const currB = Number(b.at(-1 * idx - 1) ?? "0");
+		const sum = carry + currA + currB;
+		carry = Math.trunc(sum / 2);
+		result = `${sum.toString(2).at(-1) ?? "0"}${result}`;
+	}
+	return `${carry === 0 ? "" : carry}${result}`;
+};
