89b069a267755587fb486e964fe24ab9240161aa
SuperKXT <superkxt@outlook.com>
2025-09-13 12:55:46 +0500

    Update `leet-code/easy/24`

--- leet-code/easy/24-add-binary/add-binary.ts
+++ leet-code/easy/24-add-binary/add-binary.ts
@@ -4,16 +4,33 @@
 // 	return (parseInt(a, 2) + parseInt(b, 2)).toString(2);
 // };
 
+// export const addBinary = (a: string, b: string): string => {
+// 	let result = "";
+// 	const length = Math.max(a.length, b.length);
+// 	let carry = 0;
+// 	for (let idx = 0; idx < length; idx++) {
+// 		const currA = Number(a.at(-1 * idx - 1) ?? "0");
+// 		const currB = Number(b.at(-1 * idx - 1) ?? "0");
+// 		const sum = carry + currA + currB;
+// 		carry = Math.trunc(sum / 2);
+// 		result = `${sum.toString(2).at(-1) ?? "0"}${result}`;
+// 	}
+// 	return `${carry === 0 ? "" : carry}${result}`;
+// };
+
 export const addBinary = (a: string, b: string): string => {
 	let result = "";
 	const length = Math.max(a.length, b.length);
-	let carry = 0;
+	let carry = "0";
 	for (let idx = 0; idx < length; idx++) {
-		const currA = Number(a.at(-1 * idx - 1) ?? "0");
-		const currB = Number(b.at(-1 * idx - 1) ?? "0");
-		const sum = carry + currA + currB;
-		carry = Math.trunc(sum / 2);
-		result = `${sum.toString(2).at(-1) ?? "0"}${result}`;
+		const currA = a.at(-1 * idx - 1) ?? "0";
+		const currB = b.at(-1 * idx - 1) ?? "0";
+		if (currA === currB) {
+			result = `${carry}${result}`;
+			carry = currA === "1" ? "1" : "0";
+		} else {
+			result = `${carry === "1" ? "0" : "1"}${result}`;
+		}
 	}
-	return `${carry === 0 ? "" : carry}${result}`;
+	return `${carry === "0" ? "" : carry}${result}`;
 };
