8f33f8719baf4d49d1018d14d2f5e1941780f83c
SuperKXT <superkxt@outlook.com>
2025-09-13 17:05:41 +0500

    Fix ts solution for `leet-code/easy/26`

--- leet-code/easy/26-delete-duplicate-nodes/delete-duplicate-nodes.ts
+++ leet-code/easy/26-delete-duplicate-nodes/delete-duplicate-nodes.ts
@@ -2,16 +2,22 @@
 
 import type { TLinkedListNode } from "../../../helpers/linked-list.helpers.js";
 
+type _TDeleteDuplicateNodes<
+	Node extends TLinkedListNode,
+	Prev extends Exclude<TLinkedListNode, null>,
+> =
+	Node extends Exclude<Node, null>
+		? Node["val"] extends Prev["val"]
+			? _TDeleteDuplicateNodes<Node["next"], Node>
+			: {
+					val: Node["val"];
+					next: _TDeleteDuplicateNodes<Node["next"], Node>;
+				}
+		: null;
+
 type TDeleteDuplicateNodes<Node extends TLinkedListNode> =
 	Node extends Exclude<Node, null>
-		? {
-				val: Node["val"];
-				next: Node["next"] extends Exclude<Node["next"], null>
-					? Node["val"] extends Node["next"]["val"]
-						? TDeleteDuplicateNodes<Node["next"]["next"]>
-						: TDeleteDuplicateNodes<Node["next"]>
-					: null;
-			}
+		? { val: Node["val"]; next: _TDeleteDuplicateNodes<Node["next"], Node> }
 		: null;
 
 export const deleteDuplicateNodes = <Head extends TLinkedListNode<number>>(
