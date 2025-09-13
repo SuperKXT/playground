d80dd187a362c17bf789f64093c36cbcf5c13187
SuperKXT <superkxt@outlook.com>
2025-09-13 17:17:37 +0500

    Add js solution for `leet-code/easy/14`

--- leet-code/easy/14-max-profit/max-profit.ts
+++ leet-code/easy/14-max-profit/max-profit.ts
@@ -1,31 +1,37 @@
-// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
-
-export const maxProfit = (prices: number[]): number => {
-	let start: number | undefined = undefined;
-	let profit = 0;
-	for (let idx = 0; idx < prices.length; idx++) {
-		const curr = prices[idx] as number;
-		const next = prices[idx + 1] ?? -Infinity;
-		if (next < curr) {
-			if (start !== undefined) {
-				profit += curr - start;
-			}
-			start = undefined;
-		} else if (start === undefined) {
-			start = curr;
-		}
-	}
-
-	return profit;
-};
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 
 // export const maxProfit = (prices: number[]): number => {
 // 	let profit = 0;
-// 	for (let idx = 0; idx < prices.length; idx++) {
-// 		const curr = prices[idx] as number;
-// 		const next = prices[idx + 1] ?? -Infinity;
-// 		const diff = next - curr;
-// 		if (diff > 0) profit += diff;
+// 	for (let i = 0; i < prices.length; i++) {
+// 		for (let j = i + 1; j < prices.length; j++) {
+// 			const diff = (prices[j] as number) - (prices[i] as number);
+// 			if (diff > profit) profit = diff;
+// 		}
 // 	}
+
 // 	return profit;
 // };
+
+export const maxProfit = (prices: number[]): number => {
+	const candidates: number[] = [];
+	let max = null as null | number;
+	let min = null as null | number;
+	for (let i = prices.length - 1; i >= 0; i--) {
+		const curr = prices[i] as number;
+		if (max === null || curr > max) {
+			if (max !== null && min !== null) {
+				candidates.push(max - min);
+			}
+			max = curr;
+			min = null;
+			continue;
+		} else if (min === null || curr < min) {
+			min = curr;
+		}
+	}
+	if (max !== null && min !== null) {
+		candidates.push(max - min);
+	}
+
+	return Math.max(0, ...candidates);
+};
---
+++ leet-code/easy/27-max-profit-2/max-profit-2.ts
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+
+export const maxProfit = (prices: number[]): number => {
+	let start: number | undefined = undefined;
+	let profit = 0;
+	for (let idx = 0; idx < prices.length; idx++) {
+		const curr = prices[idx] as number;
+		const next = prices[idx + 1] ?? -Infinity;
+		if (next < curr) {
+			if (start !== undefined) {
+				profit += curr - start;
+			}
+			start = undefined;
+		} else if (start === undefined) {
+			start = curr;
+		}
+	}
+
+	return profit;
+};
+
+// export const maxProfit = (prices: number[]): number => {
+// 	let profit = 0;
+// 	for (let idx = 0; idx < prices.length; idx++) {
+// 		const curr = prices[idx] as number;
+// 		const next = prices[idx + 1] ?? -Infinity;
+// 		const diff = next - curr;
+// 		if (diff > 0) profit += diff;
+// 	}
+// 	return profit;
+// };
