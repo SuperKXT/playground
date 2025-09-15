3abb10b2908fca0af9cbee15e34de25816d2d6f6
SuperKXT <superkxt@outlook.com>
2025-09-15 16:21:44 +0500

    Add js solution for `rwc#422`

---
+++ rendezvous-with-cassidoo/422/analyze-baseball-game.ts
@@ -0,0 +1,30 @@
+type TRes = {
+	homeTotal: number;
+	awayTotal: number;
+	homeLedInnings: number[];
+	awayLedInnings: number[];
+	winner: "home" | "away";
+};
+
+export const analyzeBaseballGame = (nums: [number, number][]): TRes => {
+	const res: TRes = {
+		homeTotal: 0,
+		awayTotal: 0,
+		homeLedInnings: [],
+		awayLedInnings: [],
+		winner: "home",
+	};
+	let inning = 1;
+	for (const [home, away] of nums) {
+		res.homeTotal += home;
+		res.awayTotal += away;
+		if (res.homeTotal > res.awayTotal) {
+			res.homeLedInnings.push(inning);
+		} else if (res.awayTotal > res.homeTotal) {
+			res.awayLedInnings.push(inning);
+		}
+		inning++;
+	}
+	res.winner = res.homeTotal > res.awayTotal ? "home" : "away";
+	return res;
+};
