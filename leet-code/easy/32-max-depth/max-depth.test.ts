08a9f746e50e951c7ac1e5bbc9854ddad893429f
SuperKXT <superkxt@outlook.com>
2025-09-16 16:00:38 +0500

    Add js solution for `leet-code/easy/32`

---
+++ leet-code/easy/32-max-depth/max-depth.test.ts
@@ -0,0 +1,23 @@
+import { assertType, expect, test } from "vitest";
+
+import { maxDepth } from "./max-depth.js";
+
+import { arrayToBinaryTree } from "../../../helpers/binary-tree.helpers.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing maxDepth for test 1", () => {
+	const result = maxDepth(arrayToBinaryTree([3, 9, 20, null, null, 15, 7]));
+	const expected = 3 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing maxDepth for test 3", () => {
+	const result = maxDepth(arrayToBinaryTree([1, null, 2]));
+	const expected = 2 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
