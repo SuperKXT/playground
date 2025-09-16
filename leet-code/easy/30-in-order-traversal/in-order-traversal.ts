bb0604336c8a22e00be2919d0035222b7bb799c9
SuperKXT <superkxt@outlook.com>
2025-09-16 12:59:16 +0500

    Update `leet-code/easy/30`

--- leet-code/easy/30-in-order-traversal/in-order-traversal.ts
+++ leet-code/easy/30-in-order-traversal/in-order-traversal.ts
@@ -2,11 +2,54 @@
 
 import type { TBinaryTreeNode } from "../../../helpers/binary-tree.helpers.js";
 
+// export const inOrderTraversal = (node: TBinaryTreeNode<number>): number[] => {
+// 	if (!node) return [];
+// 	return [
+// 		...inOrderTraversal(node.left),
+// 		node.val,
+// 		...inOrderTraversal(node.right),
+// 	];
+// };
+
+// export const inOrderTraversal = (node: TBinaryTreeNode<number>): number[] => {
+// 	if (!node) return [];
+// 	const res: number[] = [];
+// 	const stack: TBinaryTreeNode<number>[] = [];
+// 	const visited = new Set<TBinaryTreeNode<number>>();
+// 	let curr: TBinaryTreeNode<number> = node;
+// 	while (curr) {
+// 		if (curr.left && !visited.has(curr.left)) {
+// 			stack.push(curr);
+// 			curr = curr.left;
+// 		} else {
+// 			if (!visited.has(curr)) {
+// 				res.push(curr.val);
+// 				visited.add(curr);
+// 			}
+// 			if (curr.right && !visited.has(curr.right)) {
+// 				curr = curr.right;
+// 			} else {
+// 				curr = stack.pop() ?? null;
+// 			}
+// 		}
+// 	}
+// 	return res;
+// };
+
 export const inOrderTraversal = (node: TBinaryTreeNode<number>): number[] => {
 	if (!node) return [];
-	return [
-		...inOrderTraversal(node.left),
-		node.val,
-		...inOrderTraversal(node.right),
-	];
+	const res: number[] = [];
+	const stack: TBinaryTreeNode<number>[] = [];
+	let curr: TBinaryTreeNode<number> = node;
+	while (curr || stack.length > 0) {
+		while (curr) {
+			stack.push(curr);
+			curr = curr.left;
+		}
+		curr = stack.pop() ?? null;
+		if (!curr) continue;
+		res.push(curr.val);
+		curr = curr.right;
+	}
+	return res;
 };
