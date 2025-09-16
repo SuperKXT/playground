5df66c4810a0e1cd9cb18180c0927b32514e8682
SuperKXT <superkxt@outlook.com>
2025-09-16 13:38:50 +0500

    Add `binaryTreeToArray` helper

--- helpers/binary-tree.helpers.ts
+++ helpers/binary-tree.helpers.ts
@@ -19,3 +19,27 @@ export const arrayToBinaryTree = <T>(arr: (T | null)[]): TBinaryTreeNode<T> => {
 
 	return buildTree(0);
 };
+
+export const binaryTreeToArray = <T>(
+	root: TBinaryTreeNode<T>,
+): Array<T | null> => {
+	const arr: Array<T | null> = [];
+
+	const buildArray = (node: TBinaryTreeNode<T>, idx: number) => {
+		if (!node) return;
+		arr[idx] = node.val ?? null;
+		buildArray(node.left, 2 * idx + 1);
+		buildArray(node.right, 2 * idx + 2);
+	};
+
+	buildArray(root, 0);
+	let lastIdx = arr.length - 1;
+	for (lastIdx; lastIdx >= 0; lastIdx--) {
+		const val = arr[lastIdx] ?? null;
+		if (val !== null) break;
+	}
+	arr.splice(lastIdx + 1);
+	for (let idx = 0; idx < arr.length; idx++) arr[idx] ??= null;
+
+	return arr;
+};
