c243329c56d446fe630a9bf4604d5d7a15c1cd41
SuperKXT <superkxt@outlook.com>
2025-09-17 14:23:17 +0500

    Update `leet-code/easy/37`

--- leet-code/easy/37-one-bits/one-bits.ts
+++ leet-code/easy/37-one-bits/one-bits.ts
@@ -1,10 +1,21 @@
 // https://leetcode.com/problems/number-of-1-bits
 
+// export const oneBits = (num: number): number => {
+// 	const str = num.toString(2);
+// 	let count = 0;
+// 	for (const char of str) {
+// 		if (char === "1") count++;
+// 	}
+// 	return count;
+// };
+
 export const oneBits = (num: number): number => {
-	const str = num.toString(2);
+	let curr = num;
 	let count = 0;
-	for (const char of str) {
-		if (char === "1") count++;
+	while (curr > 0) {
+		// eslint-disable-next-line no-bitwise
+		curr &= curr - 1;
+		count++;
 	}
 	return count;
 };
