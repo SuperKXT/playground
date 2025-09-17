526921056059b7348e15277eb0e535c7a3189ac5
SuperKXT <superkxt@outlook.com>
2025-09-17 15:43:15 +0500

    Add js solution for `leet-code/easy/39`

---
+++ leet-code/easy/39-happy-number/happy-number.ts
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists
+
+export const happyNumber = (num: number): boolean => {
+	const set = new Set<number>();
+	let curr = num;
+	while (curr) {
+		if (curr === 1) return true;
+		if (set.has(curr)) break;
+		set.add(curr);
+		const digits = curr.toString().split("");
+		let sum = 0;
+		for (const digit of digits) {
+			sum += parseInt(digit) ** 2;
+		}
+		curr = sum;
+	}
+	return false;
+};
