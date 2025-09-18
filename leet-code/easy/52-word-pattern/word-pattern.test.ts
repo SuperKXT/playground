d0a2ee9b28bba1f5686725cc10aad5134a77e157
SuperKXT <superkxt@outlook.com>
2025-09-18 16:32:37 +0500

    Add ts solution for `leet-code/easy/52`

--- leet-code/easy/52-word-pattern/word-pattern.test.ts
+++ leet-code/easy/52-word-pattern/word-pattern.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing wordPattern for test 1", () => {
 	const result = wordPattern("abba", "dog cat cat dog");
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing wordPattern for test 1", () => {
 
 test("testing wordPattern for test 2", () => {
 	const result = wordPattern("abba", "dog cat cat fish");
-	const expected = false as boolean;
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -22,7 +22,7 @@ test("testing wordPattern for test 2", () => {
 
 test("testing wordPattern for test 3", () => {
 	const result = wordPattern("aaaa", "dog cat cat dog");
-	const expected = false as boolean;
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -30,7 +30,7 @@ test("testing wordPattern for test 3", () => {
 
 test("testing wordPattern for test 4", () => {
 	const result = wordPattern("abba", "dog dog dog dog");
-	const expected = false as boolean;
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
