415e6fdd82583598c0d3d847d42725c75e7b0d38
SuperKXT <superkxt@outlook.com>
2025-09-18 15:33:33 +0500

    Add ts solution for `leet-code/easy/49`

--- leet-code/easy/49-palindrome-list/palindrome-list.ts
+++ leet-code/easy/49-palindrome-list/palindrome-list.ts
@@ -2,14 +2,31 @@
 
 import type { TLinkedListNode } from "../../../helpers/linked-list.helpers.js";
 
-export const palindromeList = (head: TLinkedListNode<number>): boolean => {
+type TPalindromeList<
+	Node extends TLinkedListNode<number>,
+	forward extends string = "",
+	reverse extends string = "",
+> =
+	Node extends NonNullable<TLinkedListNode>
+		? TPalindromeList<
+				Node["next"],
+				`${forward}${Node["val"]}`,
+				`${Node["val"]}${reverse}`
+			>
+		: forward extends reverse
+			? true
+			: false;
+
+export const palindromeList = <Head extends TLinkedListNode<number>>(
+	head: Head,
+): TPalindromeList<Head> => {
 	let forward = "";
 	let reverse = "";
-	let curr = head;
+	let curr: TLinkedListNode<number> = head;
 	while (curr) {
 		forward += curr.val.toString();
 		reverse = curr.val.toString() + reverse;
 		curr = curr.next;
 	}
-	return forward === reverse;
+	return (forward === reverse) as never;
 };
