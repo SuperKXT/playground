aaf90c82b0a5b0832c68faed7d2dded232692144
SuperKXT <superkxt@outlook.com>
2025-09-18 16:19:52 +0500

    Add js solution for `leet-code/easy/52`

---
+++ leet-code/easy/52-word-pattern/word-pattern.test.ts
@@ -0,0 +1,37 @@
+import { assertType, expect, test } from "vitest";
+
+import { wordPattern } from "./word-pattern.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing wordPattern for test 1", () => {
+	const result = wordPattern("abba", "dog cat cat dog");
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing wordPattern for test 2", () => {
+	const result = wordPattern("abba", "dog cat cat fish");
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing wordPattern for test 3", () => {
+	const result = wordPattern("aaaa", "dog cat cat dog");
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing wordPattern for test 4", () => {
+	const result = wordPattern("abba", "dog dog dog dog");
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
