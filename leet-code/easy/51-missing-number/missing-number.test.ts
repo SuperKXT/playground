01769bf0a2e0e1afc59488a53303e1d9121472db
SuperKXT <superkxt@outlook.com>
2025-09-18 16:03:22 +0500

    Add js solution for `leet-code/easy/51`

---
+++ leet-code/easy/51-missing-number/missing-number.test.ts
@@ -0,0 +1,29 @@
+import { assertType, expect, test } from "vitest";
+
+import { missingNumber } from "./missing-number.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing missingNumber for test 1", () => {
+	const result = missingNumber([3, 0, 1]);
+	const expected = 2 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing missingNumber for test 2", () => {
+	const result = missingNumber([0, 1]);
+	const expected = 2 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing missingNumber for test 2", () => {
+	const result = missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]);
+	const expected = 8 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
