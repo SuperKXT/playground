5a9009024029749dcb938411c3d4917d5b31dea4
SuperKXT <superkxt@outlook.com>
2025-09-18 16:07:41 +0500

    Add ts solution for `leet-code/easy/51`

--- leet-code/easy/51-missing-number/missing-number.ts
+++ leet-code/easy/51-missing-number/missing-number.ts
@@ -1,11 +1,24 @@
 // https://leetcode.com/problems/missing-number
 
-export const missingNumber = (nums: number[]): number => {
+type TMissingNumber<
+	Nums extends number[],
+	size extends number = Nums["length"],
+	union extends number = Nums[number],
+	idx extends 1[] = [],
+> = idx["length"] extends size
+	? idx["length"]
+	: idx["length"] extends union
+		? TMissingNumber<never, size, union, [...idx, 1]>
+		: idx["length"];
+
+export const missingNumber = <const Nums extends number[]>(
+	nums: Nums,
+): TMissingNumber<Nums> => {
 	let expectedSum = 0;
 	let sum = 0;
 	for (let idx = 0; idx < nums.length; idx++) {
 		expectedSum += idx + 1;
 		sum += nums[idx] as number;
 	}
-	return expectedSum - sum;
+	return (expectedSum - sum) as never;
 };
