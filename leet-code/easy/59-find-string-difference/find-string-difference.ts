db465dfe42eeaa2566f8a03c31bacbcb155ae3b4
SuperKXT <superkxt@outlook.com>
2025-09-20 17:02:39 +0500

    Add ts solution for `leet-code/easy/59`

--- leet-code/easy/59-find-string-difference/find-string-difference.ts
+++ leet-code/easy/59-find-string-difference/find-string-difference.ts
@@ -4,13 +4,25 @@
 // 	return t.replace(new RegExp(`[${s}]`, "gu"), "");
 // };
 
-export const findStringDifference = (s: string, t: string): string => {
+type TFindStringDifference<
+	S extends string,
+	T extends string,
+> = T extends `${infer first}${infer rest}`
+	? S extends `${infer before}${first}${infer after}`
+		? TFindStringDifference<`${before}${after}`, rest>
+		: first
+	: never;
+
+export const findStringDifference = <S extends string, T extends string>(
+	s: S,
+	t: T,
+): TFindStringDifference<S, T> => {
 	const map = new Map<string, number>();
 	for (const char of s) map.set(char, (map.get(char) ?? 0) + 1);
 	for (const char of t) {
 		const count = map.get(char) ?? 0;
 		if (count > 0) map.set(char, count - 1);
-		else return char;
+		else return char as never;
 	}
 	throw new Error("No difference");
 };
