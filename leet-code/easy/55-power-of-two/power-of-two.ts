dbff52e76b26e815aeb9296ae83db5c8815a1d91
SuperKXT <superkxt@outlook.com>
2025-09-20 16:25:48 +0500

    Update `leet-code/easy/55`

--- leet-code/easy/55-power-of-two/power-of-two.ts
+++ leet-code/easy/55-power-of-two/power-of-two.ts
@@ -1,5 +1,15 @@
 // https://leetcode.com/problems/power-of-two
 
+// export const powerOfTwo = (n: number): boolean => {
+// 	if (n < 1) return false;
+// 	let curr = 1;
+// 	while (curr <= n) {
+// 		if (curr === n) return true;
+// 		curr *= 2;
+// 	}
+// 	return false;
+// };
+
 export const powerOfTwo = (n: number): boolean => {
 	return Number.isInteger(Math.log2(n));
 };
