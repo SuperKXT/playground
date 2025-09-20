c2bfbe2e8a2f586a635b8632ffd3d7b9adbb2118
SuperKXT <superkxt@outlook.com>
2025-09-20 16:38:44 +0500

    Update `leet-code/easy/57`

--- leet-code/easy/57-power-of-three/power-of-three.ts
+++ leet-code/easy/57-power-of-three/power-of-three.ts
@@ -1,11 +1,17 @@
 // https://leetcode.com/problems/power-of-three
 
+// export const powerOfThree = (n: number): boolean => {
+// 	if (n < 1) return false;
+// 	let curr = 1;
+// 	while (curr <= n) {
+// 		if (curr === n) return true;
+// 		curr *= 3;
+// 	}
+// 	return false;
+// };
+
 export const powerOfThree = (n: number): boolean => {
 	if (n < 1) return false;
-	let curr = 1;
-	while (curr <= n) {
-		if (curr === n) return true;
-		curr *= 3;
-	}
-	return false;
+	const base3 = n.toString(3);
+	return /^10*$/u.test(base3);
 };
