4ed90d88e8ea22f2dbb2ed39d2d4bda238d2c36c
SuperKXT <superkxt@outlook.com>
2025-09-21 13:55:12 +0500

    Add js solution for `leet-code/easy/61`

---
+++ leet-code/easy/61-ransom-note/ransom-note.test.ts
@@ -0,0 +1,29 @@
+import { assertType, expect, test } from "vitest";
+
+import { ransomNote } from "./ransom-note.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing ransomNote for test 1", () => {
+	const result = ransomNote("a", "b");
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing ransomNote for test 2", () => {
+	const result = ransomNote("aa", "ab");
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing ransomNote for test 3", () => {
+	const result = ransomNote("aa", "aab");
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
