c55d74db60bb1172db4d9ebb8352223bdece9243
SuperKXT <superkxt@outlook.com>
2025-09-21 13:56:56 +0500

    Add ts solution for `leet-code/easy/61`

--- leet-code/easy/61-ransom-note/ransom-note.ts
+++ leet-code/easy/61-ransom-note/ransom-note.ts
@@ -1,12 +1,24 @@
 // https://leetcode.com/problems/ransom-note
 
-export const ransomNote = (note: string, magazine: string): boolean => {
+type TRansomNote<
+	Note extends string,
+	Magazine extends string,
+> = Note extends `${infer first}${infer rest}`
+	? Magazine extends `${infer before}${first}${infer after}`
+		? TRansomNote<rest, `${before}${after}`>
+		: false
+	: true;
+
+export const ransomNote = <Note extends string, Magazine extends string>(
+	note: Note,
+	magazine: Magazine,
+): TRansomNote<Note, Magazine> => {
 	const map = new Map<string, number>();
 	for (const char of magazine) map.set(char, (map.get(char) ?? 0) + 1);
 	for (const char of note) {
 		const existing = map.get(char) ?? 0;
-		if (!existing) return false;
+		if (!existing) return false as never;
 		map.set(char, existing - 1);
 	}
-	return true;
+	return true as never;
 };
