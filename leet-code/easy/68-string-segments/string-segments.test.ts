734c6b2f7a07f17f5d18c25c48ebe734fde41433
SuperKXT <superkxt@outlook.com>
2025-09-27 17:07:45 +0500

    Add ts solution for `leet-code/easy/68`

--- leet-code/easy/68-string-segments/string-segments.test.ts
+++ leet-code/easy/68-string-segments/string-segments.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing thirdMaxNumber for test 1", () => {
 	const result = stringSegments("Hello, my name is John");
-	const expected = 5 as number;
+	const expected = 5;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing thirdMaxNumber for test 1", () => {
 
 test("testing thirdMaxNumber for test 2", () => {
 	const result = stringSegments("Hello");
-	const expected = 1 as number;
+	const expected = 1;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -22,7 +22,7 @@ test("testing thirdMaxNumber for test 2", () => {
 
 test("testing thirdMaxNumber for test 3", () => {
 	const result = stringSegments("");
-	const expected = 0 as number;
+	const expected = 0;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
