44493226c08f00c5b09e2988aa6d7bdd0b809555
SuperKXT <superkxt@outlook.com>
2025-09-27 17:54:33 +0500

    Add ts solution for `leet-code/easy/69`

--- leet-code/easy/69-disappeared-number/disappeared-number.ts
+++ leet-code/easy/69-disappeared-number/disappeared-number.ts
@@ -1,13 +1,30 @@
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
 
-// export const disappearedNumbers = (nums: number[]): number[] => {
+type TDisappearedNumbers<
+	Nums extends number[],
+	idx extends 1[] = [],
+	missing extends number[] = [],
+	curr extends number = [...idx, 1]["length"],
+> = idx["length"] extends Nums["length"]
+	? missing
+	: TDisappearedNumbers<
+			Nums,
+			[...idx, 1],
+			curr extends Nums[number] ? missing : [...missing, curr]
+		>;
+
+// export const disappearedNumbers = <const Nums extends number[]>(
+// 	nums: Nums,
+// ): TDisappearedNumbers<Nums> => {
 // 	const set = new Set<number>();
 // 	for (let i = 1; i <= nums.length; i++) set.add(i);
 // 	for (const num of nums) set.delete(num);
-// 	return Array.from(set);
+// 	return Array.from(set) as never;
 // };
 
-export const disappearedNumbers = (nums: number[]): number[] => {
+export const disappearedNumbers = <const Nums extends number[]>(
+	nums: Nums,
+): TDisappearedNumbers<Nums> => {
 	const missing: number[] = [];
 	for (const num of nums) {
 		const idx = Math.abs(num) - 1;
@@ -16,5 +33,5 @@ export const disappearedNumbers = (nums: number[]): number[] => {
 	for (let idx = 1; idx <= nums.length; idx++) {
 		if ((nums[idx - 1] as number) > 0) missing.push(idx);
 	}
-	return missing;
+	return missing as never;
 };
