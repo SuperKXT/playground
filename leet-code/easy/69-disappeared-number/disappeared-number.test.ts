8bf7f2187fa67b39cc3a8313a0ec0d1b33092d92
SuperKXT <superkxt@outlook.com>
2025-09-27 17:54:33 +0500

    Add ts solution for `leet-code/easy/69`

--- leet-code/easy/69-disappeared-number/disappeared-number.test.ts
+++ leet-code/easy/69-disappeared-number/disappeared-number.test.ts
@@ -6,16 +6,16 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing thirdMaxNumber for test 1", () => {
 	const result = disappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]);
-	const expected = [5, 6];
+	const expected = [5, 6] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
 
 test("testing thirdMaxNumber for test 2", () => {
 	const result = disappearedNumbers([1, 1]);
-	const expected = [2];
+	const expected = [2] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
