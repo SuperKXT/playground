ba8aff450921444e801dbe5dd957d77a7fd494e6
SuperKXT <superkxt@outlook.com>
2025-09-28 17:27:49 +0500

    Update `leet-code/easy/74`

--- leet-code/easy/74-arrange-coins/arrange-coins.test.ts
+++ leet-code/easy/74-arrange-coins/arrange-coins.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing arrangeCoins for test 1", () => {
 	const result = arrangeCoins(5);
-	const expected = 2;
+	const expected = 2 as number;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,15 @@ test("testing arrangeCoins for test 1", () => {
 
 test("testing arrangeCoins for test 2", () => {
 	const result = arrangeCoins(8);
-	const expected = 3;
+	const expected = 3 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing arrangeCoins for test 3", () => {
+	const result = arrangeCoins(1681692777);
+	const expected = 57994 as number;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
--- leet-code/easy/74-arrange-coins/arrange-coins.ts
+++ leet-code/easy/74-arrange-coins/arrange-coins.ts
@@ -1,28 +1,41 @@
 // https://leetcode.com/problems/arranging-coins
 
-type TArrangeCoins<
-	N extends number,
-	required extends 1[] = [1],
-	curr extends 1[] = [],
-	rows extends 1[] = [],
-	idx extends 1[] = [],
-	next extends 1[] = [...curr, 1],
-> = idx["length"] extends N
-	? rows["length"]
-	: next["length"] extends required["length"]
-		? TArrangeCoins<N, [...required, 1], [], [...rows, 1], [...idx, 1]>
-		: TArrangeCoins<N, required, next, rows, [...idx, 1]>;
+// type TArrangeCoins<
+// 	N extends number,
+// 	required extends 1[] = [1],
+// 	curr extends 1[] = [],
+// 	rows extends 1[] = [],
+// 	idx extends 1[] = [],
+// 	next extends 1[] = [...curr, 1],
+// > = idx["length"] extends N
+// 	? rows["length"]
+// 	: next["length"] extends required["length"]
+// 		? TArrangeCoins<N, [...required, 1], [], [...rows, 1], [...idx, 1]>
+// 		: TArrangeCoins<N, required, next, rows, [...idx, 1]>;
 
-export const arrangeCoins = <N extends number>(n: N): TArrangeCoins<N> => {
-	const row = { required: 1, curr: 0 };
+// ! too slow
+// export const arrangeCoins = <N extends number>(n: N): TArrangeCoins<N> => {
+// 	const row = { required: 1, curr: 0 };
+// 	let rows = 0;
+// 	for (let i = 1; i <= n; i++) {
+// 		row.curr++;
+// 		if (row.curr === row.required) {
+// 			row.curr = 0;
+// 			row.required++;
+// 			rows++;
+// 		}
+// 	}
+// 	return rows as never;
+// };
+
+export const arrangeCoins = (n: number): number => {
+	let remaining = n;
+	let required = 1;
 	let rows = 0;
-	for (let i = 1; i <= n; i++) {
-		row.curr++;
-		if (row.curr === row.required) {
-			row.curr = 0;
-			row.required++;
-			rows++;
-		}
+	while (remaining >= required) {
+		rows++;
+		remaining -= required;
+		required++;
 	}
-	return rows as never;
+	return rows;
 };
