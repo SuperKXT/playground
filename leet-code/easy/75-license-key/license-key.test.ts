f6534a5840ef56c8e482632abbd8e8a62999257d
SuperKXT <superkxt@outlook.com>
2025-09-28 18:02:56 +0500

    Add ts solution for `leet-code/easy/75`

--- leet-code/easy/75-license-key/license-key.test.ts
+++ leet-code/easy/75-license-key/license-key.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing licenseKey for test 1", () => {
 	const result = licenseKey("5F3Z-2e-9-w", 4);
-	const expected = "5F3Z-2E9W" as string;
+	const expected = "5F3Z-2E9W";
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing licenseKey for test 1", () => {
 
 test("testing licenseKey for test 2", () => {
 	const result = licenseKey("2-5g-3-J", 2);
-	const expected = "2-5G-3J" as string;
+	const expected = "2-5G-3J";
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -22,7 +22,7 @@ test("testing licenseKey for test 2", () => {
 
 test("testing licenseKey for test 3", () => {
 	const result = licenseKey("2-4A0r7-4k", 4);
-	const expected = "24A0-R74K" as string;
+	const expected = "24A0-R74K";
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -30,7 +30,7 @@ test("testing licenseKey for test 3", () => {
 
 test("testing licenseKey for test 4", () => {
 	const result = licenseKey("2", 4);
-	const expected = "2" as string;
+	const expected = "2";
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
--- leet-code/easy/75-license-key/license-key.ts
+++ leet-code/easy/75-license-key/license-key.ts
@@ -1,6 +1,75 @@
 // https://leetcode.com/problems/license-key-formatting
 
-export const licenseKey = (s: string, k: number): string => {
+type TReverse<S extends string> = S extends `${infer first}${infer rest}`
+	? `${TReverse<rest>}${first}`
+	: S;
+
+type TDigit = `0` | `1` | `2` | `3` | `4` | `5` | `6` | `7` | `8` | `9`;
+type TLowercase =
+	| `a`
+	| `b`
+	| `c`
+	| `d`
+	| `e`
+	| `f`
+	| `g`
+	| `h`
+	| `i`
+	| `j`
+	| `k`
+	| `l`
+	| `m`
+	| `n`
+	| `o`
+	| `p`
+	| `q`
+	| `r`
+	| `s`
+	| `t`
+	| `u`
+	| `v`
+	| `w`
+	| `x`
+	| `y`
+	| `z`;
+
+type TUppercase = Uppercase<TLowercase>;
+
+type TLicenseKey<
+	S extends string,
+	K extends number,
+	reversed extends string = TReverse<S>,
+	curr extends string = "",
+	len extends 1[] = [1],
+	res extends string = "",
+> = reversed extends `${infer first}${infer rest}`
+	? first extends TDigit | TLowercase | TUppercase
+		? len["length"] extends K
+			? TLicenseKey<
+					never,
+					K,
+					rest,
+					"",
+					[1],
+					`${Uppercase<first>}${curr}${res extends "" ? "" : `-${res}`}`
+				>
+			: TLicenseKey<
+					never,
+					K,
+					rest,
+					`${Uppercase<first>}${curr}`,
+					[...len, 1],
+					res
+				>
+		: TLicenseKey<never, K, rest, curr, len, res>
+	: curr extends ""
+		? res
+		: `${curr}${res extends "" ? "" : `-${res}`}`;
+
+export const licenseKey = <S extends string, K extends number>(
+	s: S,
+	k: K,
+): TLicenseKey<S, K> => {
 	let curr = "";
 	let res = "";
 	for (let id = s.length - 1; id >= 0; id--) {
@@ -17,5 +86,5 @@ export const licenseKey = (s: string, k: number): string => {
 		}
 	}
 	if (curr) res = `${curr}${res === "" ? "" : `-${res}`}`;
-	return res;
+	return res as never;
 };
