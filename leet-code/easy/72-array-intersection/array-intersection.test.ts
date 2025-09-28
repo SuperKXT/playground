2e671680d7f0387408363c105f1e8e693ad1bee0
SuperKXT <superkxt@outlook.com>
2025-09-28 15:29:12 +0500

    Add ts solution for `leet-code/easy/72`

--- leet-code/easy/72-array-intersection/array-intersection.test.ts
+++ leet-code/easy/72-array-intersection/array-intersection.test.ts
@@ -6,16 +6,16 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing perfectSquare for test 1", () => {
 	const result = arrayIntersection([1, 2, 2, 1], [2, 2]);
-	const expected = [2];
+	const expected = [2] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
 
 test("testing perfectSquare for test 2", () => {
 	const result = arrayIntersection([4, 9, 5], [9, 4, 9, 8, 4]);
-	const expected = [9, 4];
+	const expected = [9, 4] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
