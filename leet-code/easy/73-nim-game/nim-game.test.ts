b64cece479bdc4915fd6a50a90f4e928833ca39e
SuperKXT <superkxt@outlook.com>
2025-09-28 16:36:04 +0500

    Add ts solution for `leet-code/easy/73`

--- leet-code/easy/73-nim-game/nim-game.test.ts
+++ leet-code/easy/73-nim-game/nim-game.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing nimGame for test 1", () => {
 	const result = nimGame(4);
-	const expected = false as boolean;
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing nimGame for test 1", () => {
 
 test("testing nimGame for test 2", () => {
 	const result = nimGame(5);
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -22,7 +22,7 @@ test("testing nimGame for test 2", () => {
 
 test("testing nimGame for test 3", () => {
 	const result = nimGame(1);
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -30,7 +30,7 @@ test("testing nimGame for test 3", () => {
 
 test("testing nimGame for test 4", () => {
 	const result = nimGame(2);
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
--- leet-code/easy/73-nim-game/nim-game.ts
+++ leet-code/easy/73-nim-game/nim-game.ts
@@ -1,5 +1,39 @@
 // https://leetcode.com/problems/nim-game
 
-export const nimGame = (n: number): boolean => {
-	return n % 4 !== 0;
+type TDivisibleBy4 =
+	| `04`
+	| `08`
+	| `12`
+	| `16`
+	| `20`
+	| `24`
+	| `28`
+	| `32`
+	| `36`
+	| `40`
+	| `44`
+	| `48`
+	| `52`
+	| `56`
+	| `60`
+	| `64`
+	| `68`
+	| `72`
+	| `76`
+	| `80`
+	| `84`
+	| `88`
+	| `92`
+	| `96`
+	| `100`;
+
+type TNimGame<N extends number> = `${N}` extends
+	| `4`
+	| `8`
+	| `${string}${TDivisibleBy4}`
+	? false
+	: true;
+
+export const nimGame = <N extends number>(n: N): TNimGame<N> => {
+	return (n % 4 !== 0) as never;
 };
