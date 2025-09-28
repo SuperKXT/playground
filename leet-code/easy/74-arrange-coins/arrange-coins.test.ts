b9f7f219c56f0f5714b7f74e64a62a12707373c2
SuperKXT <superkxt@outlook.com>
2025-09-28 17:05:44 +0500

    Add js solution for `leet-code/easy/74`

---
+++ leet-code/easy/74-arrange-coins/arrange-coins.test.ts
@@ -0,0 +1,21 @@
+import { assertType, expect, test } from "vitest";
+
+import { arrangeCoins } from "./arrange-coins.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing arrangeCoins for test 1", () => {
+	const result = arrangeCoins(5);
+	const expected = 2 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing arrangeCoins for test 2", () => {
+	const result = arrangeCoins(8);
+	const expected = 3 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
