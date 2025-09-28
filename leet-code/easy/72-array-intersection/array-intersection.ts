a0ec9d64d6c944eaa1462348a4fdd1c747cd2499
SuperKXT <superkxt@outlook.com>
2025-09-28 15:29:12 +0500

    Add ts solution for `leet-code/easy/72`

--- leet-code/easy/72-array-intersection/array-intersection.ts
+++ leet-code/easy/72-array-intersection/array-intersection.ts
@@ -1,18 +1,38 @@
 // https://leetcode.com/problems/intersection-of-two-arrays
 
-// export const arrayIntersection = (
-// 	nums1: number[],
-// 	nums2: number[],
-// ): number[] => {
+type TArrayIntersection<
+	Nums1 extends number[],
+	Nums2 extends number[],
+	res extends number[] = [],
+> = Nums1 extends [infer first extends number, ...infer rest extends number[]]
+	? first extends Nums2[number]
+		? TArrayIntersection<
+				rest,
+				Nums2,
+				first extends res[number] ? res : [first, ...res]
+			>
+		: TArrayIntersection<rest, Nums2, res>
+	: res;
+
+// export const arrayIntersection = <
+// 	const Nums1 extends number[],
+// 	const Nums2 extends number[],
+// >(
+// 	nums1: Nums1,
+// 	nums2: Nums2,
+// ): TArrayIntersection<Nums1, Nums2> => {
 // 	const set1 = new Set(nums1);
 // 	const set2 = new Set(nums2);
-// 	return Array.from(set2.intersection(set1));
+// 	return Array.from(set2.intersection(set1)) as never;
 // };
 
-export const arrayIntersection = (
-	nums1: number[],
-	nums2: number[],
-): number[] => {
+export const arrayIntersection = <
+	const Nums1 extends number[],
+	const Nums2 extends number[],
+>(
+	nums1: Nums1,
+	nums2: Nums2,
+): TArrayIntersection<Nums1, Nums2> => {
 	const set = new Set(nums1);
 	const res: number[] = [];
 	for (const num of nums2) {
@@ -21,5 +41,5 @@ export const arrayIntersection = (
 			set.delete(num);
 		}
 	}
-	return res;
+	return res as never;
 };
