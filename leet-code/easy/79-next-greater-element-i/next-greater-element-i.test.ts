d208d0880d9d813ea1ef9848cc8fc7b391e13ccd
SuperKXT <superkxt@outlook.com>
2025-09-29 19:31:40 +0500

    Add ts solution for `leet-code/easy/79`

--- leet-code/easy/79-next-greater-element-i/next-greater-element-i.test.ts
+++ leet-code/easy/79-next-greater-element-i/next-greater-element-i.test.ts
@@ -6,16 +6,16 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing nextGreaterElement for test 1", () => {
 	const result = nextGreaterElement([4, 1, 2], [1, 3, 4, 2]);
-	const expected = [-1, 3, -1] as number[];
+	const expected = [-1, 3, -1] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
 
 test("testing nextGreaterElement for test 2", () => {
 	const result = nextGreaterElement([2, 4], [1, 2, 3, 4]);
-	const expected = [3, -1] as number[];
+	const expected = [3, -1] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
