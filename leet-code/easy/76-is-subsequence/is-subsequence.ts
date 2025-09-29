941d98679f953900db8ea39dc91d8eb3438fa142
SuperKXT <superkxt@outlook.com>
2025-09-29 17:53:23 +0500

    Add ts solution for leet-code/easy/76`

--- leet-code/easy/76-is-subsequence/is-subsequence.ts
+++ leet-code/easy/76-is-subsequence/is-subsequence.ts
@@ -1,10 +1,22 @@
-// https://leetcode.com/problems/license-key-formatting
+// https://leetcode.com/problems/is-subsequence
 
-export const isSubsequence = (s: string, t: string): boolean => {
+type TIsSubsequence<
+	S extends string,
+	T extends string,
+> = S extends `${infer first}${infer sRest}`
+	? T extends `${string}${first}${infer tRest}`
+		? TIsSubsequence<sRest, tRest>
+		: false
+	: true;
+
+export const isSubsequence = <S extends string, T extends string>(
+	s: S,
+	t: T,
+): TIsSubsequence<S, T> => {
 	let idx = 0;
 	for (const char of t) {
 		if (idx === s.length) break;
 		if (char === s[idx]) idx++;
 	}
-	return idx === s.length;
+	return (idx === s.length) as never;
 };
