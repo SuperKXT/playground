5b80a51098c86d92082d43b7bafadc12403fc728
SuperKXT <superkxt@outlook.com>
2025-09-30 19:49:06 +0500

    Add ts solution for `leet-code/easy/86`

--- leet-code/easy/86-reverse-string-ii/reverse-string-ii.test.ts
+++ leet-code/easy/86-reverse-string-ii/reverse-string-ii.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing detectCapital for test 1", () => {
 	const result = reverseString("abcdefg", 2); // cSpell: disable-line
-	const expected = "bacdfeg" as string; // cSpell: disable-line
+	const expected = "bacdfeg"; // cSpell: disable-line
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing detectCapital for test 1", () => {
 
 test("testing detectCapital for test 2", () => {
 	const result = reverseString("abcd", 2); // cSpell: disable-line
-	const expected = "bacd" as string; // cSpell: disable-line
+	const expected = "bacd"; // cSpell: disable-line
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
