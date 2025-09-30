c3135f6a94f0b6b4916d17afd3d998b31b3912ca
SuperKXT <superkxt@outlook.com>
2025-09-30 19:49:06 +0500

    Add ts solution for `leet-code/easy/86`

--- leet-code/easy/86-reverse-string-ii/reverse-string-ii.ts
+++ leet-code/easy/86-reverse-string-ii/reverse-string-ii.ts
@@ -1,6 +1,54 @@
 // https://leetcode.com/problems/reverse-string-ii
 
-export const reverseString = (s: string, k: number): string => {
+type TReverseString<
+	S extends string,
+	K extends number,
+	res extends string = "",
+	curr extends { reverse: string; rLen: 1[]; straight: string; sLen: 1[] } = {
+		reverse: "";
+		rLen: [];
+		straight: "";
+		sLen: [];
+	},
+> = S extends `${infer first}${infer rest}`
+	? [1, ...curr["sLen"]]["length"] extends K
+		? TReverseString<
+				rest,
+				K,
+				`${res}${curr["reverse"]}${curr["straight"]}${first}`,
+				{ reverse: ""; rLen: []; straight: ""; sLen: [] }
+			>
+		: curr["rLen"]["length"] extends K
+			? TReverseString<
+					rest,
+					K,
+					res,
+					{
+						reverse: curr["reverse"];
+						rLen: curr["rLen"];
+						straight: `${curr["straight"]}${first}`;
+						sLen: [...curr["sLen"], 1];
+					}
+				>
+			: TReverseString<
+					rest,
+					K,
+					res,
+					{
+						reverse: `${first}${curr["reverse"]}`;
+						rLen: [...curr["rLen"], 1];
+						straight: curr["straight"];
+						sLen: curr["sLen"];
+					}
+				>
+	: curr["reverse"] extends ""
+		? res
+		: `${res}${curr["reverse"]}${curr["straight"]}`;
+
+export const reverseString = <S extends string, K extends number>(
+	s: S,
+	k: K,
+): TReverseString<S, K> => {
 	const curr = { reverse: "", straight: "" };
 	let reverse = "";
 	for (const char of s) {
@@ -16,5 +64,5 @@ export const reverseString = (s: string, k: number): string => {
 		}
 	}
 	if (curr.reverse.length) reverse += `${curr.reverse}${curr.straight}`;
-	return reverse;
+	return reverse as never;
 };
