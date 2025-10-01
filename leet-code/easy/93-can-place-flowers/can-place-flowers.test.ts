a14c85227f79db835ac05b66aacbee0435919e84
SuperKXT <superkxt@outlook.com>
2025-10-01 18:41:35 +0500

    Add ts solution for `leet-code/easy/93`

--- leet-code/easy/93-can-place-flowers/can-place-flowers.test.ts
+++ leet-code/easy/93-can-place-flowers/can-place-flowers.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing canPlaceFlowers for test 1", () => {
 	const result = canPlaceFlowers([1, 0, 0, 0, 1], 1);
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing canPlaceFlowers for test 1", () => {
 
 test("testing canPlaceFlowers for test 2", () => {
 	const result = canPlaceFlowers([1, 0, 0, 0, 1], 2);
-	const expected = false as boolean;
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -22,7 +22,7 @@ test("testing canPlaceFlowers for test 2", () => {
 
 test("testing canPlaceFlowers for test 3", () => {
 	const result = canPlaceFlowers([1, 0, 1, 0, 1, 0, 1], 0);
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -30,7 +30,7 @@ test("testing canPlaceFlowers for test 3", () => {
 
 test("testing canPlaceFlowers for test 3", () => {
 	const result = canPlaceFlowers([0, 1, 0, 1, 0, 1, 0, 0], 1);
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
--- leet-code/easy/93-can-place-flowers/can-place-flowers.ts
+++ leet-code/easy/93-can-place-flowers/can-place-flowers.ts
@@ -1,7 +1,37 @@
 // https://leetcode.com/problems/can-place-flowers
 
-export const canPlaceFlowers = (flowerbed: number[], n: number): boolean => {
-	if (n === 0) return true;
+type TCanPlaceFlowers<
+	Flowerbed extends number[],
+	N extends number,
+	prev extends number = 0,
+	count extends 1[] = [],
+> = count["length"] extends N
+	? true
+	: Flowerbed extends [
+				infer first extends number,
+				infer second extends number,
+				...infer rest extends number[],
+		  ]
+		? prev | first | second extends 0
+			? TCanPlaceFlowers<[second, ...rest], N, first, [...count, 1]>
+			: TCanPlaceFlowers<[second, ...rest], N, first, count>
+		: Flowerbed extends [
+					infer first extends number,
+					...infer rest extends number[],
+			  ]
+			? first | prev extends 0
+				? TCanPlaceFlowers<rest, N, first, [...count, 1]>
+				: false
+			: false;
+
+export const canPlaceFlowers = <
+	const Flowerbed extends number[],
+	N extends number,
+>(
+	flowerbed: Flowerbed,
+	n: N,
+): TCanPlaceFlowers<Flowerbed, N> => {
+	if (n === 0) return true as never;
 	let remaining = n;
 	for (let i = 0; i < flowerbed.length; i++) {
 		const prev = flowerbed[i - 1] ?? 0;
@@ -9,9 +39,9 @@ export const canPlaceFlowers = (flowerbed: number[], n: number): boolean => {
 		const next = flowerbed[i + 1] ?? 0;
 		if (prev === 0 && curr === 0 && next === 0) {
 			remaining--;
-			if (remaining === 0) return true;
+			if (remaining === 0) return true as never;
 			i++;
 		}
 	}
-	return false;
+	return false as never;
 };
