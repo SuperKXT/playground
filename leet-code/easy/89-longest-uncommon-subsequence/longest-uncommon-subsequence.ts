887620b53ef0c347577ec134273be846823a0bda
SuperKXT <superkxt@outlook.com>
2025-10-01 16:25:52 +0500

    Add ts solution for `leet-code/easy/89`

--- leet-code/easy/89-longest-uncommon-subsequence/longest-uncommon-subsequence.ts
+++ leet-code/easy/89-longest-uncommon-subsequence/longest-uncommon-subsequence.ts
@@ -1,6 +1,26 @@
 // https://leetcode.com/problems/longest-uncommon-subsequence-i
 
-export const longestUncommonSubsequence = (a: string, b: string): number => {
-	if (a === b) return -1;
-	return Math.max(a.length, b.length);
+type TBiggerStringLength<
+	A extends string,
+	B extends string,
+	len extends 1[] = [],
+> = A extends `${string}${infer aRest}`
+	? B extends `${string}${infer bRest}`
+		? TBiggerStringLength<aRest, bRest, [...len, 1]>
+		: TBiggerStringLength<aRest, B, [...len, 1]>
+	: B extends `${string}${infer bRest}`
+		? TBiggerStringLength<A, bRest, [...len, 1]>
+		: len["length"];
+
+type TLongestUncommonSubsequence<
+	A extends string,
+	B extends string,
+> = A extends B ? -1 : TBiggerStringLength<A, B>;
+
+export const longestUncommonSubsequence = <A extends string, B extends string>(
+	a: A,
+	b: B,
+): TLongestUncommonSubsequence<A, B> => {
+	if (a === (b as string)) return -1 as never;
+	return Math.max(a.length, b.length) as never;
 };
