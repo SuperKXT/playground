17476053d249239c7b58f53e53c146e9d5075bc0
SuperKXT <superkxt@outlook.com>
2025-10-01 18:29:32 +0500

    Add js solution for `leet-code/easy/93`

---
+++ leet-code/easy/93-can-place-flowers/can-place-flowers.test.ts
@@ -0,0 +1,37 @@
+import { assertType, expect, test } from "vitest";
+
+import { canPlaceFlowers } from "./can-place-flowers.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing canPlaceFlowers for test 1", () => {
+	const result = canPlaceFlowers([1, 0, 0, 0, 1], 1);
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing canPlaceFlowers for test 2", () => {
+	const result = canPlaceFlowers([1, 0, 0, 0, 1], 2);
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing canPlaceFlowers for test 3", () => {
+	const result = canPlaceFlowers([1, 0, 1, 0, 1, 0, 1], 0);
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing canPlaceFlowers for test 3", () => {
+	const result = canPlaceFlowers([0, 1, 0, 1, 0, 1, 0, 0], 1);
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
