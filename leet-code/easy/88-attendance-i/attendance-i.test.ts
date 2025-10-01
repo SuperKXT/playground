96cde5f055b6d91bc1e13938a9ae93dc2a289538
SuperKXT <superkxt@outlook.com>
2025-10-01 16:08:03 +0500

    Add ts solution for `leet-code/easy/88`

--- leet-code/easy/88-attendance-i/attendance-i.test.ts
+++ leet-code/easy/88-attendance-i/attendance-i.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing attendance for test 1", () => {
 	const result = attendance("PPALLP"); // cSpell: disable-line
-	const expected = true as boolean;
+	const expected = true;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing attendance for test 1", () => {
 
 test("testing attendance for test 2", () => {
 	const result = attendance("PPALLL"); // cSpell: disable-line
-	const expected = false as boolean;
+	const expected = false;
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
--- leet-code/easy/88-attendance-i/attendance-i.ts
+++ leet-code/easy/88-attendance-i/attendance-i.ts
@@ -1,14 +1,30 @@
 // https://leetcode.com/problems/attendance-i
 
-export const attendance = (s: string): boolean => {
+type TAttendance<
+	S extends string,
+	absents extends 1[] = [],
+	connectives extends 1[] = [],
+> = absents["length"] extends 2
+	? false
+	: connectives["length"] extends 3
+		? false
+		: S extends `${infer first}${infer rest}`
+			? TAttendance<
+					rest,
+					first extends "A" ? [...absents, 1] : absents,
+					first extends "L" ? [...connectives, 1] : []
+				>
+			: true;
+
+export const attendance = <S extends string>(s: S): TAttendance<S> => {
 	let absents = 0;
-	let consecutiveLate = 0;
+	let connectives = 0;
 	for (const char of s) {
 		if (char === "A") absents++;
-		if (char === "L") consecutiveLate++;
-		else consecutiveLate = 0;
-		if (absents === 2) return false;
-		if (consecutiveLate === 3) return false;
+		if (char === "L") connectives++;
+		else connectives = 0;
+		if (absents === 2) return false as never;
+		if (connectives === 3) return false as never;
 	}
-	return true;
+	return true as never;
 };
