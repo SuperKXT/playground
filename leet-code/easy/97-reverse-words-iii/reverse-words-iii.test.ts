10489369b2f1271355cb98d2099a8b744dcffb59
SuperKXT <superkxt@outlook.com>
2025-10-02 20:59:18 +0500

    Add ts solution for `leet-code/easy/97`

--- leet-code/easy/97-reverse-words-iii/reverse-words-iii.test.ts
+++ leet-code/easy/97-reverse-words-iii/reverse-words-iii.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing reverseWords for test 1", () => {
 	const result = reverseWords("Let's take LeetCode contest");
-	const expected = "s'teL ekat edoCteeL tsetnoc" as string; // cSpell: disable-line
+	const expected = "s'teL ekat edoCteeL tsetnoc"; // cSpell: disable-line
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing reverseWords for test 1", () => {
 
 test("testing reverseWords for test 2", () => {
 	const result = reverseWords("Mr Ding");
-	const expected = "rM gniD" as string; // cSpell: disable-line
+	const expected = "rM gniD"; // cSpell: disable-line
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
