5a843ae591bb08dff3317c1ab6692ee1ed5c30f9
SuperKXT <superkxt@outlook.com>
2025-10-03 18:59:23 +0500

    Add ts solution for `leet-code/easy/98`

--- leet-code/easy/98-reshape-matrix/reshape-matrix.ts
+++ leet-code/easy/98-reshape-matrix/reshape-matrix.ts
@@ -1,5 +1,35 @@
 // https://leetcode.com/problems/reshape-the-matrix
 
+type TReshapeMatrix<
+	Original extends number[][],
+	Rows extends number,
+	Columns extends number,
+	Matrix extends number[][] = Original,
+	res extends number[][] = [],
+	row extends number[] = [],
+> = Matrix extends [
+	infer matrixRow extends number[],
+	...infer restRow extends number[][],
+]
+	? matrixRow extends [
+			infer curr extends number,
+			...infer restCol extends number[],
+		]
+		? TReshapeMatrix<
+				Original,
+				Rows,
+				Columns,
+				[restCol, ...restRow],
+				row["length"] extends Columns ? [...res, row] : res,
+				row["length"] extends Columns ? [curr] : [...row, curr]
+			>
+		: TReshapeMatrix<Original, Rows, Columns, restRow, res, row>
+	: row["length"] extends Columns
+		? [...res, row]["length"] extends Rows
+			? [...res, row]
+			: Original
+		: Original;
+
 // export const reshapeMatrix = (
 // 	matrix: number[][],
 // 	rows: number,
@@ -21,13 +51,17 @@
 // 	return reshaped;
 // };
 
-export const reshapeMatrix = (
-	matrix: number[][],
-	rows: number,
-	columns: number,
-): number[][] => {
+export const reshapeMatrix = <
+	const Matrix extends number[][],
+	Rows extends number,
+	Columns extends number,
+>(
+	matrix: Matrix,
+	rows: Rows,
+	columns: Columns,
+): TReshapeMatrix<Matrix, Rows, Columns> => {
 	if (rows * columns !== matrix.length * (matrix[0]?.length ?? 0))
-		return matrix;
+		return matrix as never;
 	const toAdd = { row: 0, col: 0 };
 	const reshaped: number[][] = [];
 	for (let r = 0; r < rows; r++) {
@@ -45,5 +79,5 @@ export const reshapeMatrix = (
 			}
 		}
 	}
-	return reshaped;
+	return reshaped as never;
 };
