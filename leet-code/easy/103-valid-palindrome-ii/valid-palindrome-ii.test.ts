902464d4d7692251ec7a80172d84dc75e37a3c2d
SuperKXT <superkxt@outlook.com>
2025-10-04 17:09:58 +0500

    Add js solution for `leet-code/easy/103`

---
+++ leet-code/easy/103-valid-palindrome-ii/valid-palindrome-ii.test.ts
@@ -0,0 +1,37 @@
+import { assertType, expect, test } from "vitest";
+
+import { validPalindrome } from "./valid-palindrome-ii.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing maximumAverageSubarray for test 1", () => {
+	const result = validPalindrome("aba");
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing maximumAverageSubarray for test 2", () => {
+	const result = validPalindrome("abca"); // cSpell: disable-line
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing maximumAverageSubarray for test 2", () => {
+	const result = validPalindrome("abc"); // cSpell: disable-line
+	const expected = false as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing maximumAverageSubarray for test 2", () => {
+	const result = validPalindrome("eccer"); // cSpell: disable-line
+	const expected = true as boolean;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
