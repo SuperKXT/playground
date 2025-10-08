14adeffa28c65dff45b4ad2c6f68b5dbec89aa18
SuperKXT <superkxt@outlook.com>
2025-10-08 18:45:31 +0500

    Update `leet-code/easy/110`

--- leet-code/easy/110-lower-case/lower-case.ts
+++ leet-code/easy/110-lower-case/lower-case.ts
@@ -1,8 +1,8 @@
 // https://leetcode.com/problems/to-lower-case
 
-export const lowerCase = <Str extends string>(str: Str): Lowercase<Str> => {
-	return str.toLowerCase();
-};
+// export const lowerCase = <Str extends string>(str: Str): Lowercase<Str> => {
+// 	return str.toLowerCase() as never;
+// };
 
 // export const lowerCase = <Str extends string>(str: Str): Lowercase<Str> => {
 // 	let res = "";
@@ -13,3 +13,46 @@ export const lowerCase = <Str extends string>(str: Str): Lowercase<Str> => {
 // 	}
 // 	return res as never;
 // };
+
+const map = {
+	A: "a",
+	B: "b",
+	C: "c",
+	D: "d",
+	E: "e",
+	F: "f",
+	G: "g",
+	H: "h",
+	I: "i",
+	J: "j",
+	K: "k",
+	L: "l",
+	M: "m",
+	N: "n",
+	O: "o",
+	P: "p",
+	Q: "q",
+	R: "r",
+	S: "s",
+	T: "t",
+	U: "u",
+	V: "v",
+	W: "w",
+	X: "x",
+	Y: "y",
+	Z: "z",
+} as const;
+
+type TMap = typeof map;
+
+type TLowerCase<Str extends string> = Str extends `${infer first}${infer rest}`
+	? `${first extends keyof TMap ? TMap[first] : first}${TLowerCase<rest>}`
+	: Str;
+
+export const lowerCase = <Str extends string>(str: Str): TLowerCase<Str> => {
+	let res = "";
+	for (const char of str) {
+		res += (map as Record<string, string>)[char] ?? char;
+	}
+	return res as never;
+};
