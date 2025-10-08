13917ba600538579bcaa1f36dd9c03c3cc79eaf3
SuperKXT <superkxt@outlook.com>
2025-10-08 18:23:55 +0500

    Add js solution for `leet-code/easy/109`

--- leet-code/easy/108-binary-search/binary-search.test.ts
+++ leet-code/easy/108-binary-search/binary-search.test.ts
@@ -4,7 +4,7 @@ import { binarySearch } from "./binary-search.js";
 
 import type { Utils } from "../../../types/utils.types.js";
 
-test("testing selfDividingNumbers for test 1", () => {
+test("testing binarySearch for test 1", () => {
 	const result = binarySearch([-1, 0, 3, 5, 9, 12], 9);
 	const expected = 4 as number;
 	expect(result).toStrictEqual(expected);
@@ -12,7 +12,7 @@ test("testing selfDividingNumbers for test 1", () => {
 	assertType<TTrue>(true);
 });
 
-test("testing selfDividingNumbers for test 2", () => {
+test("testing binarySearch for test 2", () => {
 	const result = binarySearch([-1, 0, 3, 5, 9, 12], 2);
 	const expected = -1 as number;
 	expect(result).toStrictEqual(expected);
