0b84f114300ce5a5acde5c5ab7d800562be6decd
SuperKXT <superkxt@outlook.com>
2025-10-08 19:02:15 +0500

    Add ts solution for `leet-code/easy/111`

--- leet-code/easy/111-one-bit-char/one-bit-char.ts
+++ leet-code/easy/111-one-bit-char/one-bit-char.ts
@@ -1,11 +1,23 @@
 // https://leetcode.com/problems/1-bit-and-2-bit-characters
 
-export const oneBitChar = (bits: number[]): boolean => {
+type TOneBitChar<Bits extends number[]> = Bits extends []
+	? false
+	: Bits extends [unknown]
+		? true
+		: Bits extends [1, unknown, ...infer rest extends number[]]
+			? TOneBitChar<rest>
+			: Bits extends [0, ...infer rest extends number[]]
+				? TOneBitChar<rest>
+				: false;
+
+export const oneBitChar = <const Bits extends number[]>(
+	bits: Bits,
+): TOneBitChar<Bits> => {
 	let i = bits.length;
 	while (i > 1) {
 		const curr = bits[bits.length - i];
 		i--;
 		if (curr === 1) i--;
 	}
-	return i === 1;
+	return (i === 1) as never;
 };
