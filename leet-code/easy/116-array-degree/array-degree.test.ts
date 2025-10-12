76f6337ed6098802d569120a96e55c8fa514dc21
SuperKXT <superkxt@outlook.com>
2025-10-12 14:30:07 +0500

    Add js solution for `leet-code/easy/116`

---
+++ leet-code/easy/116-array-degree/array-degree.test.ts
@@ -0,0 +1,21 @@
+import { assertType, expect, test } from "vitest";
+
+import { arrayDegree } from "./array-degree.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing pascalTriangle for test 1", () => {
+	const result = arrayDegree([1, 2, 2, 3, 1]);
+	const expected = 2 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing pascalTriangle for test 2", () => {
+	const result = arrayDegree([1, 2, 2, 3, 1, 4, 2]);
+	const expected = 6 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
