9fdeee6c3b39039fe2a93eb73409fad9f283e740
SuperKXT <superkxt@outlook.com>
2025-10-15 20:32:00 +0500

    Add ts solution for `rwc#426`

--- rendezvous-with-cassidoo/426/csv-to-list.ts
+++ rendezvous-with-cassidoo/426/csv-to-list.ts
@@ -1,4 +1,38 @@
-export const csvToList = (input: string): string => {
+type _TCsvToList<
+	Input extends string,
+	res extends string = "",
+	line extends string = "",
+	inQuote extends boolean = false,
+	reading extends "name" | "age" | "city" = "name",
+> = Input extends `${infer char}${infer rest}`
+	? char extends '"'
+		? _TCsvToList<rest, res, line, Exclude<boolean, inQuote>, reading>
+		: inQuote extends true
+			? _TCsvToList<rest, res, `${line}${char}`, inQuote, reading>
+			: char extends "\n"
+				? _TCsvToList<rest, `${res}\n- ${line}`>
+				: char extends ","
+					? _TCsvToList<
+							rest,
+							res,
+							`${line}, ${reading extends "name" ? "age" : reading extends "age" ? "from" : ""} `,
+							false,
+							reading extends "name"
+								? "age"
+								: reading extends "age"
+									? "city"
+									: "name"
+						>
+					: _TCsvToList<rest, res, `${line}${char}`, inQuote, reading>
+	: `${res}\n- ${line}\n`;
+
+type TCsvToList<Input extends string> = Input extends `${string}\n${infer rest}`
+	? _TCsvToList<rest>
+	: never;
+
+export const csvToList = <Input extends string>(
+	input: Input,
+): TCsvToList<Input> => {
 	let res = "";
 	let line = "";
 	let inQuote = false;
@@ -26,5 +60,5 @@ export const csvToList = (input: string): string => {
 		}
 	}
 	res += `\n- ${line}\n`;
-	return res;
+	return res as never;
 };
