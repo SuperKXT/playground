e882b3942508506146c7429fc84da8a7c373933f
SuperKXT <superkxt@outlook.com>
2025-10-18 15:51:01 +0500

    Update `leet-code/easy/117`

--- leet-code/easy/117-most-common-word/most-common-word.test.ts
+++ leet-code/easy/117-most-common-word/most-common-word.test.ts
@@ -22,3 +22,19 @@ test("testing mostCommonWord for test 2", () => {
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
 });
+
+test("testing mostCommonWord for test 3", () => {
+	const result = mostCommonWord("a b.b", []);
+	const expected = "b" as string;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing mostCommonWord for test 4", () => {
+	const result = mostCommonWord("Bob. hIt, baLl", ["bob", "hit"]);
+	const expected = "ball" as string;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
