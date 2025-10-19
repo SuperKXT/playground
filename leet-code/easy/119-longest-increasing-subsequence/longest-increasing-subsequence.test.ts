914c3c17ac2db67937765a86ea0e154a77decc86
SuperKXT <superkxt@outlook.com>
2025-10-19 15:47:18 +0500

    Add js solution for `leet-code/easy/119`

---
+++ leet-code/easy/119-longest-increasing-subsequence/longest-increasing-subsequence.test.ts
@@ -0,0 +1,29 @@
+import { assertType, expect, test } from "vitest";
+
+import { longestIncreasingSubsequence } from "./longest-increasing-subsequence.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing longestIncreasingSubsequence for test 1", () => {
+	const result = longestIncreasingSubsequence([1, 3, 5, 4, 7]);
+	const expected = 3 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing longestIncreasingSubsequence for test 2", () => {
+	const result = longestIncreasingSubsequence([2, 2, 2, 2, 2]);
+	const expected = 1 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing longestIncreasingSubsequence for test 3", () => {
+	const result = longestIncreasingSubsequence([1, 3, 5, 4, 2, 3, 4, 5]);
+	const expected = 4 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
