c8ec008320d0234613fcf095d6c59cbd1d6b1271
SuperKXT <superkxt@outlook.com>
2025-10-19 15:37:11 +0500

    Update `leet-code/easy/118`

--- leet-code/easy/118-alternating-bits/alternating-bits.ts
+++ leet-code/easy/118-alternating-bits/alternating-bits.ts
@@ -1,5 +1,17 @@
 // https://leetcode.com/problems/binary-number-with-alternating-bits
 
+// export const alternatingBits = (n: number): boolean => {
+// 	let curr = n;
+// 	let last: undefined | number;
+// 	while (curr > 1) {
+// 		const remainder = curr % 2;
+// 		if (last === remainder) return false;
+// 		last = remainder;
+// 		curr = Math.floor(curr / 2);
+// 	}
+// 	return curr !== last;
+// };
+
 export const alternatingBits = (n: number): boolean => {
 	const str = n.toString(2);
 	let last: undefined | string;
