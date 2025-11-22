460d7aa8f1aacc6cf5bfff6ba46054b1ce29ff1b
SuperKXT <superkxt@outlook.com>
2025-11-22 14:28:23 +0500

    Add ts solution for `rwc`

--- rendezvous-with-cassidoo/431/repeated-integers.test.ts
+++ rendezvous-with-cassidoo/431/repeated-integers.test.ts
@@ -6,8 +6,8 @@ import type { Utils } from "../../types/utils.types.js";
 
 test("testing repeatedIntegers against test 1", () => {
 	const result = repeatedIntegers(4);
-	const expected = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+	const expected = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Readonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
