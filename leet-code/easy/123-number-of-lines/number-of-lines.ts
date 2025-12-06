87a5bbfbb87872d757039f7e5e1c5ca3f5214caa
SuperKXT <superkxt@outlook.com>
2025-12-06 14:28:04 +0000

    Update `leet-code/easy/123`

--- leet-code/easy/123-number-of-lines/number-of-lines.ts
+++ leet-code/easy/123-number-of-lines/number-of-lines.ts
@@ -1,14 +1,36 @@
 // https://leetcode.com/problems/number-of-lines-to-write-string
 
+// export const numberOfLines = (
+// 	widths: number[],
+// 	s: string,
+// ): [number, number] => {
+// 	let lines = 1;
+// 	let length = 0;
+// 	for (const char of s) {
+// 		const idx = char.charCodeAt(0) - 97;
+// 		const width = widths[idx] as number;
+// 		if (length + width > 100) {
+// 			lines++;
+// 			length = 0;
+// 		}
+// 		length += width;
+// 	}
+// 	return [lines, length];
+// };
+
 export const numberOfLines = (
 	widths: number[],
 	s: string,
 ): [number, number] => {
 	let lines = 1;
 	let length = 0;
+	const map = new Map<string, number>();
+	for (let i = 0; i < widths.length; i++) {
+		const char = String.fromCharCode(i + 97);
+		map.set(char, widths[i] as number);
+	}
 	for (const char of s) {
-		const idx = char.charCodeAt(0) - 97;
-		const width = widths[idx] as number;
+		const width = map.get(char) as number;
 		if (length + width > 100) {
 			lines++;
 			length = 0;
