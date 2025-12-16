5148adec9de29feed6c1ada4f09ca8c9e70e9158
SuperKXT <superkxt@outlook.com>
2025-12-16 21:07:13 +0000

    Add ts solution for `leet-cody/easy/128`

--- leet-code/easy/128-reverse-only-letters/reverse-only-letters.test.ts
+++ leet-code/easy/128-reverse-only-letters/reverse-only-letters.test.ts
@@ -6,7 +6,7 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing reverseOnlyLetters for test 1", () => {
 	const result = reverseOnlyLetters("ab-cd");
-	const expected = "dc-ba" as string;
+	const expected = "dc-ba";
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -14,7 +14,7 @@ test("testing reverseOnlyLetters for test 1", () => {
 
 test("testing reverseOnlyLetters for test 2", () => {
 	const result = reverseOnlyLetters("a-bC-dEf-ghIj");
-	const expected = "j-Ih-gfE-dCba" as string;
+	const expected = "j-Ih-gfE-dCba";
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
@@ -22,7 +22,7 @@ test("testing reverseOnlyLetters for test 2", () => {
 
 test("testing reverseOnlyLetters for test 3", () => {
 	const result = reverseOnlyLetters("Test1ng-Leet=code-Q!");
-	const expected = "Qedo1ct-eeLg=ntse-T!" as string; // cSpell: disable-line
+	const expected = "Qedo1ct-eeLg=ntse-T!"; // cSpell: disable-line
 	expect(result).toStrictEqual(expected);
 	type TTrue = Utils.equal<typeof result, typeof expected>;
 	assertType<TTrue>(true);
--- leet-code/easy/128-reverse-only-letters/reverse-only-letters.ts
+++ leet-code/easy/128-reverse-only-letters/reverse-only-letters.ts
@@ -1,10 +1,55 @@
 // https://leetcode.com/problems/min-cost-climbing-stairs
 
+type TStringToTuple<S extends string> = S extends `${infer first}${infer rest}`
+	? [first, ...TStringToTuple<rest>]
+	: [];
+
+type TIsAlphabet<S extends string> =
+	Uppercase<S> extends Lowercase<S> ? false : true;
+
+type TFindRight<
+	tup extends string[],
+	left extends string,
+	right extends string = "",
+> = tup extends [...infer rest extends string[], infer last extends string]
+	? TIsAlphabet<last> extends true
+		? { tup: rest; left: last; right: `${left}${right}` }
+		: TFindRight<rest, left, `${last}${right}`>
+	: { tup: []; left: left; right: right };
+
+type _TReverseOnlyLetters<
+	tup extends string[],
+	left extends string,
+	right extends string,
+> = tup extends [infer first extends string, ...infer rest extends string[]]
+	? TIsAlphabet<first> extends true
+		? TFindRight<rest, first> extends infer res extends {
+				tup: string[];
+				left: string;
+				right: string;
+			}
+			? _TReverseOnlyLetters<
+					res["tup"],
+					`${left}${res["left"]}`,
+					`${res["right"]}${right}`
+				>
+			: never
+		: _TReverseOnlyLetters<rest, `${left}${first}`, right>
+	: `${left}${right}`;
+
+type TReverseOnlyLetters<S extends string> = _TReverseOnlyLetters<
+	TStringToTuple<S>,
+	"",
+	""
+>;
+
 const isAlphabet = (s: string): boolean => {
 	return s.toLowerCase() !== s.toUpperCase();
 };
 
-export const reverseOnlyLetters = (s: string): string => {
+export const reverseOnlyLetters = <S extends string>(
+	s: S,
+): TReverseOnlyLetters<S> => {
 	let leftIdx = 0;
 	let rightIdx = s.length - 1;
 	let left = "";
@@ -34,5 +79,7 @@ export const reverseOnlyLetters = (s: string): string => {
 		}
 		leftIdx++;
 	}
-	return left + (leftIdx === rightIdx ? (s[leftIdx] as string) : "") + right;
+	return (left +
+		(leftIdx === rightIdx ? (s[leftIdx] as string) : "") +
+		right) as never;
 };
