0d295b1d3c1d1aafe7eb5921c894912c826e3520
SuperKXT <superkxt@outlook.com>
2025-12-16 14:11:06 +0500

    Add js solution for `leet-code/easy/126`

---
+++ leet-code/easy/126-dominant-index/dominant-index.test.ts
@@ -0,0 +1,21 @@
+import { assertType, expect, test } from "vitest";
+
+import { dominantIndex } from "./dominant-index.js";
+
+import type { Utils } from "../../../types/utils.types.js";
+
+test("testing monotonicArray for test 1", () => {
+	const result = dominantIndex([3, 6, 1, 0]);
+	const expected = 1 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
+
+test("testing monotonicArray for test 2", () => {
+	const result = dominantIndex([1, 2, 3, 4]);
+	const expected = -1 as number;
+	expect(result).toStrictEqual(expected);
+	type TTrue = Utils.equal<typeof result, typeof expected>;
+	assertType<TTrue>(true);
+});
---
+++ leet-code/easy/126-dominant-index/dominant-index.ts
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/monotonic-array
+
+export const dominantIndex = (nums: number[]): number => {
+	let greatestIdx = -1;
+	let greatest = -Infinity;
+	let second = -Infinity;
+	for (let idx = 0; idx < nums.length; idx++) {
+		const num = nums[idx] as number;
+		if (greatest < num) {
+			second = greatest;
+			greatest = num;
+			greatestIdx = idx;
+		} else if (second < num) {
+			second = num;
+		}
+	}
+	return greatest >= second * 2 ? greatestIdx : -1;
+};
