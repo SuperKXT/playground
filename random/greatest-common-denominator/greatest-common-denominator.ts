a130ce535e4b997f467d76dc6e7b757bdf27bb64
SuperKXT <superkxt@outlook.com>
2025-12-18 03:32:00 +0500

    Update `random/greatest-common-denominator`

--- random/greatest-common-denominator/greatest-common-denominator.ts
+++ random/greatest-common-denominator/greatest-common-denominator.ts
@@ -1,16 +1,33 @@
 // https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards
 
+// export const gcd = (nums: number[]): number => {
+// 	if (nums.length === 0) return 0;
+// 	if (nums.length === 1) return nums[0] as number;
+// 	let smallest = Infinity;
+// 	for (const num of nums) {
+// 		smallest = Math.min(smallest, num);
+// 	}
+// 	for (let i = smallest; i > 1; i--) {
+// 		if (nums.every((num) => num % i === 0)) {
+// 			return i;
+// 		}
+// 	}
+// 	return 1;
+// };
+
+const _gcd = (a: number, b: number): number => {
+	return b === 0 ? a : _gcd(b, a % b);
+};
+
 export const gcd = (nums: number[]): number => {
-	if (nums.length === 0) return 0;
-	if (nums.length === 1) return nums[0] as number;
-	let smallest = Infinity;
-	for (const num of nums) {
-		smallest = Math.min(smallest, num);
-	}
-	for (let i = smallest; i > 1; i--) {
-		if (nums.every((num) => num % i === 0)) {
-			return i;
+	const [first, ...rest] = nums;
+	if (first === undefined) return 0;
+	let res = first;
+	if (rest.length > 1) {
+		for (const num of rest) {
+			const prev = res;
+			res = _gcd(res, num);
 		}
 	}
-	return 1;
+	return res;
 };
