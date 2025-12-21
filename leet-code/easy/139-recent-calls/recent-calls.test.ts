5bca26c4083ea7cf4486f96d104bb30da50a9dc8
SuperKXT <superkxt@outlook.com>
2025-12-22 04:13:07 +0500

    Update `leet-code/easy/139`

--- leet-code/easy/139-recent-calls/recent-calls.test.ts
+++ leet-code/easy/139-recent-calls/recent-calls.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 
 import { RecentCounter } from "./recent-calls.js";
 
-test("testing RecentCounter", () => {
+test("testing RecentCounter for test 1", () => {
 	const counter = new RecentCounter();
 
 	const result1 = counter.ping(1);
@@ -20,4 +20,15 @@ test("testing RecentCounter", () => {
 	const result4 = counter.ping(3002);
 	const expected4 = 3;
 	expect(result4).toStrictEqual(expected4);
+});
+
+test("testing RecentCounter for test 2", () => {
+	const counter = new RecentCounter();
+
+	for (let i = 0; i < 3002; i++) {
+		const res = counter.ping(i);
+		expect(res).toStrictEqual(i < 3001 ? i + 1 : i);
+	}
+	const res = counter.ping(6001);
+	expect(res).toBe(2);
 });
--- leet-code/easy/139-recent-calls/recent-calls.ts
+++ leet-code/easy/139-recent-calls/recent-calls.ts
@@ -9,7 +9,18 @@ export class RecentCounter {
 	ping(t: number): number {
 		this.requests.push(t);
 		// return this.requests.filter((r) => r >= t - 3000).length;
-		const lastIdx = this.requests.findLastIndex((r) => r < t - 3000);
-		return this.requests.length - (lastIdx + 1);
+		// const lastIdx = this.requests.findLastIndex((r) => r < t - 3000);
+		// return this.requests.length - (lastIdx + 1);
+		let low = 0;
+		let high = this.requests.length - 1;
+		while (low <= high) {
+			const mid = Math.floor((low + high) / 2);
+			if ((this.requests[mid] as number) < t - 3000) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return this.requests.length - low;
 	}
 }
