fbf2f30b965b44e2e63bb39c9db8eb0554d1bf71
SuperKXT <superkxt@outlook.com>
2025-12-23 06:49:57 +0500

    Add ts solution for `leet-code/easy/141`

--- leet-code/easy/141-backspace-string-compare/backspace-string-compare.ts
+++ leet-code/easy/141-backspace-string-compare/backspace-string-compare.ts
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/backspace-string-compare
 
+type TReverseString<Str extends string> =
+	Str extends `${infer first}${infer rest}`
+		? `${TReverseString<rest>}${first}`
+		: "";
+
+type TRemoveFirstChar<Str extends string> = Str extends `${string}${infer rest}`
+	? rest
+	: "";
+
+type TParseString<
+	Str extends string,
+	parsed extends string = "",
+> = Str extends `${infer first}${infer rest}`
+	? first extends "#"
+		? TParseString<rest, TRemoveFirstChar<parsed>>
+		: TParseString<rest, `${first}${parsed}`>
+	: TReverseString<parsed>;
+
+type TBackspaceCompare<S extends string, T extends string> =
+	TParseString<S> extends TParseString<T> ? true : false;
+
 const parseString = (str: string): string => {
 	let parsed = "";
 	for (const char of str) {
@@ -9,6 +30,9 @@ const parseString = (str: string): string => {
 	return parsed;
 };
 
-export const backspaceCompare = (s: string, t: string): boolean => {
-	return parseString(s) === parseString(t);
+export const backspaceCompare = <S extends string, T extends string>(
+	s: S,
+	t: T,
+): TBackspaceCompare<S, T> => {
+	return (parseString(s) === parseString(t)) as never;
 };
