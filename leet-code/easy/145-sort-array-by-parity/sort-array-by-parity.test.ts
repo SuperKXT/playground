eb8da9721ebe67a077e00b9f2f951234dea0b2a6
SuperKXT <superkxt@outlook.com>
2025-12-28 04:00:44 +0500

    Add ts solution for `leet-code/easy/145`

--- leet-code/easy/145-sort-array-by-parity/sort-array-by-parity.test.ts
+++ leet-code/easy/145-sort-array-by-parity/sort-array-by-parity.test.ts
@@ -6,16 +6,16 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing sortArrayByParity for test 1", () => {
 	const result = sortArrayByParity([3, 1, 2, 4]);
-	const expected = [4, 2, 3, 1];
+	const expected = [4, 2, 3, 1] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Utils.deepReadonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
 
 test("testing sortArrayByParity for test 2", () => {
 	const result = sortArrayByParity([0]);
-	const expected = [0];
+	const expected = [0] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Utils.deepReadonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
--- leet-code/easy/145-sort-array-by-parity/sort-array-by-parity.ts
+++ leet-code/easy/145-sort-array-by-parity/sort-array-by-parity.ts
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/sort-array-by-parity
 
-export const sortArrayByParity = (nums: number[]): number[] => {
+type TIsEven<N extends number> = `${N}` extends `${string}${0 | 2 | 4 | 6 | 8}`
+	? true
+	: false;
+
+type TSortArrayByParity<
+	Nums extends number[],
+	res extends number[] = [],
+> = Nums extends [infer first extends number, ...infer rest extends number[]]
+	? TSortArrayByParity<
+			rest,
+			TIsEven<first> extends true ? [first, ...res] : [...res, first]
+		>
+	: res;
+
+export const sortArrayByParity = <const Nums extends number[]>(
+	nums: Nums,
+): TSortArrayByParity<Nums> => {
 	const res: number[] = [];
 	for (const num of nums) {
 		if (num % 2 === 0) res.unshift(num);
 		else res.push(num);
 	}
-	return res;
+	return res as never;
 };
