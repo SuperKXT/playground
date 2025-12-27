42bbcdcefed288fa622a9980fdfcac92980339aa
SuperKXT <superkxt@outlook.com>
2025-12-27 23:54:43 +0500

    Add ts solution for `leet-code/easy/144`

--- leet-code/easy/144-large-group-positions/large-group-positions.test.ts
+++ leet-code/easy/144-large-group-positions/large-group-positions.test.ts
@@ -6,28 +6,28 @@ import type { Utils } from "../../../types/utils.types.js";
 
 test("testing largeGroupPositions for test 1", () => {
 	const result = largeGroupPositions("abbxxxxzzy"); // cSpell: disable-line
-	const expected: Array<[number, number]> = [[3, 6]];
+	const expected = [[3, 6]] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Utils.deepReadonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
 
 test("testing largeGroupPositions for test 2", () => {
 	const result = largeGroupPositions("abc"); // cSpell: disable-line
-	const expected: Array<[number, number]> = [];
+	const expected = [] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Utils.deepReadonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
 
 test("testing largeGroupPositions for test 3", () => {
 	const result = largeGroupPositions("abcdddeeeeaabbbcd"); // cSpell: disable-line
-	const expected: Array<[number, number]> = [
+	const expected = [
 		[3, 5],
 		[6, 9],
 		[12, 14],
-	];
+	] as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Utils.deepReadonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
--- leet-code/easy/144-large-group-positions/large-group-positions.ts
+++ leet-code/easy/144-large-group-positions/large-group-positions.ts
@@ -1,18 +1,62 @@
 // https://leetcode.com/problems/positions-of-large-groups
 
+type TCoord = [number, number];
+
 type TCurr = { char: string; start: number };
+
+type TTCheck = { char: string; start: number; length: Array<1> };
+
+type TLengthMinusOne<T extends unknown[]> = T extends [unknown, ...infer rest]
+	? rest["length"]
+	: 0;
+
+type TLargeGroupPositions<
+	S extends string,
+	idx extends Array<1> = [],
+	res extends TCoord[] = [],
+	curr extends null | TTCheck = null,
+> = S extends `${infer first}${infer rest}`
+	? curr extends TTCheck
+		? first extends curr["char"]
+			? TLargeGroupPositions<
+					rest,
+					[...idx, 1],
+					res,
+					{ char: first; start: curr["start"]; length: [...curr["length"], 1] }
+				>
+			: TLargeGroupPositions<
+					rest,
+					[...idx, 1],
+					curr["length"]["length"] extends 0 | 1 | 2
+						? res
+						: [...res, [curr["start"], TLengthMinusOne<idx>]],
+					{
+						char: first;
+						start: idx["length"];
+						length: [1];
+					}
+				>
+		: TLargeGroupPositions<
+				rest,
+				[...idx, 1],
+				res,
+				{ char: first; start: TLengthMinusOne<idx>; length: [] }
+			>
+	: res;
 
 const checkInterval = (
 	curr: TCurr | null,
 	end: number,
-	intervals: Array<[number, number]>,
+	intervals: TCoord[],
 ): void => {
 	if (!curr) return;
 	if (end - curr.start >= 2) intervals.push([curr.start, end]);
 };
 
-export const largeGroupPositions = (s: string): Array<[number, number]> => {
-	const intervals: Array<[number, number]> = [];
+export const largeGroupPositions = <S extends string>(
+	s: S,
+): TLargeGroupPositions<S> => {
+	const intervals: TCoord[] = [];
 	let curr: null | TCurr = null;
 	for (let i = 0; i < s.length; i++) {
 		const char = s[i] as string;
@@ -23,5 +67,5 @@ export const largeGroupPositions = (s: string): Array<[number, number]> => {
 		}
 	}
 	checkInterval(curr, s.length - 1, intervals);
-	return intervals;
+	return intervals as never;
 };
