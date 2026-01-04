9a7ba0c1e5d7e5f19c5fdfe41fdb196f840ae23b
Saad Khan <superkxt@outlook.com>
2026-01-04 08:50:06 +0500

    Add incomplete ts solution for `leet-code/easy/147`

--- leet-code/easy/147-rook-captures/rook-captures.ts
+++ leet-code/easy/147-rook-captures/rook-captures.ts
@@ -1,4 +1,88 @@
-// https://leetcode.com/problems/available-captures-for-rook 
+// https://leetcode.com/problems/available-captures-for-rook
+
+type TDirection = "up" | "down" | "left" | "right";
+
+type TShift<Arr extends 1[]> = Arr extends [
+  infer first,
+  ...infer rest extends 1[],
+]
+  ? rest
+  : [];
+
+type TNextSquare<
+  Pos extends [1[], 1[]],
+  Direction extends TDirection,
+> = Direction extends "up"
+  ? Pos[0] extends []
+    ? null
+    : [TShift<Pos[0]>, Pos[1]]
+  : Direction extends "left"
+    ? Pos[1] extends []
+      ? null
+      : [Pos[0], TShift<Pos[1]>]
+    : Direction extends "down"
+      ? Pos[0]["length"] extends 7
+        ? null
+        : [[...Pos[0], 1], Pos[1]]
+      : Pos[1]["length"] extends 7
+        ? null
+        : [Pos[0], [...Pos[1], 1]];
+
+type TCheckSquare<
+  Board extends string[][],
+  Pos extends [1[], 1[]],
+  Direcction extends TDirection,
+  cell extends string = Board[Pos[0]["length"]][Pos[1]["length"]],
+> = cell extends "p"
+  ? 1
+  : cell extends "B"
+    ? 0
+    : TNextSquare<Pos, Direcction> extends infer next
+      ? next extends [1[], 1[]]
+        ? TCheckSquare<Board, next, Direcction>
+        : 0
+      : 0;
+
+type TFindRookInRow<Row extends unknown[], idx extends 1[] = []> = Row extends [
+  infer first,
+  ...infer rest,
+]
+  ? first extends "R"
+    ? idx
+    : TFindRookInRow<rest, [...idx, 1]>
+  : null;
+
+type TFindRook<Board extends string[][], x extends 1[] = []> = Board extends [
+  infer row extends string[],
+  ...infer rest extends string[][],
+]
+  ? TFindRookInRow<row> extends infer y
+    ? y extends 1[]
+      ? [x, y]
+      : null
+    : null
+  : null;
+
+type TOneCount<Arr extends unknown[], count extends 1[] = []> = Arr extends [
+  infer first,
+  ...infer rest,
+]
+  ? TOneCount<rest, first extends 1 ? [...count, 1] : count>
+  : count["length"];
+
+type TNumRookCaptures<Board extends string[][]> =
+  TFindRook<Board> extends infer pos
+    ? pos extends [1[], 1[]]
+      ? TOneCount<
+          [
+            TCheckSquare<Board, pos, "up">,
+            TCheckSquare<Board, pos, "down">,
+            TCheckSquare<Board, pos, "left">,
+            TCheckSquare<Board, pos, "right">,
+          ]
+        >
+      : 0
+    : 0;
 
 const checkSquares = (board: string[][], pos: [number, number], next: (curr: [number, number]) => [number, number]): number => {
     let cell = board[pos[0]]?.[pos[1]];
