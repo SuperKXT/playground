241a2c1881ca5ad0033b9a5173c776db2875ed9f
SuperKXT <superkxt@outlook.com>
2026-01-22 16:16:46 +0500

    Update `leet-code/easy/46` and `leet-code/easy/47`

--- leet-code/easy/46-min-tree-depth/min-tree-depth.ts
+++ leet-code/easy/46-min-tree-depth/min-tree-depth.ts
@@ -2,15 +2,31 @@
 
 import type { TBinaryTreeNode } from "../../../helpers/binary-tree.helpers.js";
 
-const getDepths = (node: TBinaryTreeNode<number>, depth: number): number[] => {
-	if (!node) return [depth];
-	if (!node.left && !node.right) return [depth + 1];
-	const depths: number[] = [];
-	if (node.left) depths.push(...getDepths(node.left, depth + 1));
-	if (node.right) depths.push(...getDepths(node.right, depth + 1));
-	return depths;
+// const getDepths = (node: TBinaryTreeNode<number>, depth: number): number[] => {
+// 	if (!node) return [depth];
+// 	if (!node.left && !node.right) return [depth + 1];
+// 	const depths: number[] = [];
+// 	if (node.left) depths.push(...getDepths(node.left, depth + 1));
+// 	if (node.right) depths.push(...getDepths(node.right, depth + 1));
+// 	return depths;
+// };
+
+// export const minDepth = (root: TBinaryTreeNode<number>): number => {
+// 	return Math.min(...getDepths(root, 0));
+// };
+
+const getMinDepth = (
+	node: Exclude<TBinaryTreeNode<number>, null>,
+	depth: number = 0,
+): number => {
+	if (!node.left && !node.right) return depth + 1;
+	return Math.min(
+		node.left ? getMinDepth(node.left, depth + 1) : Infinity,
+		node.right ? getMinDepth(node.right, depth + 1) : Infinity,
+	);
 };
 
 export const minDepth = (root: TBinaryTreeNode<number>): number => {
-	return Math.min(...getDepths(root, 0));
+	if (!root) return 0;
+	return getMinDepth(root);
 };
--- leet-code/easy/47-max-tree-depth/max-tree-depth.ts
+++ leet-code/easy/47-max-tree-depth/max-tree-depth.ts
@@ -2,15 +2,30 @@
 
 import type { TBinaryTreeNode } from "../../../helpers/binary-tree.helpers.js";
 
-const getDepths = (node: TBinaryTreeNode<number>, depth: number): number[] => {
-	if (!node) return [depth];
-	if (!node.left && !node.right) return [depth + 1];
-	const depths: number[] = [];
-	if (node.left) depths.push(...getDepths(node.left, depth + 1));
-	if (node.right) depths.push(...getDepths(node.right, depth + 1));
-	return depths;
+// const getDepths = (node: TBinaryTreeNode<number>, depth: number): number[] => {
+// 	if (!node) return [depth];
+// 	if (!node.left && !node.right) return [depth + 1];
+// 	const depths: number[] = [];
+// 	if (node.left) depths.push(...getDepths(node.left, depth + 1));
+// 	if (node.right) depths.push(...getDepths(node.right, depth + 1));
+// 	return depths;
+// };
+
+// export const maxDepth = (root: TBinaryTreeNode<number>): number => {
+// 	return Math.max(...getDepths(root, 0));
+// };
+
+const getMaxDepth = (
+	node: TBinaryTreeNode<number>,
+	depth: number = 0,
+): number => {
+	if (!node) return depth;
+	return Math.max(
+		getMaxDepth(node.left, depth + 1),
+		getMaxDepth(node.right, depth + 1),
+	);
 };
 
 export const maxDepth = (root: TBinaryTreeNode<number>): number => {
-	return Math.max(...getDepths(root, 0));
+	return getMaxDepth(root);
 };
