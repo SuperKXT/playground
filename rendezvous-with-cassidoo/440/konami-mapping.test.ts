f27c4d71be18b468c66586a7d5cebd25fd385a17
SuperKXT <superkxt@outlook.com>
2026-01-25 11:48:54 +0500

    Add ts solution for `rwc#440`

--- rendezvous-with-cassidoo/440/konami-mapping.test.ts
+++ rendezvous-with-cassidoo/440/konami-mapping.test.ts
@@ -6,30 +6,30 @@ import type { Utils } from "../../types/utils.types.js";
 
 test("testing konamiMapping against test 1", () => {
 	const result = konamiMapping("xx2233454590yy11110");
-	const expected: Record<string, string> = {
-		"0": "A",
+	const expected = {
 		"2": "U",
 		"3": "D",
 		"4": "L",
 		"5": "R",
 		"9": "B",
-	};
+		"0": "A",
+	} as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Utils.deepReadonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
 
 test("testing konamiMapping against test 2", () => {
 	const result = konamiMapping("sduwahoda22ii0d0dbn"); // cSpell: disable-line
-	const expected: Record<string, string> = {
-		"0": "L",
+	const expected = {
 		"2": "U",
 		i: "D",
+		"0": "L",
 		d: "R",
 		b: "B",
 		n: "A",
-	};
+	} as const;
 	expect(result).toStrictEqual(expected);
-	type TTrue = Utils.equal<typeof result, typeof expected>;
+	type TTrue = Utils.equal<Utils.deepReadonly<typeof result>, typeof expected>;
 	assertType<TTrue>(true);
 });
--- rendezvous-with-cassidoo/440/konami-mapping.ts
+++ rendezvous-with-cassidoo/440/konami-mapping.ts
@@ -1,12 +1,44 @@
+import type { Utils } from "../../types/utils.types.js";
+
 const regex =
 	/(?<U>.)(?:\1)(?!\1)(?<D>.)(?:\2)(?!\1|\2)((?<L>.)(?!\1|\2|\4)(?<R>.)){2}(?!\1|\2|\4|\5)(?<B>.)(?!\1|\2|\4|\5|\6)(?<A>.)/u;
 
-export const konamiMapping = (str: string): Record<string, string> => {
+type TKonamiMap<Str extends string> = Str extends `${infer U}${infer rest_1}`
+	? rest_1 extends `${U}${infer D}${infer rest_2}`
+		? rest_2 extends `${D}${infer L}${infer R}${infer rest_3}`
+			? L extends R
+				? never
+				: rest_3 extends `${L}${R}${infer B}${infer A}${string}`
+					? B extends A
+						? never
+						: Utils.prettify<
+								Record<U, "U"> &
+									Record<D, "D"> &
+									Record<L, "L"> &
+									Record<R, "R"> &
+									Record<B, "B"> &
+									Record<A, "A">
+							>
+					: never
+			: never
+		: never
+	: never;
+
+type TKonamiMapping<Str extends string> =
+	Str extends `${infer first}${infer rest}`
+		? rest extends `${first}${string}`
+			? TKonamiMap<Str> | TKonamiMapping<rest>
+			: TKonamiMapping<rest>
+		: never;
+
+export const konamiMapping = <Str extends string>(
+	str: Str,
+): TKonamiMapping<Str> => {
 	const match = regex.exec(str);
 	if (!match?.groups) throw new Error("Konami code not found!");
 	const map = {} as Record<string, string>;
 	for (const [key, value] of Object.entries(match.groups)) {
 		map[value] = key;
 	}
-	return map;
+	return map as never;
 };
