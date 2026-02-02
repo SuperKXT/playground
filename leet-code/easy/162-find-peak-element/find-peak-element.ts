2d1c61bb9ebbae9d9d018c81896769fc9a68b837
SuperKXT <superkxt@outlook.com>
2026-02-02 10:12:07 +0500

    Add ts solution for `leet-code/easy/162`

--- leet-code/easy/162-find-peak-element/find-peak-element.ts
+++ leet-code/easy/162-find-peak-element/find-peak-element.ts
@@ -2,6 +2,91 @@
 
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
+type TTuple<
+	size extends number,
+	res extends unknown[] = [],
+> = res["length"] extends size ? res : TTuple<size, [...res, 1]>;
+
+type TNumberToTuple<
+	Num extends number,
+	str extends string = `${Num}`,
+	tup extends number[] = [],
+> = str extends `${"-" | "+"}${infer rest}`
+	? TNumberToTuple<never, rest, tup>
+	: str extends `${infer first extends number}${infer rest}`
+		? TNumberToTuple<never, rest, [...tup, first]>
+		: tup;
+
+type TCompareDigits<
+	digitA extends number,
+	digitB extends number,
+	tupA extends unknown[] = TTuple<digitA>,
+	tupB extends unknown[] = TTuple<digitB>,
+> = digitA extends digitB
+	? "equal"
+	: tupA[tupB["length"]] extends 1
+		? "greater"
+		: "lesser";
+
+type _TCompareNumbers<
+	tupA extends unknown[],
+	tupB extends unknown[],
+> = tupA["length"] extends tupB["length"]
+	? [tupA, tupB] extends [
+			[infer firstA extends number, ...infer restA],
+			[infer firstB extends number, ...infer restB],
+		]
+		? TCompareDigits<firstA, firstB> extends infer res
+			? res extends "equal"
+				? _TCompareNumbers<restA, restB>
+				: res
+			: never
+		: never
+	: tupA[tupB["length"]] extends number
+		? "greater"
+		: "lesser";
+type TCompareNumbers<
+	numA extends number,
+	numB extends number,
+> = numA extends numB
+	? "equal"
+	: `${numA}` extends `-${string}`
+		? `${numB}` extends `-${string}`
+			? _TCompareNumbers<
+					TNumberToTuple<numA>,
+					TNumberToTuple<numB>
+				> extends "greater"
+				? "lesser"
+				: "greater"
+			: "lesser"
+		: `${numB}` extends `-${string}`
+			? "greater"
+			: _TCompareNumbers<TNumberToTuple<numA>, TNumberToTuple<numB>>;
+
+type TFindPeakElement<
+	Nums extends number[],
+	idx extends Array<1> = [],
+	prev extends number = never,
+> = Nums extends [
+	infer first extends number,
+	infer next extends number,
+	...infer rest extends number[],
+]
+	?
+			| (
+					| TCompareNumbers<first, prev>
+					| TCompareNumbers<first, next> extends "greater"
+					? idx["length"]
+					: never)
+			| TFindPeakElement<[next, ...rest], [...idx, 1], first>
+	: Nums extends [infer first extends number, ...infer rest extends number[]]
+		?
+				| (TCompareNumbers<first, prev> extends "greater"
+						? idx["length"]
+						: never)
+				| TFindPeakElement<rest, [...idx, 1], first>
+		: never;
+
 // export const findPeakElement = (nums: number[]): number => {
 // 	for (let i = 0; i < nums.length; i++) {
 // 		if (nums[i]! > nums[i - 1]! && nums[i]! > nums[i + 1]!) return i;
@@ -9,15 +94,17 @@
 // 	return -1;
 // };
 
-export const findPeakElement = (nums: number[]): number => {
+export const findPeakElement = <const Nums extends number[]>(
+	nums: Nums,
+): TFindPeakElement<Nums> => {
 	let left = 0;
 	const right = nums.length - 1;
 	while (left <= right) {
 		const mid = Math.floor((right + left) / 2);
 		if (nums[mid]! > nums[mid - 1]! && nums[mid]! > nums[mid + 1]!) {
-			return mid;
+			return mid as never;
 		}
 		left = mid + 1;
 	}
-	return -1;
+	return -1 as never;
 };
--- leet-code/easy/162-find-peak-element/find-peak-elementfind-peak-element.test.ts
+++ leet-code/easy/162-find-peak-element/find-peak-elementfind-peak-element.test.ts
@@ -1,18 +1,32 @@
-import { expect, test } from "vitest";
+import { assertType, expect, test } from "vitest";
 
 import { findPeakElement } from "./find-peak-element.js";
 
+import type { Utils } from "../../../types/utils.types.js";
+
 test("testing findPeakElement for test 1", () => {
 	const result = findPeakElement([1, 2, 3, 1]);
-	expect(result).toBeOneOf([2]);
+	const expected = [2] as const;
+	expect(result).toBeOneOf(expected as never);
+
+	type TTrue = Utils.equal<typeof result, (typeof expected)[number]>;
+	assertType<TTrue>(true);
 });
 
 test("testing findPeakElement for test 2", () => {
 	const result = findPeakElement([1, 2, 1, 3, 5, 6, 4]);
-	expect(result).toBeOneOf([1, 5]);
+	const expected = [1, 5] as const;
+	expect(result).toBeOneOf(expected as never);
+
+	type TTrue = Utils.equal<typeof result, (typeof expected)[number]>;
+	assertType<TTrue>(true);
 });
 
 test("testing findPeakElement for test 3", () => {
 	const result = findPeakElement([1, 2, 1, 3, 1, 4, 5, 1, 3, 2]);
-	expect(result).toBeOneOf([1, 3, 6, 8]);
+	const expected = [1, 3, 6, 8] as const;
+	expect(result).toBeOneOf(expected as never);
+
+	type TTrue = Utils.equal<typeof result, (typeof expected)[number]>;
+	assertType<TTrue>(true);
 });
