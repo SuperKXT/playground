f251cc6ce647ea205cbaaf653774df546b42b188
SuperKXT <superkxt@outlook.com>
2026-02-02 10:01:02 +0500

    Update `leet-code/easy/162`

--- leet-code/easy/162-find-peak-element/find-peak-element.ts
+++ leet-code/easy/162-find-peak-element/find-peak-element.ts
@@ -1,9 +1,23 @@
 // https://leetcode.com/problems/find-peak-element
 
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+// export const findPeakElement = (nums: number[]): number => {
+// 	for (let i = 0; i < nums.length; i++) {
+// 		if (nums[i]! > nums[i - 1]! && nums[i]! > nums[i + 1]!) return i;
+// 	}
+// 	return -1;
+// };
+
 export const findPeakElement = (nums: number[]): number => {
-	for (let i = 0; i < nums.length; i++) {
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		if (nums[i]! > nums[i - 1]! && nums[i]! > nums[i + 1]!) return i;
+	let left = 0;
+	const right = nums.length - 1;
+	while (left <= right) {
+		const mid = Math.floor((right + left) / 2);
+		if (nums[mid]! > nums[mid - 1]! && nums[mid]! > nums[mid + 1]!) {
+			return mid;
+		}
+		left = mid + 1;
 	}
 	return -1;
 };
--- leet-code/easy/162-find-peak-element/find-peak-elementfind-peak-element.test.ts
+++ leet-code/easy/162-find-peak-element/find-peak-elementfind-peak-element.test.ts
@@ -2,12 +2,17 @@ import { expect, test } from "vitest";
 
 import { findPeakElement } from "./find-peak-element.js";
 
-test("testing replaceElements for test 1", () => {
+test("testing findPeakElement for test 1", () => {
 	const result = findPeakElement([1, 2, 3, 1]);
 	expect(result).toBeOneOf([2]);
 });
 
-test("testing replaceElements for test 2", () => {
+test("testing findPeakElement for test 2", () => {
 	const result = findPeakElement([1, 2, 1, 3, 5, 6, 4]);
-	expect(result).toStrictEqual([1, 5]);
+	expect(result).toBeOneOf([1, 5]);
+});
+
+test("testing findPeakElement for test 3", () => {
+	const result = findPeakElement([1, 2, 1, 3, 1, 4, 5, 1, 3, 2]);
+	expect(result).toBeOneOf([1, 3, 6, 8]);
 });
