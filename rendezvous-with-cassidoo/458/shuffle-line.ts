9c43d2fb3c6b69a0b81e1bd87aeed38875819106
SuperKXT <superkxt@outlook.com>
2026-05-29 10:40:51 +0500

    Update `rwc#458`

--- rendezvous-with-cassidoo/458/shuffle-line.ts
+++ rendezvous-with-cassidoo/458/shuffle-line.ts
@@ -1,11 +1,15 @@
 export const shuffleLine = (names: string[], n: number): string[] => {
 	const left: string[] = [];
 	const right: string[] = [];
-
-	for (let i = 1; i <= names.length; i++) {
-		const curr = names[i - 1] as string;
-		if (i % n === 0) right.push(curr);
-		else left.push(curr);
+	let curr = 1;
+	for (const name of names) {
+		if (curr !== n) {
+			left.push(name);
+		} else {
+			right.push(name);
+			curr = 0;
+		}
+		curr++;
 	}
 
 	return [...left, ...right];
