cf9eb79ff2b484cacf0a53a83fedb1576512a336
SuperKXT <superkxt@outlook.com>
2026-08-22 11:26:05 +0500

    Add ts solution for `rwc#270`

--- rendezvous-with-cassidoo/470/pack-rectangles.test.ts
+++ rendezvous-with-cassidoo/470/pack-rectangles.test.ts
@@ -4,7 +4,7 @@ import { packRectangles } from "./pack-rectangles.js";
 
 test("testing packRectangles against test 1", () => {
 	const result = packRectangles(10, 10, 3, 4);
-	const expected = 6 as number;
+	const expected = 6 as const;
 
 	expect(result).toStrictEqual(expected);
 
@@ -13,7 +13,7 @@ test("testing packRectangles against test 1", () => {
 
 test("testing packRectangles against test 2", () => {
 	const result = packRectangles(10, 6, 2, 3);
-	const expected = 10 as number;
+	const expected = 10 as const;
 
 	expect(result).toStrictEqual(expected);
 
@@ -22,7 +22,7 @@ test("testing packRectangles against test 2", () => {
 
 test("testing packRectangles against test 3", () => {
 	const result = packRectangles(10, 6, 11, 2);
-	const expected = 0 as number;
+	const expected = 0 as const;
 
 	expect(result).toStrictEqual(expected);
 
@@ -31,7 +31,7 @@ test("testing packRectangles against test 3", () => {
 
 test("testing packRectangles against test 4", () => {
 	const result = packRectangles(6, 10, 2, 3);
-	const expected = 10 as number;
+	const expected = 10 as const;
 
 	expect(result).toStrictEqual(expected);
 
--- rendezvous-with-cassidoo/470/pack-rectangles.ts
+++ rendezvous-with-cassidoo/470/pack-rectangles.ts
@@ -1,3 +1,55 @@
+type TCheckCol<
+	N extends number,
+	M extends number,
+	A extends number,
+	B extends number,
+	row extends Array<1>,
+	count extends Array<1>,
+	col extends Array<1> = [],
+	idx extends Array<1> = [],
+> = col["length"] extends N
+	? TCheckRow<N, M, A, B, row, count>
+	: [...idx, 1]["length"] extends A
+		? TCheckCol<N, M, A, B, row, [...count, 1], [...col, 1]>
+		: TCheckCol<N, M, A, B, row, count, [...col, 1], [...idx, 1]>;
+
+type TCheckRow<
+	N extends number,
+	M extends number,
+	A extends number,
+	B extends number,
+	row extends Array<1>,
+	count extends Array<1>,
+	idx extends Array<1> = [],
+> = row["length"] extends M
+	? count
+	: [...idx, 1]["length"] extends B
+		? TCheckCol<N, M, A, B, [...row, 1], count>
+		: TCheckRow<N, M, A, B, [...row, 1], count, [...idx, 1]>;
+
+type TCountRectangles<
+	N extends number,
+	M extends number,
+	A extends number,
+	B extends number,
+> = TCheckRow<N, M, A, B, [], []>;
+
+type TBigger<
+	A extends Array<1>,
+	B extends Array<1>,
+> = A["length"] extends B["length"]
+	? A["length"]
+	: A[B["length"]] extends 1
+		? A["length"]
+		: B["length"];
+
+type TPackRectangles<
+	N extends number,
+	M extends number,
+	A extends number,
+	B extends number,
+> = TBigger<TCountRectangles<N, M, A, B>, TCountRectangles<N, M, B, A>>;
+
 export const countRectangles = (
 	n: number,
 	m: number,
@@ -12,11 +64,19 @@ export const countRectangles = (
 	}
 	return count;
 };
-export const packRectangles = (
-	n: number,
-	m: number,
-	a: number,
-	b: number,
-): number => {
-	return Math.max(countRectangles(n, m, a, b), countRectangles(n, m, b, a));
+export const packRectangles = <
+	N extends number,
+	M extends number,
+	A extends number,
+	B extends number,
+>(
+	n: N,
+	m: M,
+	a: A,
+	b: B,
+): TPackRectangles<N, M, A, B> => {
+	return Math.max(
+		countRectangles(n, m, a, b),
+		countRectangles(n, m, b, a),
+	) as never;
 };
