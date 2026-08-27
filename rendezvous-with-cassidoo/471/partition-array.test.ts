dfed7169b66b93e9b64df3e00810cd86ac23b116
SuperKXT <superkxt@outlook.com>
2026-08-27 14:32:26 +0500

    Add ts solution for `rwc#471`

--- rendezvous-with-cassidoo/471/partition-array.test.ts
+++ rendezvous-with-cassidoo/471/partition-array.test.ts
@@ -4,9 +4,9 @@ import { partitionArray } from "./partition-array.js";
 
 test("testing partitionArray against test 1", () => {
 	const result = partitionArray([0, 3, 2, 1, 4, 0, 7]);
-	const expected = [3, 1, 7, 2, 4, 0, 0];
+	const expected = [3, 1, 7, 2, 4, 0, 0] as const;
 
 	expect(result).toStrictEqual(expected);
 
-	expectTypeOf<typeof result>(result).toEqualTypeOf(expected);
+	expectTypeOf<Readonly<typeof result>>(result).toEqualTypeOf(expected);
 });
--- rendezvous-with-cassidoo/471/partition-array.ts
+++ rendezvous-with-cassidoo/471/partition-array.ts
@@ -1,4 +1,23 @@
-export const partitionArray = (arr: number[]): number[] => {
+type TIsEven<T extends number> = `${T}` extends `${string}${0 | 2 | 4 | 6 | 8}`
+	? true
+	: false;
+
+type TPartitionArray<
+	Arr extends number[],
+	odd extends number[] = [],
+	even extends number[] = [],
+	zero extends number[] = [],
+> = Arr extends [infer num extends number, ...infer res extends number[]]
+	? num extends 0
+		? TPartitionArray<res, odd, even, [...zero, num]>
+		: TIsEven<num> extends true
+			? TPartitionArray<res, odd, [...even, num], zero>
+			: TPartitionArray<res, [...odd, num], even, zero>
+	: [...odd, ...even, ...zero];
+
+export const partitionArray = <const Arr extends number[]>(
+	arr: Arr,
+): TPartitionArray<Arr> => {
 	const odd: number[] = [];
 	const even: number[] = [];
 	const zero: number[] = [];
@@ -7,5 +26,5 @@ export const partitionArray = (arr: number[]): number[] => {
 		else if (num % 2 === 0) even.push(num);
 		else odd.push(num);
 	}
-	return [...odd, ...even, ...zero];
+	return [...odd, ...even, ...zero] as never;
 };
